c08f5f51e7eb5957306d7c72d05eed0744815edd
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-20 18:39:45 +0900

    adding adobedigitaleditions

---
+++ adobedigitaleditions/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'adobedigitaleditions'
+$installerType = 'exe'
+$url = 'https://adedownload.adobe.com/pub/adobe/digitaleditions/ADE_4.5_Installer.exe'
+$silentArgs = '/S' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes  -Checksum 1457FFF9877B292F66A1BA9E5D95D069503B92121165EFB4CE41CD8FADA9BEB8 -ChecksumType sha256
