17b7cf217797d4096f12f1138e1d0590dff0eb5b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-20 17:35:20 +0900

    adding virtualrouter

---
+++ virtualrouter/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'virtualrouter' 
+$installerType = 'msi' 
+$url = 'https://virtualrouter.codeplex.com/downloads/get/621827' 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 73BC4652BC1B4FF67C990A0AB9A68DDE4EC03E90B0FC1A642CEA0FF08E25CBB2 -ChecksumType sha256
