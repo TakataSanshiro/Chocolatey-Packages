4e432fb266b3163eb7fa9d68a2181631738ed656
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-22 01:43:12 +0900

    adding confide

---
+++ confide/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'confide' 
+$installerType = 'exe' 
+$url = 'https://getconfide.com/download/windows/dl' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 83B3CD41C6CC5AF3556A3BDDFCE9A13A509F4C51CAAAA6C50DF2F1936E7A9A71 -ChecksumType sha256
