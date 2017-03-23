4c3d0fbf502df0c7e5375d5c9f01a61124c3ebad
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-23 23:13:58 +0900

    adding itch

---
+++ itch/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'itch' 
+$installerType = 'exe' 
+$url = 'http://nuts.itch.zone/download/windows' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 0E1635478936954F75873546798773D109D35D21F30C56EEF66467D60EAFB538 -ChecksumType sha256
+
+
