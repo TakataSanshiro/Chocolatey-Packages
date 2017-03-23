469d9ab76b4808d21ed04bfb3ce6697d53ac9c18
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-24 00:23:54 +0900

    adding tinywall

---
+++ tinywall/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'tinywall' 
+$installerType = 'msi' 
+$url = 'http://tinywall.pados.hu/ccount/click.php?id=3' 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 05D348D18F99FED60E4ED82370EC3690655D0170F9034E1D06094F563190D64A -ChecksumType sha256
