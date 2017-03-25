eda93e98fe952c9073fd0698f789a39c53518812
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-25 18:38:45 +0900

    adding grammarlyformsoffice

---
+++ grammarlyformsoffice/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'grammarlyformsoffice' 
+$installerType = 'exe' 
+$url = 'https://download-office.grammarly.com/latest/GrammarlyAddInSetup.exe' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum F3DB7B7952B7DFD21CC7812E66FF50227F0AB261D13BE08583272A68F543FDF7 -ChecksumType sha256
