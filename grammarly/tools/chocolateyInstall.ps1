c3c619a2303d8cac0e85c7a852cd13d994df45fa
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-25 18:18:14 +0900

    adding grammarly

---
+++ grammarly/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'grammarly' 
+$installerType = 'exe' 
+$url = 'https://download-editor.grammarly.com/windows/GrammarlySetup.exe' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 3E3DAABFF26BB436EF3925B83CFC9553D3269695C31A4F76F168A508018826ED -ChecksumType sha256
