a3b57698701abb800b62e9ce78df402cffa99574
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-27 00:53:37 +0900

    adding textify

---
+++ textify/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'textify' 
+$installerType = 'exe' 
+$url = 'http://rammichael.com/downloads/textify_setup.exe' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 996CD588D664188CE41696B729A58876B5B0209492CFD7C280E6C57E7148C5F2 -ChecksumType sha256
