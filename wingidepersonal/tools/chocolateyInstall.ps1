ad28a256f8717a9efc8719a6456db798821592e2
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-27 00:20:54 +0900

    adding wingidepersonal

---
+++ wingidepersonal/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'wingidepersonal' 
+$installerType = 'exe' 
+$url = 'https://wingware.com/pub/wingide-personal/6.0.3/wingide-personal-6.0.3-1.exe' 
+$silentArgs = '/VERYSILENT' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 47148C22EFD5282944312967C4DD99735A49E465004C8B64651A819806ED3CBB -ChecksumType sha256
