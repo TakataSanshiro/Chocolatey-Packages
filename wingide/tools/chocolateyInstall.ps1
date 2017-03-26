538fcfacf7f32fd5d2ff6c7993694e96ec407524
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-03-27 00:01:05 +0900

    adding wingide

---
+++ wingide/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'wingide' 
+$installerType = 'exe' 
+$url = 'http://wingware.com/pub/wingide/6.0.3/wingide-6.0.3-1.exe' 
+$silentArgs = '/VERYSILENT' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 6756649FBF3BC7A8D98C45ADC840778B684CE26549F981F43184E67A474DC90C -ChecksumType sha256
