8fa24e6b0692629410962701daaa272b6252ac68
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-04-08 04:12:51 +0900

    adding xenko

---
+++ xenko/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'xenko'
+$installerType = 'exe'
+$url = 'https://store.xenko.com/Content/XenkoSetup.exe' 
+$silentArgs = '/q'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum C6F73CCDFA994E1F1D942F40E52202FC8CF84BAA58C37C60422B345EB901ECAB -ChecksumType sha256
