66c5a41e9e02f5d89ee3da541d0d22c4535e5eb7
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-04-13 02:25:38 +0900

    adding twitch

---
+++ twitch/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'twitch'
+$installerType = 'exe'
+$url = 'https://updates.twitchapp.net/windows/TwitchSetup.exe' 
+$silentArgs = '/Q'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum FC06BDF473CAEABB7C9FDCF97BB01E4C9CD5E783EA3DD43DD86269FA7B4B4EA0 -ChecksumType sha256
