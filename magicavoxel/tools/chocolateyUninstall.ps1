9c45a39be9702134b3d7ad5e7f69edc2c945c011
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-04-21 22:10:45 +0900

    add chocolateyUninstall.ps1

---
+++ magicavoxel/tools/chocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿Uninstall-ChocolateyZipPackage `
+  -PackageName "magicavoxel" `
+  -ZipFileName "MagicaVoxel-0.98.2-win.zip" 
+
+Remove-item -path "$Home\Desktop\MagicaVoxel.lnk" -Force -ErrorAction 'SilentlyContinue'
