c5707dfcdef1ae4276e994e9ca24d9da05dc0038
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-04-21 22:31:53 +0900

    add chocolateyUninstall.ps1

---
+++ magicavoxelviewer/tools/chocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿Uninstall-ChocolateyZipPackage `
+  -PackageName "magicavoxelviewer" `
+  -ZipFileName "MagicaVoxel%20Viewer-win-mac.zip" 
+
+Remove-item -path "$Home\Desktop\MagicaVoxelViewer.lnk" -Force -ErrorAction 'SilentlyContinue'
