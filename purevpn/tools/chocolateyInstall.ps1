8ccc3499013fc596e199020aed71708bed017524
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-04-27 20:38:48 +0900

    change a silentArgs and validExitCodes
    and update hash

--- purevpn/tools/chocolateyInstall.ps1
+++ purevpn/tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
-﻿$packageName = 'purevpn' 
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'purevpn' 
 $installerType = 'exe' 
 $url = 'https://s3.amazonaws.com/purevpn-dialer-assets/windows/app/purevpn_setup.exe' 
 $silentArgs = '/VERYSILENT' 
-$validExitCodes = @(0) 
+$validExitCodes = @(0 3010, 1641) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 38B5F9AF1D92A7BEAD486164EDC0E3666F2F4AFC423E143370FA00535BF08E1D -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 5212777915F2402D13022EB010BAC0AFFC12E33E30C9C48125720F9B2BD5F526 -ChecksumType sha256
