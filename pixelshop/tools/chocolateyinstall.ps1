ba7837896cc61bee1da83a2c9eb5d185109a1413
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-04-29 15:10:44 +0900

    add pixelshop

---
+++ pixelshop/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'pixelshop'
+$url = 'https://blackflux.com/software/ps/download/PixelShop.zip' 
+$checksum = '3CC3822A0B8337F19DD77B040E1F60A0830D6E316975879A86722E941B30697B'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an execution shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\PixelShop.lnk" `
+						     -TargetPath "$toolsDir\PixelShop\PixelShop.exe"
+							 
---
+++ pixelshop/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "pixelshop" `
+  -ZipFileName "PixelShop.zip" 
+
+Remove-item -path "$Home\Desktop\PixelShop.lnk" -Force -ErrorAction 'SilentlyContinue'
