3b86389c26beab4352828289fa345cbd0783944b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-04-29 18:04:17 +0900

    add pixie

---
+++ pixie/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'pixie'
+$url = 'http://www.nattyware.com/bin/pixie.zip' 
+$checksum = '140DE93744FB05E307D840C07E84F1C22759EEE8360BCAB2122D02A41FAEA0B7'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an execution shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\Pixie.lnk" `
+						     -TargetPath "$toolsDir\pixie.exe"
+							 
---
+++ pixie/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "pixie" `
+  -ZipFileName "pixie.zip" 
+
+Remove-item -path "$Home\Desktop\Pixie.lnk" -Force -ErrorAction 'SilentlyContinue'
