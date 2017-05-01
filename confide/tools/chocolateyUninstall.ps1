f9408a6c85da902ddc2437dc2e3eb39cdac38b84
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 02:43:01 +0900

    add uninstall script

---
+++ confide/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'confide' 
+$installerType = 'exe' 
+$silentArgs = '--uninstall -s' 
+$validExitCodes = @(0) 
+$file = "$Env:USERPROFILE\AppData\Local\Confide\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Confide at $Env:USERPROFILE\AppData\Local\Confide
+Remove-Item $Env:USERPROFILE\AppData\Local\Confide -recurse
