5db4a2d23b5a8c8275730de34bc11cf6a396422a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 02:41:08 +0900

    add uninstall script

---
+++ boostnote/tools/chocolateyUninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'boostnote'
+$installerType = 'exe'
+$silentArgs = '--uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\boost\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of BoostNote at $Env:USERPROFILE\AppData\Local\boost
+# Doesn't delete user's .json and .cson note data.
+Remove-Item $Env:USERPROFILE\AppData\Local\boost -recurse
