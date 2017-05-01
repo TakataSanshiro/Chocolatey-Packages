ee69565863aabfddb95ad5ea08140c4b2a7d8e75
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-01 23:40:53 +0900

    update to 6.2.0.10

--- gamingapp/tools/chocolateyInstall.ps1
+++ gamingapp/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $url = 'http://download.msi.com/uti_exe/gaming_app_6.zip'
 $silentArgs = '/VERYSILENT' 
 $validExitCodes = @(0) 
 $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$exe = "$scriptDir\6.2.0.08\Gaming APP.exe"
+$exe = "$scriptDir\6.2.0.10\Gaming APP.exe"
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$scriptDir" -Checksum 5BC321CF851E32CCC5CE5E5A7FCA16878F834FB389E5AE1AE130190452E13A6E -ChecksumType sha256
+Install-ChocolateyZipPackage "$packageName" "$url" "$scriptDir" -Checksum 8E80BBEC110D4C9176F6D4F460F6F0FD0E49AE99F3FFCC32EBF7BE93DA5E3692 -ChecksumType sha256
 Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$exe"
