fec2bd5d4bd86c5e2e6b8122d12f4cc786bc0c24
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 22:26:22 +0900

    add uninstall script

---
+++ itch/tools/chocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'itch' 
+$installerType = 'exe' 
+$silentArgs = '--uninstall --uninstall -s' 
+$validExitCodes = @(0) 
+$file = "$Env:USERPROFILE\AppData\Local\itch\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
