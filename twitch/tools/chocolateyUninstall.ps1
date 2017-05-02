40add2a04048b07307147babffb356b19364d821
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 20:49:27 +0900

    add uninstall script

---
+++ twitch/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'twitch' 
+$installerType = 'exe' 
+$silentArgs = '/qn' 
+$validExitCodes = @(0) 
+$file = 'Get-UninstallRegistryKey -SoftwareName "twitch"'.UninstallString
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
