77ff2c8e5e62594c9178febd77032ec1dcc15000
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-03 01:00:30 +0900

    add uninstall script

---
+++ bitcrypt/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'bitcrypt'
+$installerType = 'exe'
+$silentArgs = '/currentuser /S'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\Programs\bitcrypt\Uninstall bitcrypt.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Bitcrypt at $Env:USERPROFILE\AppData\Local\Programs\bitcrypt
+Remove-Item $Env:USERPROFILE\AppData\Local\Programs\bitcrypt -recurse
