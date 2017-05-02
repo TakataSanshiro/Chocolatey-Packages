e4991fa1029ca030aa0e746a2d9f464d2b2f3669
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 18:57:10 +0900

    add uninstall script

---
+++ gamingapp/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'gamingapp' 
+$installerType = 'exe' 
+$silentArgs = '/SILENT' 
+$validExitCodes = @(0) 
+$file = "${env:ProgramFiles(x86)}\MSI\Gaming APP\unins000.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
