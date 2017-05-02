33d1d67b502c164c78797bed5201227bcaa06094
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 23:10:34 +0900

    add uninstall script

---
+++ textify/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'textify'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Roaming\Textify\uninstall.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Textify at $Env:USERPROFILE\AppData\Roaming\Textify
+Remove-Item $Env:USERPROFILE\AppData\Roaming\Textify -recurse
