fbf33b5a2bde63a356becf95068f87cc101aa746
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 19:19:27 +0900

    add uninstall script

---
+++ wingide/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wingide' 
+$installerType = 'exe' 
+$silentArgs = '/SILENT' 
+$validExitCodes = @(0) 
+$file = "${env:ProgramFiles(x86)}\Wing IDE 6.0\unins000.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
