2e4d2eabde72d592ca6dae5428e89f3964e7ecbb
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 21:09:34 +0900

    add uninstall script

---
+++ cryptocat/tools/chocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'cryptocat' 
+$installerType = 'exe' 
+$silentArgs = '/currentuser /S' 
+$validExitCodes = @(0) 
+$file = "$Env:USERPROFILE\AppData\Local\Programs\Cryptocat\Uninstall Cryptocat.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
