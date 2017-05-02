0ab9af67e76485f2c69831c007af833355af8f6c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 22:57:21 +0900

    add uninstall script

---
+++ grammarly/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'grammarly'
+$installerType = 'exe'
+$silentArgs = '--uninstall --uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\GrammarlyForWindows\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of GrammarlyForWindows at $Env:USERPROFILE\AppData\Local\GrammarlyForWindows
+Remove-Item $Env:USERPROFILE\AppData\Local\GrammarlyForWindows -recurse
