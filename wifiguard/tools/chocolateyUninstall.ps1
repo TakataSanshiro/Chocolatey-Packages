81b4748a35ed65c9d302b4a6072597868eaf7e79
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-02 23:22:56 +0900

    add uninstall script

---
+++ wifiguard/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wifiguard' 
+$installerType = 'exe' 
+$silentArgs = '/SILENT' 
+$validExitCodes = @(0) 
+$file = "${env:ProgramFiles}\SoftPerfect WiFi Guard\unins000.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+  
