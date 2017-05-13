c57a40151b72a2e10406775d6b1980890438e9e5
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-14 04:43:21 +0900

    add brave-browser

---
+++ brave-browser/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'brave-browser'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/brave/browser-laptop/releases/download/v0.15.2dev/BraveSetup-ia32.exe'
+$url64 = 'https://github.com/brave/browser-laptop/releases/download/v0.15.2dev/BraveSetup-x64.exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum C5D06F073E05EA89995C3BAD47BD1E421B53577A264BA47B934E6264F55B795F -ChecksumType sha256 -checksum64 CB45E42EAEB70442EF28237FAE39AE1CF09CC8FA403986746226E69C9B89E227 -checksumtype64 sha256
---
+++ brave-browser/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'brave-browser'
+$installerType = 'exe'
+$silentArgs = '--uninstall --uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\brave\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Brave at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Local\brave -recurse
+Remove-Item $Env:USERPROFILE\AppData\Roaming\brave -recurse
