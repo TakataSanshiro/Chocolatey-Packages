0304cbc6cfa90bc1283a98adc13a867d226c77cf
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-15 01:18:00 +0900

    add wordpress-com-for-desktop

---
+++ wordpress-com-for-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wordpress-com-for-desktop'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://public-api.wordpress.com/rest/v1.1/desktop/windows/download'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 5E97E65663B9F2D250C8CA01F28736FBF78593EEF194A7F291B1F72258C12285 -ChecksumType sha256
---
+++ wordpress-com-for-desktop/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wordpress-com-for-desktop'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "${env:ProgramFiles(x86)}\WordPress.com\Uninstall WordPress.com.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of WordPress.com at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Roaming\WordPress.com -recurse
