8f42b9eabeac70132872d893258f850a4fbf6ff6
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-14 22:15:02 +0900

    add webtorrent-desktop

---
+++ webtorrent-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'webtorrent-desktop'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.18.0/WebTorrentSetup-v0.18.0-ia32.exe'
+$url64 = 'https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.18.0/WebTorrentSetup-v0.18.0.exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 0B53D6DE17C33C5E87E41D86F3EF8276BE0677B74707E8780594AA6F0BD10FCD -ChecksumType sha256 -checksum64 40F04854CD376AC686689B83E4D6208649493BD20E76E9CDE8CB8700ACEFB1DA -checksumtype64 sha256
---
+++ webtorrent-desktop/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'webtorrent-desktop'
+$installerType = 'exe'
+$silentArgs = '--uninstall --uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\WebTorrent\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of WebTorrent at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Local\WebTorrent -recurse
+Remove-Item $Env:USERPROFILE\AppData\Roaming\WebTorrent -recurse
