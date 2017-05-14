c9fa5b4b4376b78bfa7b85e5b9c46b397d452d8c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-14 16:25:57 +0900

    add 1clipboard

---
+++ 1clipboard/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = '1clipboard'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'http://1clipboard.io/download/windows/1ClipboardSetup.exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum A3425082F63FFB0F7AABFE5C01BBD6346D90ACB88366CF293FB55AD2C85D9196 -ChecksumType sha256
---
+++ 1clipboard/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = '1clipboard'
+$installerType = 'exe'
+$silentArgs = '--uninstall --uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\1clipboard\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Brave at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Local\1clipboard -recurse
+Remove-Item $Env:USERPROFILE\AppData\Roaming\1clipboard -recurse
