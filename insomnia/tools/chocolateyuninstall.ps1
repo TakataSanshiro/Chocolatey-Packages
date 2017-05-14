e39a2ac7242fdf0fcc02896f3628325aff2168e8
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-14 19:40:50 +0900

    add insomnia

---
+++ insomnia/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'insomnia'
+$installerType = 'exe'
+$silentArgs = '--uninstall --uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\insomnia\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Insomnia at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Local\insomnia -recurse
+Remove-Item $Env:USERPROFILE\AppData\Roaming\insomnia -recurse
