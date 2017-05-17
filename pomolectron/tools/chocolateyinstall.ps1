0c3b267b1a8475487e7b43e63dd4cb09c49ea15a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-17 19:15:22 +0900

    add pomolectron

---
+++ pomolectron/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'pomolectron'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/amitmerchant1990/pomolectron/releases/download/v1.1.0/Pomolectron-1.1.0.exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum D12BA816C017FDE8EED6E8F7C65C29CC99B8B1A420365F5D591D104B99472A42 -ChecksumType sha256
---
+++ pomolectron/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'pomolectron'
+$installerType = 'exe'
+$silentArgs = '/SILENT'
+$validExitCodes = @(0)
+$file = "${env:ProgramFiles(x86)}\Pomolectron\unins000.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Pomolectron at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Roaming\Pomolectron -recurse
