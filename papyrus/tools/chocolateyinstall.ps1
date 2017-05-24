5096026cbdecf03ac4d4bda5c131418dfcb933aa
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-24 20:48:34 +0900

    add papyrus

---
+++ papyrus/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'papyrus'
+$url = 'https://github.com/morkro/papyrus/releases/download/1.0.2/Papyrus-windows-1.0.2.zip'
+$checksum = 'EC624A7387ED2257BEB9C0483A422B0E4B1D950E1F01AA188B8AB4077FFBF2DF'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an .exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\Papyrus.lnk" `
+						     -TargetPath "$toolsDir\Papyrus.exe"
+							 
---
+++ papyrus/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿Uninstall-ChocolateyZipPackage `
+  -PackageName "papyrus" `
+  -ZipFileName "Papyrus-windows*" 
+
+Remove-item -path "$Home\Desktop\Papyrus.lnk" -Force -ErrorAction 'SilentlyContinue'
+Remove-Item $Env:USERPROFILE\AppData\Roaming\Papyrus -recurse
