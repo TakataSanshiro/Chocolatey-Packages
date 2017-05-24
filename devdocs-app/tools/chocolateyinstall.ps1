7225afe657c93b3b0480fe7fe2e54b1cac7dbd5a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-24 18:50:42 +0900

    add devdocs-app

---
+++ devdocs-app/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'devdocs-app'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/egoist/devdocs-app/releases/download/v0.2.2/DevDocs-0.2.2-ia32.exe'
+$checksum = '6563254134E69CB1D641132BE63341430B3DB18F67635F29AD98C73F2AFFA818'
+$checksumType = 'sha256'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
---
+++ devdocs-app/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'devdocs-app'
+$installerType = 'exe'
+$silentArgs = '--uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\DevDocs\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
