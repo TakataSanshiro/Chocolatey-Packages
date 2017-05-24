253d64e836e41330ed7143453e30f8bfc4a881a1
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-24 22:13:44 +0900

    add wow-stat

---
+++ wow-stat/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wow-stat'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/arzynik/wowstat/releases/download/3.0.5/WoWStat-win-3.0.5.exe'
+$checksum = 'F838FBA73B86584B0F51585242170A9CA0B5EFA8C5B26A165735A399A20B3001'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
---
+++ wow-stat/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wow-stat'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\Programs\WoW Stat\Uninstall WoW Stat.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+
+# Delete junk files of wowstat at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Roaming\wowstat -recurse
