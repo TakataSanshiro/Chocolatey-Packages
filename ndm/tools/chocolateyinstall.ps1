95eb3757dd99e09b27a9ffea16f1c39999653225
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-24 18:51:49 +0900

    add ndm

---
+++ ndm/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'ndm'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/720kb/ndm/releases/download/v1.2.0/ndm-1.2.0.exe'
+$checksum = '2AF8B2AAF106B0F3411F9E9594715B3A29ECE444EA62A7024AC09C09CA0F475D'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
---
+++ ndm/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'ndm'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\Programs\ndm\Uninstall ndm.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
