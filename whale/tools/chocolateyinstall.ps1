b8b363846e1eb5dbb2e9685184bba51171550ea0
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-24 22:53:26 +0900

    add whale

---
+++ whale/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'whale'
+$url = 'https://github.com/1000ch/whale/releases/download/v0.9.0/Whale-windows-v0.9.0.zip'
+$checksum = '2B2EC403151D2826A20B60F803E967502408403F45FD24311A16302C07C0424E'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an .exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\Whale.lnk" `
+						     -TargetPath "$toolsDir\Whale.exe"
+							 
---
+++ whale/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "whale" `
+  -ZipFileName "Whale-windows*" 
+
+Remove-item -path "$Home\Desktop\Whale.lnk" -Force -ErrorAction 'SilentlyContinue'
