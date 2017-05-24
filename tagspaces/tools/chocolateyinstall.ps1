f52b75155962d6b612a38d20ee4f7cff28c6fb77
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-24 15:20:08 +0900

    add tagspaces

---
+++ tagspaces/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'tagspaces'
+$url = 'https://github.com/tagspaces/tagspaces/releases/download/v2.8.0/tagspaces-2.8.0-win32.zip'
+$checksum = '6159D3FEC31F315AF7B2BA74A0BE59F3F365CE91AE9345BBAC1C77347C83D81A'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/tagspaces/tagspaces/releases/download/v2.8.0/tagspaces-2.8.0-win64.zip'
+$checksum64 = 'F417BEEE16031603E3B14F1FFE99683FABBD97CC544822D2DD10560DBD9A7421'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+							 -Url64bit "$url64" `
+							 -Checksum64 "$checksum64" `
+							 -ChecksumType64 "$checksumType64"
+
+# Create an .exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\tagspaces.lnk" `
+						     -TargetPath "$toolsDir\tagspaces\tagspaces.exe"
+							 
---
+++ tagspaces/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "tagspaces" `
+  -ZipFileName "tagspaces*" 
+
+Remove-item -path "$Home\Desktop\tagspaces.lnk" -Force -ErrorAction 'SilentlyContinue'
+Remove-Item $Env:USERPROFILE\AppData\Roaming\tagspaces -recurse
