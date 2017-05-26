30a54bd0965444c093c2d0ec6f1af368ecc2cead
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-26 18:15:44 +0900

    add wowcrypt

---
+++ wowcrypt/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wowcrypt'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\Programs\WoW Crypt\0.0.4\Uninstall WoW Crypt.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+
+# Delete junk files of WoW Crypt at $Env:USERPROFILE\AppData
+Remove-Item "$Env:USERPROFILE\AppData\Roaming\WoW Crypt" -recurse
