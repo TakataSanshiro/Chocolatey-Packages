9b3cbe5c94b57d4af5bce31c66c9695fef3dbc67
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-26 20:06:16 +0900

    add github-desktop

---
+++ github-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'github-desktop'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://central.github.com/deployments/desktop/desktop/latest/win32'
+$checksum = '368528069EEFE47071CD61868E3D8F429429D8867F7EBAE0C230D2DC52B15EBE'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
