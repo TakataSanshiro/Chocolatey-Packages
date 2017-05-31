4064b3e951d06646cf5e795868541e1d0bc6fbc5
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-05-31 17:35:46 +0900

    add validExitCodes 1638 for the update error handling

--- twitch/tools/chocolateyInstall.ps1
+++ twitch/tools/chocolateyInstall.ps1
@@ -4,6 +4,6 @@ $packageName = 'twitch'
 $installerType = 'exe'
 $url = 'https://updates.twitchapp.net/windows/TwitchSetup.exe' 
 $silentArgs = '/Q'
-$validExitCodes = @(0)
+$validExitCodes = @(0, 1638)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum B15B2236E9BFE12C331C27C7246B72711A79E2B832F2C90C5620D5D6DC1EB592 -ChecksumType sha256
