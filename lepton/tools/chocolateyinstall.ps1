048bf3eeee1920463e82aee4eccbe35c64cfbbe9
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-07 22:31:15 +0900

    update to 1.3.0

---
+++ lepton/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'lepton'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/hackjutsu/Lepton/releases/download/v1.3.0/Lepton-Setup-1.3.0.exe'
+$checksum = 'A59B6CE7663459D57502DBC7DB5DA11A5145699BFF65AAC0279ACD2CC21DA807'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
