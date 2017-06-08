c42d3d4a74b34f2a6bf04fc3b9488be4da44d770
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-08 16:11:52 +0900

    update to 1.5.27

--- grammarly/tools/chocolateyInstall.ps1
+++ grammarly/tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
-﻿$packageName = 'grammarly' 
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'grammarly' 
 $installerType = 'exe' 
 $url = 'https://download-editor.grammarly.com/windows/GrammarlySetup.exe' 
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 5820616B37AACFFDDB69884BC378792F553A47F902D88A4844355E6E9DC0ECB2 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 5898327AC8069B3469F36261E2430861707B9D974167B4FB03A643F1C15B618D -ChecksumType sha256
--- grammarly/tools/chocolateyUninstall.ps1
+++ grammarly/tools/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'grammarly'
 $installerType = 'exe'
-$silentArgs = '--uninstall --uninstall -s'
+$silentArgs = '--uninstall -s'
 $validExitCodes = @(0)
 $file = "$Env:USERPROFILE\AppData\Local\GrammarlyForWindows\Update.exe"
 
