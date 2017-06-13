1a971c019128c67c1800d770f389530e7ee42875
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-14 00:39:24 +0900

    add fileassassin

---
+++ fileassassin/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'fileassassin'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://downloads.malwarebytes.com/file/fileassassin/'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  silentArgs  = "/S"
+  softwareName  = '*fileassassin*'
+  checksum      = '829D12A736B29D03699F840616B0A8B658CD4CB29D6F3AEDAB53B065293FE05D'
+  checksumType  = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ fileassassin/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'fileassassin'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "${env:ProgramFiles(x86)}\FileASSASSIN\uninst.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
