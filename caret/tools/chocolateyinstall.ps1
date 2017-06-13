0d218664aab739150c20ef0bdf91df1572ed9b78
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-14 02:27:24 +0900

    add caret

---
+++ caret/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'caret'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/careteditor/caret/releases/download/2.1.3/CaretSetup.exe'
+$checksum = '69987A77B1999C87AD178571ACEC5DCFD79B1AD92E31B430798650143EF29478'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
---
+++ caret/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'caret'
+$installerType = 'exe'
+$silentArgs = '--uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\Caret\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+
+# Delete junk files of Caret at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Local\Caret -recurse
+Remove-Item $Env:USERPROFILE\AppData\Roaming\Caret -recurse
