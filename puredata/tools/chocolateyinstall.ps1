d74c8e02d9805533fdbd6d661ce674ffa7d5ce95
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-14 16:54:55 +0900

    add puredata

---
+++ puredata/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'puredata'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://msp.ucsd.edu/Software/pd-0.47-1.windows-installer.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  silentArgs  = "/S"
+  softwareName  = '*puredata*'
+  checksum      = 'CF9BA0D009ACFBB1914F9C50E4E24F9465327B85BB3445609C1478AB1C7A3BB6'
+  checksumType  = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ puredata/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'puredata'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "${env:ProgramFiles(x86)}\Pd\uninst.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
