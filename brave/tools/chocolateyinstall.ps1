7056a172de538f94389ee4cdcab767e99d43fb9e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-16 00:16:43 +0900

    brave replaced brave-browser

---
+++ brave/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'brave'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/brave/browser-laptop/releases/download/v0.15.314dev/BraveSetup-ia32.exe'
+$checksum = 'A9D6D2E2A8C71489B43484C135EF636641AEC2893CCCE35F75C6AF124C18BD85'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/brave/browser-laptop/releases/download/v0.15.314dev/BraveSetup-x64.exe'
+$checksum64 = '22BF4A2AFE6E8307B9445C74D270837B17D52BAFE2F0267F7350E623EA144B22'
+$checksumType64 = 'sha256'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -Url64bit "$url64" `
+  -Checksum64 "$checksum64" `
+  -ChecksumType64 "$checksumType64" `
+  -ValidExitCodes "$validExitCodes"
