676d9ebbcbdbf6b72103c43bd94881c213955240
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-16 22:47:38 +0900

    add rosaimagewriter

---
+++ rosaimagewriter/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "rosaimagewriter" `
+  -ZipFileName "RosaImageWriter*" 
+
+Remove-item -path "$Home\Desktop\RosaImageWriter.lnk" -Force -ErrorAction 'SilentlyContinue'
