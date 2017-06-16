8dd96a9e5f1731b5c5a7bbf0a00ac04687949a18
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-17 00:45:06 +0900

    add kuadro

---
+++ kuadro/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'kuadro'
+$url = 'http://kruelgames.com/tools/kuadro/latest' 
+$checksum = 'BEDD9F480D7DF47F1D6E14F37726A7DA105CC3641FB38B063138E91856594E6C'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$toolsDir\kuadro-0.8.5.exe" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+# Create an exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\kuadro.lnk" `
+						     -TargetPath "$toolsDir\kuadro-0.8.5.exe"
+							 
---
+++ kuadro/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "kuadro" `
+  -ZipFileName "kuadro*" 
+
+Remove-item -path "$Home\Desktop\kuadro.lnk" -Force -ErrorAction 'SilentlyContinue'
