c1e6718c2f8de841ef64b2a59636e0f45801952a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-17 02:36:25 +0900

    add clibor

---
+++ clibor/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "clibor" `
+  -ZipFileName "clibor*.zip" 
+
+Remove-item -path "$Home\Desktop\Clibor.lnk" -Force -ErrorAction 'SilentlyContinue'
