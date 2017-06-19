b3cc421200fd1df315efca96a594f50475fc664e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-19 15:41:38 +0900

    update to 0.5.1

--- devdocs-app/tools/chocolateyinstall.ps1
+++ devdocs-app/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'devdocs-app'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/egoist/devdocs-app/releases/download/v0.3.0/DevDocs.Setup.0.3.0-ia32.exe'
-$checksum = '0C9012C74083BA87D59507573F7C99C745656CBCF285FFC6F538D5D233933263'
+$url = 'https://github.com/egoist/devdocs-app/releases/download/v0.5.1/DevDocs.Setup.0.5.1.exe'
+$checksum = '19B7796C76E8FAABC750B70FE1882343B32D7A14FF00E7B8FCDFD21CBD51353F'
 $checksumType = 'sha256'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0) 
