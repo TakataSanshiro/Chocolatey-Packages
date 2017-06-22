b6472ad6ca303fd5b86a46995e9c1fed542f524d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-22 22:49:02 +0900

    add crowdinspect

---
+++ crowdinspect/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'crowdinspect'
+$url = 'http://download.crowdstrike.com/crowdinspect/CrowdInspect.zip' 
+$checksum = '32FEC9FE206257901F348512A037BAECC959C7487EB8BAE9913BA010B72CB35E'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\CrowdInspect.lnk" `
+						     -TargetPath "$toolsDir\CrowdInspect.exe"
---
+++ crowdinspect/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "crowdinspect" `
+  -ZipFileName "CrowdInspect*" 
+
+Remove-item -path "$Home\Desktop\CrowdInspect.lnk" -Force -ErrorAction 'SilentlyContinue'
+Remove-item -path "$Home\Desktop\CrowdInspect64.exe" -Force -ErrorAction 'SilentlyContinue'
