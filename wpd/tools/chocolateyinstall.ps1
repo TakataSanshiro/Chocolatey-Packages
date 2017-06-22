c596de9349f721af73a986c3bbf9dbdd11e4f6e7
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-22 23:11:45 +0900

    add wpd

---
+++ wpd/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wpd'
+$url = 'https://getwpd.com/get/WPD_1.1.451.zip' 
+$checksum = '45C383E2240F5E6E83DF84D7FDE53B1B23235DC89129850402FAE58A80D9B9DE'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\WPD.lnk" `
+						     -TargetPath "$toolsDir\WPD.exe"
---
+++ wpd/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "wpd" `
+  -ZipFileName "WPD*" 
+
+Remove-item -path "$Home\Desktop\WPD.lnk" -Force -ErrorAction 'SilentlyContinue'
