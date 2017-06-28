f7754cb6e10a48fcf2be358a48b7f367f42634f3
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-29 03:34:15 +0900

    add fontbase

---
+++ fontbase/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'fontbase'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'http://releases.fontba.se/win/2.0.2/FontBase%20Setup%202.0.2.exe'
+$checksum = 'BAF14B07D2BDB5C04A902677371044D7921127A19B080ECA9CA73F193E601E93'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
---
+++ fontbase/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'fontbase'
+$installerType = 'exe'
+$silentArgs = '--uninstall -s'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\FontBase\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+
+# Delete junk files of github-desktop at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Local\FontBase -recurse
+Remove-Item "$Env:USERPROFILE\AppData\Roaming\FontBase" -recurse
