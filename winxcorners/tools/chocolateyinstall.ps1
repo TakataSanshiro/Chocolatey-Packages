85e54f18e391b9a0274575dd3d7934994db18a79
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-01 01:44:57 +0900

    add winxcorners

---
+++ winxcorners/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'winxcorners'
+$url = 'https://doc-0o-a8-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/4psmcitjkbhtoe6v890g30jtfi9n669c/1498838400000/12606421919403606798/*/0B_iiJl28uix-ZlluRFZDVnFzRVU?e=download' 
+$checksum = '5D2ECA3BE0CDA7C8DADF18D465C7E7B99B0EDE7979DBE65AF31CF9CE9A99BFEB'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\WinXcorners.lnk" `
+						     -TargetPath "$toolsDir\WinXCorners.exe"
---
+++ winxcorners/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "winxcorners" `
+  -ZipFileName "WinXCorners*" 
+
+Remove-item -path "$Home\Desktop\WinXcorners.lnk" -Force -ErrorAction 'SilentlyContinue'
