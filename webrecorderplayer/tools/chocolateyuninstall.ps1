e47501c2cd6473ad8bce99b9ca70e191c8fe2c54
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-30 17:15:13 +0900

    Remove Uninstall-ChocolateyZipPackage command, so it's not needed

--- webrecorderplayer/tools/chocolateyuninstall.ps1
+++ webrecorderplayer/tools/chocolateyuninstall.ps1
@@ -1,7 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-Uninstall-ChocolateyZipPackage `
-  -PackageName "webrecorderplayer" `
-  -ZipFileName "webrecorderplayer*" 
-
 Remove-item -path "$Home\Desktop\Webrecorder Player.lnk" -Force -ErrorAction 'SilentlyContinue'
