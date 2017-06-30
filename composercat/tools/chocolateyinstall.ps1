6e43b422912d3f7f625dd7fe87abee61d3ac3783
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-30 19:34:50 +0900

    add composercat

---
+++ composercat/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'composercat'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://downloads.getcomposercat.com/composercat/Composercat%20Setup%200.3.2.exe'
+$checksum = 'A35EF2F776BC0975B7F932187915504A3A49EB303C4ACB416ABE536437426DED'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
---
+++ composercat/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'composercat'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\Programs\composercat\Uninstall Composercat.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Composercat at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Roaming\Composercat -recurse
