701af6b44b32781ea91b3a0c027f26e9d96ecfde
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-30 20:08:24 +0900

    add datapixels

---
+++ datapixels/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'datapixels'
+$url = 'https://github.com/gmattie/Data-Pixels/releases/download/1.0.0/data-pixels-playground-win-x64.zip' 
+$checksum = '0A8C0BDCFF5BFBE3731ADBC129F9FF230067156539AB3F6BBAEAF3D25671ED71'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\Data Pixels Playground.lnk" `
+						     -TargetPath "$toolsDir\Data Pixels Playground\Data Pixels Playground.exe"
---
+++ datapixels/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "datapixels" `
+  -ZipFileName "data-pixels*" 
+
+Remove-item -path "$Home\Desktop\Data Pixels Playground.lnk" -Force -ErrorAction 'SilentlyContinue'
