df9751e920ec5ef6ac57f65bdab08928493d75d3
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-06-30 19:13:50 +0900

    add elements

---
+++ elements/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'elements'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/FlorianFe/Elements/releases/download/v1.1.1/Elements.exe'
+$checksum = '0250D52B4FEACDED8712F2BF86976F0D388A3B5A32BAB7933FA4A354500FA542'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -ValidExitCodes "$validExitCodes"
---
+++ elements/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'elements'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\Programs\Elements\Uninstall Elements.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+ 
+# Delete junk files of Elements at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Roaming\Elements -recurse
