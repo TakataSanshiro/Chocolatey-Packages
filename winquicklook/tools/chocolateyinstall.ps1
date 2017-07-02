036ce11e9820e7e14a9d831791c545e7d3598e52
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-02 20:54:31 +0900

    add winquicklook

---
+++ winquicklook/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'winquicklook'
+$url = 'https://github.com/shibayan/WinQuickLook/releases/download/v1.1.31/WinQuickLook.zip' 
+$checksum = '5A0DEC8CE886A262FE71EEAAB0CF26C47A11643C8CA85876D238F1937A060C46'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# Create an exe shotcut at the desktop.
+Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\WinQuickLook.lnk" `
+						     -TargetPath "$toolsDir\WinQuickLook.exe"
---
+++ winquicklook/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Uninstall-ChocolateyZipPackage `
+  -PackageName "winquicklook" `
+  -ZipFileName "WinQuickLook*" 
+
+Remove-item -path "$Home\Desktop\WinQuickLook.lnk" -Force -ErrorAction 'SilentlyContinue'
