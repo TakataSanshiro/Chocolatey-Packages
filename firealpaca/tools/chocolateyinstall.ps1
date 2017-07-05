cd83e9a71affd61e4b17338f166a640baa74f342
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-06 01:45:01 +0900

    add firealpaca

---
+++ firealpaca/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'firealpaca' 
+$installerType = 'exe' 
+$url = 'http://115.146.50.91/firealpaca/bin/FireAlpaca_setup.exe' 
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 1B2249048F26FE9D9A3554102EFBF386782A983161731EA50D7D56A493B8EFB6 -ChecksumType sha256
---
+++ firealpaca/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'firealpaca' 
+$installerType = 'exe' 
+$silentArgs = '/SILENT' 
+$validExitCodes = @(0) 
+$file = "${env:ProgramFiles(x86)}\FireAlpaca\FireAlpaca15\unins000.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
