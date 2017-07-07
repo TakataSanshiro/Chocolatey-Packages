6d8ab8a36b03c4ec4ca3ac6add0c99b9b4c2f083
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-08 02:57:27 +0900

    add insomnia-rest-api-client

---
+++ insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'insomnia-rest-api-client'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/getinsomnia/insomnia/releases/download/v5.5.2/Insomnia.Setup.5.5.2.exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 7F631417661185957D3176CFC4D1A8EF8669FD7DDA03674B234376C400507623 -ChecksumType sha256
