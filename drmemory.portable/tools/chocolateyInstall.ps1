3880ac833901e3cddcc93de3cd6edbdde99d2c7b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-10 01:46:31 +0900

    handover drmemory.portable from narnaud
    https://github.com/narnaud/chocolatey-packages/tree/master/unmaintained-packages/drmemory.portable

---
+++ drmemory.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'drmemory.portable'
+$url = 'https://github.com/DynamoRIO/drmemory/releases/download/release_1.11.0/DrMemory-Windows-1.11.0-2.zip' 
+$checksum = 'BAB2AE2A59238DE29B6B12E0CAC9F1F9B97328F9DE2CE5DA76D397DBBB2B8872'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
