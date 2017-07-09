ea387c7e8bb10adcaf598f6dd836bdb28d41497e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-10 01:45:35 +0900

    handover drmemory.install from narnaud
    https://github.com/narnaud/chocolatey-packages/tree/master/unmaintained-packages/drmemory.install

---
+++ drmemory.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'drmemory.install'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/DynamoRIO/drmemory/releases/download/release_1.11.0/DrMemory-Windows-1.11.0-2.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  url           = $url
+  silentArgs  = "/qn"
+  softwareName  = 'drmemory*'
+  checksum      = '4A611745335CBB92A11238F7745DA3F4C484F9CFE541D6AC01B98F5C78CBAD1C'
+  checksumType  = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
