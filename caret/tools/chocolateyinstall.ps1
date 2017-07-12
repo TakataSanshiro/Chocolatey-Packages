6f30b5baf1f735bc9b0667403bc15c1f4d636d13
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-13 00:07:39 +0900

    update to 3.2.1

--- caret/tools/chocolateyinstall.ps1
+++ caret/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'caret'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/careteditor/caret/releases/download/3.1.0/CaretSetup.exe'
-$checksum = '26CE9C0539CFC2C516CCBC1531CA528C3539E604D040CB9E72C6E35B2C85BB3E'
+$url = 'https://github.com/careteditor/caret/releases/download/3.2.1/CaretSetup.exe'
+$checksum = '5E47203CFCA1F53F1A9925E48BD403DF3D116C5C6A7553941DAC033AB18FE215'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
