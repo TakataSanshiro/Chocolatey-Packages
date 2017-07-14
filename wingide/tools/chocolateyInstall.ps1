0de3d70103d4b985931e72a1d5f6df5748ecb650
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-15 02:46:49 +0900

    upload to 6.0.6.1

--- wingide/tools/chocolateyInstall.ps1
+++ wingide/tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
-﻿$packageName = 'wingide' 
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'wingide' 
 $installerType = 'exe' 
-$url = 'http://wingware.com/pub/wingide/6.0.3/wingide-6.0.3-1.exe' 
+$url = 'http://wingware.com/pub/wingide/6.0.6/wingide-6.0.6-1.exe' 
 $silentArgs = '/VERYSILENT' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 6756649FBF3BC7A8D98C45ADC840778B684CE26549F981F43184E67A474DC90C -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum D5E018E28FA71A4C97F510CD3CB06F1E75951B652239AD7CF7B671CE16D32ED1 -ChecksumType sha256
