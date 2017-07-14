1630d7abe7aa6243dfcbfc0a15369979ce38b3a1
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-15 03:25:21 +0900

    update to 1.0.2459.0

--- opera-neon/tools/chocolateyInstall.ps1
+++ opera-neon/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'opera-neon'
 $installerType = 'exe'
-$url = 'https://get.geo.opera.com/.private/Opera_Moment_Developer_1.0.2459.0_Autoupdate.exe' 
+$url = 'https://get.geo.opera.com/.private/Opera_Moment_Developer_1.0.2531.0_Autoupdate.exe' 
 $silentArgs = '/Q'
 $validExitCodes = @(0, 3010, 1641)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 564AB174FCA1E72AF9D606E1701916709D3B66FBB5DA62F8BF1603D33E8D810E -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 76D998A47C34E89447EB6CA177EA3D563ABBF3C1153184F7C47923D0249ABD00 -ChecksumType sha256
