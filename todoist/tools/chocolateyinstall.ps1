a75ae0d362998267eaea4699f11c0291bae2056b
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-18 22:39:20 +0900

    handover from narnaud

---
+++ todoist/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'todoist'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+  softwareName  = 'Todoist'
+}
+
+Install-ChocolateyPackage @packageArgs
