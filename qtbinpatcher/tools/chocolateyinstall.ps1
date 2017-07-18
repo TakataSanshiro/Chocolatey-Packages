1d82e32896af32e86c20d2bf524a5c7729f1f754
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-18 22:33:49 +0900

    deprecated qtbinpatcher

---
+++ qtbinpatcher/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'qtbinpatcher'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+$url64      = '{{DownloadUrlx64}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  url64bit      = $url64
+}
+
+Install-ChocolateyZipPackage @packageArgs
