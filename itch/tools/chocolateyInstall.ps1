e92524f48c1b16638583e7a5311a9ce1adcb2cad
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-18 21:39:32 +0900

    update to 23.4.2

--- itch/tools/chocolateyInstall.ps1
+++ itch/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'itch' 
 $installerType = 'exe' 
-$url = 'https://github.com/itchio/itch/releases/download/v23.4.1/itchSetup.exe' 
+$url = 'https://github.com/itchio/itch/releases/download/v23.4.2/itchSetup.exe' 
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum D0F6C200F90E399801796B8B2FFA69B8CB94415D167046C1D6CE4401FBB64A85 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum DE75B0D17FF2BF8593846309460D946BADB7B7DF56E02BF15CC8A4F3F401B3C2 -ChecksumType sha256
