4ccc2b85d91695d91c05cee8cfd1246170ed6d60
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-18 22:15:17 +0900

    added instantbird

---
+++ instantbird/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'instantbird'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://instantbird.com/downloads/1.5/instantbird-1.5.en-US.win32.installer.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  silentArgs   = '/S'
+  softwareName  = 'Instantbird*'
+}
+
+Install-ChocolateyPackage @packageArgs
