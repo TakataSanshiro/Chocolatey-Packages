aff6aaa1e7f133975ff4e668588df43dbe43a7ed
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-18 22:58:36 +0900

    added checksum

--- todoist/tools/chocolateyinstall.ps1
+++ todoist/tools/chocolateyinstall.ps1
@@ -2,17 +2,17 @@
 
 $packageName= 'todoist'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '{{DownloadUrl}}'
+$url        = 'https://dateist.todoist.com.s3.amazonaws.com/Todoist_for_Windows_Desktop_2_7_6.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-
   softwareName  = 'Todoist'
+  checksum      = 'E207E8ABFE3D134E8BD3BE448A50CB02E7B091CFAFC0AB68E254D478684DF1F8'
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
