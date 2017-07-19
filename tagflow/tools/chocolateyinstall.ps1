0b4261bb0f029a456034a3747d6c8804d90c17e6
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-20 00:42:59 +0900

    added tagflow

---
+++ tagflow/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'tagflow' 
+$installerType = 'exe' 
+$url = 'https://tagflow-app-auto-update.herokuapp.com/download/win/' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum DD909BEAA81C828CC7364D123F6B537B37CF2F7A00D7948A14FC65331352A14B -ChecksumType sha256
