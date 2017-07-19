a3c3cc67da85a730c8b34b0a388178c463cdf61a
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-20 00:21:16 +0900

    added flow

---
+++ flow/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'flow' 
+$installerType = 'exe' 
+$url = 'https://cdn.getflow.com/win32_ia32/production/release/Flow.zip' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+$scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$exe = "$scriptDir\Flow Setup 6.0.9-ia32.exe"
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$scriptDir" -Checksum CA886CE236F3229750E0966E31A6A23520762861D438AE6BA30EB40C75723750 -ChecksumType sha256
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$exe"
---
+++ flow/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'flow' 
+$installerType = 'exe' 
+$silentArgs = '--uninstall -s' 
+$validExitCodes = @(0) 
+$file = "$Env:USERPROFILE\AppData\Local\Flow\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+
+# Delete junk files of Flow at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Local\Flow -recurse
+Remove-Item $Env:USERPROFILE\AppData\Roaming\Flow -recurse
