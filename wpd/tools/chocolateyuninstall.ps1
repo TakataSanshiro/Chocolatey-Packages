9559c9ef795aaca4917457009a89e3dd9f8e6d42
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-20 02:27:29 +0900

    This script is not required.
    TIP: Since you're only extracting the archive into the package tools directory, you don't need to call Uninstall-ChocolateyZipPackage.

--- wpd/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "wpd" `
-  -ZipFileName "WPD*" 
-
-Remove-item -path "$Home\Desktop\WPD.lnk" -Force -ErrorAction 'SilentlyContinue'
