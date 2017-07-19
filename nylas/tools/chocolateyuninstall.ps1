f4e3b2d3fb845ca57f537fb03a65ccacddcdc012
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-20 01:10:23 +0900

    added nylas

---
+++ nylas/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'nylas' 
+$installerType = 'exe' 
+$silentArgs = '--uninstall -s' 
+$validExitCodes = @(0) 
+$file = "$Env:USERPROFILE\AppData\Local\NylasMail\Update.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
+
+# Delete junk files of NylasMail at $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Local\NylasMail -recurse
+Remove-Item $Env:USERPROFILE\AppData\Roaming\NylasMail -recurse
