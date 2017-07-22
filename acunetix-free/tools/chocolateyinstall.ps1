c0a7d56b685962572bd3ed3505455c3eb1e100ec
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-23 02:45:13 +0900

    added acunetix-free

---
+++ acunetix-free/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'acunetix-free'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://s3.amazonaws.com/a280ccaaf904330a389db759e6275285/acunetix_manual_tools_setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  checksum      = '3456B7EBC26AD8362BEF81313A96C32B5D90E12040B58BF9645FAE51CF41D210'
+  checksumType  = 'sha256'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ acunetix-free/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'acunetix-free' 
+$installerType = 'exe' 
+$silentArgs = '/SILENT' 
+$validExitCodes = @(0) 
+$file = "${env:ProgramFiles(x86)}\Acunetix Manual Tools\unins000.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
