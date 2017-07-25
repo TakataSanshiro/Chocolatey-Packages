914d76233cee9ec30dd3196474a10409a1936aa8
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-25 20:18:29 +0900

    update to 3.2.3.

--- ramme/tools/chocolateyinstall.ps1
+++ ramme/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'ramme'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/terkelg/ramme/releases/download/v3.2.2/Ramme-Setup-3.2.2.exe'
+$url = 'https://github.com/terkelg/ramme/releases/download/v3.2.3/Ramme-Setup-3.2.3.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 9C4CAA7985BB2982FCD48814B12521A239B56B4D87A67466502FC179E4A5B575 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 707DD10D9D2C825FF353EC76418C18EEEB51C9D0776BD601C97F7772F8ACA8E7 -ChecksumType sha256
