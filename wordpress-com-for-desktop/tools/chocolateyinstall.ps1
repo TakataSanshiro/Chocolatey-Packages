7da9cdcca9b7af5fbeaa18588c072e82491e4b53
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-25 20:42:03 +0900

    update to 2.7.1

--- wordpress-com-for-desktop/tools/chocolateyinstall.ps1
+++ wordpress-com-for-desktop/tools/chocolateyinstall.ps1
@@ -7,4 +7,4 @@ $url = 'https://public-api.wordpress.com/rest/v1.1/desktop/windows/download'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum D5AC4466D2D4D395788E21D1FADBD3200727FC262202306646B3E80F2B368F3F -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum DD085637FCD6B369501E38BEC59EE1FD326061BAAEF90895ABE53107FC9CD1F5 -ChecksumType sha256
