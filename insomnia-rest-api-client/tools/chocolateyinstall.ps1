05565db033a94269363c0efe8fd59b687179192e
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-25 21:51:51 +0900

    update to 5.6.3

--- insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'insomnia-rest-api-client'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/getinsomnia/insomnia/releases/download/v5.5.2/Insomnia.Setup.5.5.2.exe'
+$url = 'https://github.com/getinsomnia/insomnia/releases/download/v5.6.3/Insomnia.Setup.5.6.3.exe'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 7F631417661185957D3176CFC4D1A8EF8669FD7DDA03674B234376C400507623 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 2DC79797C21417D36428E8414ED7378AC1F80F3F32FD0DC5C5A6FA726BF1134D -ChecksumType sha256
