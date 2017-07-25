8dc0112169a0e03e5bf07fcb98cff05352c6346b
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-25 20:51:43 +0900

    update to 1.1.2

--- elements/tools/chocolateyinstall.ps1
+++ elements/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'elements'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/FlorianFe/Elements/releases/download/v1.1.1/Elements.exe'
-$checksum = '0250D52B4FEACDED8712F2BF86976F0D388A3B5A32BAB7933FA4A354500FA542'
+$url = 'https://github.com/FlorianFe/Elements/releases/download/v1.1.2/Elements.exe'
+$checksum = 'B271AA26C6EA06B939D4A5E2986B2F1BC6A3661CD62190680FEDB4C351E44C1F'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
