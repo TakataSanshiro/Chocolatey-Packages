cde5446594e516f97f72369ad45af9c3caf954ae
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-09-09 00:03:23 +0900

    update to 1.5.2

--- confide/tools/chocolateyInstall.ps1
+++ confide/tools/chocolateyInstall.ps1
@@ -6,4 +6,4 @@ $url = 'https://getconfide.com/download/windows/dl'
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 8F2152042BBB520A8772DE43C289C441668EFC8B9B2841BBB8EED425816C32DB -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 2E7C6C3FFF30F01D8D16514ADC1DD00C1D0DADCD7EB66F1A1592BC6A18B8EE16 -ChecksumType sha256
