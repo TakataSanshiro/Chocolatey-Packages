c0de5a4d08fbe9540a4e843ae5efe6193f12597a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-10-02 19:59:10 +0900

    update to 2.1.3

--- fontbase/tools/chocolateyinstall.ps1
+++ fontbase/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'fontbase'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'http://releases.fontba.se/win/2.0.2/FontBase%20Setup%202.0.2.exe'
-$checksum = 'BAF14B07D2BDB5C04A902677371044D7921127A19B080ECA9CA73F193E601E93'
+$url = 'http://releases.fontba.se/win/2.1.3/FontBase%20Setup%202.1.3.exe'
+$checksum = 'DE5BAB24031D0C890D60ECCE3C0568D7F263C515B76D5C41408E0D07A51632DE'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
