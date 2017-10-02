ce4f45fdde25c9fc4f357e0b216382a4f6721af6
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-10-02 20:13:17 +0900

    update to 1.6.0

--- textify/tools/chocolateyInstall.ps1
+++ textify/tools/chocolateyInstall.ps1
@@ -4,4 +4,4 @@ $url = 'http://rammichael.com/downloads/textify_setup.exe'
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum D3F93A4E740D5AFF453EDA2CEF71041FDC391860F1755FFE169EA2F443498CF9 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum ADCF10CFB77099FD5244C53959A15A548E3872D7FEF216781AB543C06CC34303 -ChecksumType sha256
