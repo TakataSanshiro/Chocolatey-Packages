486efe4bb395252113247109fd13e2ad3e9b792f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-10-03 03:11:33 +0900

    added pycharm

---
+++ pycharm/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'pycharm'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://download.jetbrains.com/python/pycharm-professional-2017.2.3.exe' 
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 9ce635b5bb8de851d5613a79d3724b11018e71953f9dbcc1f8aecbcb8d019c52 -ChecksumType sha256
