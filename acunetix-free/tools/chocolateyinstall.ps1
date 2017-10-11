9ba855dc32b2924bad7d9f58072695690ef6858e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-10-12 02:21:50 +0900

    fixed the checksum and removed a junk file

--- acunetix-free/tools/chocolateyinstall.ps1
+++ acunetix-free/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
-  checksum      = '3456B7EBC26AD8362BEF81313A96C32B5D90E12040B58BF9645FAE51CF41D210'
+  checksum      = '68FE10818C055DAEF06D6F6D65D554A20D9F15BCE7F4B956CC3EFBBE94F85EF2'
   checksumType  = 'sha256'
   validExitCodes = @(0)
 }
