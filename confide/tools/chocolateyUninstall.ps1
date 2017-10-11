aee6e59f363a33b525be0cd7d6aa8d32649c2c28
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-10-12 04:26:43 +0900

    added a test for whether the uninstall file exists

--- confide/tools/chocolateyUninstall.ps1
+++ confide/tools/chocolateyUninstall.ps1
@@ -5,13 +5,22 @@ $installerType = 'exe'
 $silentArgs = '--uninstall -s' 
 $validExitCodes = @(0) 
 $file = "$Env:USERPROFILE\AppData\Local\Confide\Update.exe"
+$userprofile = "$Env:USERPROFILE\AppData\Local\Confide"
 
-Uninstall-ChocolateyPackage `
-  -PackageName $packageName `
-  -FileType $installerType `
-  -SilentArgs "$silentArgs" `
-  -ValidExitCodes $validExitCodes `
-  -File "$file"
- 
-# Delete junk files of Confide at $Env:USERPROFILE\AppData\Local\Confide
-Remove-Item $Env:USERPROFILE\AppData\Local\Confide -recurse
+if(Test-Path "$file"){
+  Uninstall-ChocolateyPackage `
+    -PackageName $packageName `
+    -FileType $installerType `
+    -SilentArgs "$silentArgs" `
+    -ValidExitCodes $validExitCodes `
+    -File "$file"
+}else{
+  Write-Host "The uninstall script doesn't exist, the software has already been uninstalled." -foreground "magenta"
+}
+
+if(Test-Path "$userprofile"){
+  # Delete junk files of Confide at $Env:USERPROFILE\AppData\Local\Confide
+  Remove-Item  $userprofile -recurse
+}else{
+  Write-Host "The userprofile data doesn't exist, the software has already been uninstalled." -foreground "magenta"
+}
