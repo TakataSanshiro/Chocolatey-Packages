22a9a474615a6d584cba8f1289248a93175f7b0e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-10-18 04:12:59 +0900

    update one to 1.1.0
    changed one from zip type to installer type.

--- disable-nvidia-telemetry/tools/chocolateyinstall.ps1
+++ disable-nvidia-telemetry/tools/chocolateyinstall.ps1
@@ -1,17 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'disable-nvidia-telemetry'
-$url = 'https://github.com/NateShoffner/Disable-Nvidia-Telemetry/releases/download/1.0/Disable.Nvidia.Telemetry.v1.0.zip' 
-$checksum = 'F30ECEEA7BBF4F1F68A7185C1C80D2A2D32697E77C732B8E66846CCDA87B2285'
-$checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://github.com/NateShoffner/Disable-Nvidia-Telemetry/releases/download/1.1/Disable.Nvidia.Telemetry.1.1.0.16.Setup.exe' 
+$checksum = 'BEC60010867B5759CF35FF6F4064F9523E11ED78DFCC41DECFE1B78B7285DC9D'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType"
-
-# Create an exe shotcut at the desktop.
-Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\Disable Nvidia Telemetry.lnk" `
-						     -TargetPath "$toolsDir\Disable Nvidia Telemetry.exe"							 
