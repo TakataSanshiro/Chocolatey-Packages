b3e0596474c61b347a1fe2e92a22f99bbf1de6b9
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-03 04:41:27 +0900

    update one to 0.6.7
    and just fixed the markdown description

--- devdocs-app/tools/chocolateyinstall.ps1
+++ devdocs-app/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'devdocs-app'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/egoist/devdocs-desktop/releases/download/v0.6.6/DevDocs-Setup-0.6.6.exe'
-$checksum = '6E480084D9DEA8274B0890F92796EA8287231B366505B432C3DBCA440A719629'
+$url = 'https://github.com/egoist/devdocs-desktop/releases/download/v0.6.7/DevDocs-Setup-0.6.7.exe'
+$checksum = 'C2C08D3FCBDB18E61973F333C236522D9A78170AB7CA0F03AF29AC25B953BB33'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
