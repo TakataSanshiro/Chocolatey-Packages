e53d50a26de5b52c32430be0ae9a49fd55a1b5fe
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-03 04:56:00 +0900

    update one to 0.8.16

--- boostnote/tools/chocolateyInstall.ps1
+++ boostnote/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'boostnote'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/BoostIO/boost-releases/releases/download/v0.8.15/Boostnote-win.exe' 
+$url = 'https://github.com/BoostIO/boost-releases/releases/download/v0.8.16/Boostnote-win.exe' 
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 01823505F78034F961998C12E4B20128826731F1F8145F35DA3A9518CB64CFF1 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum A336C63BB2C1EB7ADD297C0AF0AA1303C233E7E9532C0FF2852B80B8C9053D0E -ChecksumType sha256
