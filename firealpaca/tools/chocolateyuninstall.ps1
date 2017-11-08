bc5a0c82e0ea9719afc1b84ae586605417595c5e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-09 03:29:45 +0900

    added a test for whether the uninstall file exists

--- firealpaca/tools/chocolateyuninstall.ps1
+++ firealpaca/tools/chocolateyuninstall.ps1
@@ -5,10 +5,14 @@ $installerType = 'exe'
 $silentArgs = '/SILENT' 
 $validExitCodes = @(0) 
 $file = "${env:ProgramFiles(x86)}\FireAlpaca\FireAlpaca15\unins000.exe"
-
-Uninstall-ChocolateyPackage `
-  -PackageName $packageName `
-  -FileType $installerType `
-  -SilentArgs "$silentArgs" `
-  -ValidExitCodes $validExitCodes `
-  -File "$file"
+  
+if(Test-Path "$file"){
+  Uninstall-ChocolateyPackage `
+    -PackageName $packageName `
+    -FileType $installerType `
+    -SilentArgs "$silentArgs" `
+    -ValidExitCodes $validExitCodes `
+    -File "$file"
+}else{
+  Write-Host "The uninstall script doesn't exist, the software has already been uninstalled." -foreground "magenta"
+}
