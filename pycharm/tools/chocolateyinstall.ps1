2342596f1703709162bf63a1d1b5efe4f29c228f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-09 02:19:33 +0900

    updated one to 2017.2.4

--- pycharm/tools/chocolateyinstall.ps1
+++ pycharm/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'pycharm'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://download.jetbrains.com/python/pycharm-professional-2017.2.3.exe' 
+$url = 'https://download.jetbrains.com/python/pycharm-professional-2017.2.4.exe' 
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 9ce635b5bb8de851d5613a79d3724b11018e71953f9dbcc1f8aecbcb8d019c52 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 0e7ba9ee642f751167e4a34de68c445dea8807ace56a5f54c94f4e07cddb1961 -ChecksumType sha256
