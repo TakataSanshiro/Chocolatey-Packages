67bf17545809fd47c54c5e753e6e7278a6ecad67
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-09 03:06:49 +0900

    updated one to 1.15

--- uninstallview/tools/chocolateyinstall.ps1
+++ uninstallview/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'uninstallview'
 $url = 'https://www.nirsoft.net/utils/uninstallview.zip' 
 $url64 = 'https://www.nirsoft.net/utils/uninstallview-x64.zip' 
-$checksum = '02A591AF7F94BFA43E5CDE2B9E4ADA0434F1FB05B5C28654A089E9BF1E9761BE'
-$checksum64 = '40E9FB2BA031DF8C32B9D4A509B2AA46A0FA80A383A9738A5923CB5ABE0880C6'
+$checksum = 'E403BB08005221D4DACFD513C1A897431C26FD460988AB68B2BE4387B6F2DE14'
+$checksum64 = '9ABD6ECEFFE0D5DE72F2AC264F79A715E8327BB19FFFB0AF2B3D02BBDB36E151'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
