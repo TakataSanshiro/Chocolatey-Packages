dd17b24bd67ab0f79b3660bdf0979b4db0f22f27
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-09 02:23:17 +0900

    updated one to 6.0.11

--- flow/tools/chocolateyinstall.ps1
+++ flow/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $url = 'https://cdn.getflow.com/win32_ia32/production/release/Flow.zip'
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$exe = "$scriptDir\Flow Setup 6.0.9-ia32.exe"
+$exe = "$scriptDir\Flow Setup 6.0.11-ia32.exe"
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$scriptDir" -Checksum CA886CE236F3229750E0966E31A6A23520762861D438AE6BA30EB40C75723750 -ChecksumType sha256
+Install-ChocolateyZipPackage "$packageName" "$url" "$scriptDir" -Checksum 2F6B9214823F1406AE307C09E7C060D6FC0D8D2FCC9C938754C4C73C58ECE47E -ChecksumType sha256
 Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$exe"
