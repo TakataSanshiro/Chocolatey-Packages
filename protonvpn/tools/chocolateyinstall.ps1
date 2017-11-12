f556830a43cd0aef2b9b1613f2bcd3e170c602be
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-13 04:53:23 +0900

    added new protonvpn package
    But, installing code via AutoHotkey is NOT completely.

---
+++ protonvpn/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'protonvpn'
+$installerType  = 'exe'
+$url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.2.4.exe'
+$checksum       = 'D23A8ACE6D2D2EFB2938A5EA7DC3D5E387AE9DCF5179EDC4E06A3DAEA23DDFFD'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$silentArgs     = ''
+$validExitCodes = @(0)
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "protonvpnInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'ProtonVPN*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
