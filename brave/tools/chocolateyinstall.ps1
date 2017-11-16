550085c65fa09bd79db166ba5b9435bbd6c79d47
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-17 03:39:47 +0900

    updated one to 0.19.95

--- brave/tools/chocolateyinstall.ps1
+++ brave/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageName = 'brave'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/brave/browser-laptop/releases/download/v0.19.80dev/BraveSetup-ia32.exe'
-$checksum = 'F711BFE48B74B754AB6BB35CE5E549078234D8E4F5CD217357C3E8E8A1AB8393'
+$url = 'https://github.com/brave/browser-laptop/releases/download/v0.19.95dev/BraveSetup-ia32.exe'
+$checksum = '3F12A76A8BAA3ED2D3969278EC170FC2EE22F3C56BEA7DCD29F9713C3CDD9788'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/brave/browser-laptop/releases/download/v0.19.80dev/BraveSetup-x64.exe'
-$checksum64 = '416E71E57DE000F0BA287EB2F938B947BB1778BF38D8C58039CF7D11FCCA0EED'
+$url64 = 'https://github.com/brave/browser-laptop/releases/download/v0.19.95dev/BraveSetup-x64.exe'
+$checksum64 = 'B6BDF88D2F56E5B9E5E1C0C9D0F30D5DB48ACFA5E98CECCFD814F9470B08F4F5'
 $checksumType64 = 'sha256'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0) 
