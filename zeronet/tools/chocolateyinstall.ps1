77bafd20c45a5a6cb3fa0b898167c73bce7913c0
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-17 03:59:57 +0900

    initial commit zeronet

---
+++ zeronet/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'zeronet'
+$url = 'https://github.com/HelloZeroNet/ZeroNet-win/archive/dist/ZeroNet-win.zip' 
+$checksum = 'CFE885B6A0D233B628475915AB97BD85D85AF81744BF2374986CB9356094E5CC'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
