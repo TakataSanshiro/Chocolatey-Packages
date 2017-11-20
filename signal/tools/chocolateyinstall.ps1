9e19cac61ec867930e8d674c21945d1dc3f4c208
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-11-21 00:40:50 +0900

    updated one to 1.0.38

--- signal/tools/chocolateyinstall.ps1
+++ signal/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'signal'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://updates.signal.org/desktop/Signal-win-1.0.37.exe' 
-$checksum = '79197F9615D14471F8AE7BFF2A6FF2043FBCF5EDC09F3F4AE22A5BB0C8947BD5'
+$url = 'https://updates.signal.org/desktop/Signal-win-1.0.38.exe' 
+$checksum = '175AB3840600ABE9D06817D1A36893E079EC67D2E5B88F20F200C11108A03913'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
