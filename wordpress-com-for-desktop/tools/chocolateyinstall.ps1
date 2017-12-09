980d9493a5c015334b78840fba1e1f7e25da8686
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-10 02:45:05 +0900

    updated one to 3.1.0

--- wordpress-com-for-desktop/tools/chocolateyinstall.ps1
+++ wordpress-com-for-desktop/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'wordpress-com-for-desktop'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/Automattic/wp-desktop/releases/download/v3.0.0/WordPress.com-3.0.0-Setup.exe'
+$url = 'https://github.com/Automattic/wp-desktop/releases/download/v3.1.0/WordPress.com-3.1.0-Setup.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 1F93B1CD0A36590D0838F6F5E9A58C5ABCFE5B4C6861EFB243B6F4B6B4146F17 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum F8882DEE842537425D3E41DB6541A6955507966D2F95E0F344E0652A89DD71AB -ChecksumType sha256
