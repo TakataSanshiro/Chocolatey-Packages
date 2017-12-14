2f77ff60d4c3c86c20643db335fa88bdf1e9a9a3
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-15 04:47:29 +0900

    updated the url and fixed the markdown

--- winxcorners/tools/chocolateyinstall.ps1
+++ winxcorners/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'winxcorners'
-$url = 'https://doc-0o-a8-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/4psmcitjkbhtoe6v890g30jtfi9n669c/1498838400000/12606421919403606798/*/0B_iiJl28uix-ZlluRFZDVnFzRVU?e=download' 
+$url = 'https://doc-0o-a8-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/urctuhrk8vu3n9qspb9009mteupmscov/1513274400000/12606421919403606798/*/0B_iiJl28uix-ZlluRFZDVnFzRVU?e=download' 
 $checksum = '5D2ECA3BE0CDA7C8DADF18D465C7E7B99B0EDE7979DBE65AF31CF9CE9A99BFEB'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
