99610b133d9431e8c4319489b75eecfee1b5e093
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-28 21:01:55 +0900

    added a test for whether the uninstall file exists

--- nylas/tools/chocolateyuninstall.ps1
+++ nylas/tools/chocolateyuninstall.ps1
@@ -6,13 +6,13 @@ $silentArgs = '--uninstall -s'
 $validExitCodes = @(0) 
 $file = "$Env:USERPROFILE\AppData\Local\NylasMail\Update.exe"
 
-Uninstall-ChocolateyPackage `
-  -PackageName $packageName `
-  -FileType $installerType `
-  -SilentArgs "$silentArgs" `
-  -ValidExitCodes $validExitCodes `
-  -File "$file"
-
-# Delete junk files of NylasMail at $Env:USERPROFILE\AppData
-Remove-Item $Env:USERPROFILE\AppData\Local\NylasMail -recurse
-Remove-Item $Env:USERPROFILE\AppData\Roaming\NylasMail -recurse
+if(Test-Path "$file"){
+  Uninstall-ChocolateyPackage `
+    -PackageName $packageName `
+    -FileType $installerType `
+    -SilentArgs "$silentArgs" `
+    -ValidExitCodes $validExitCodes `
+    -File "$file"
+}else{
+  Write-Host "The uninstall script doesn't exist, the software has already been uninstalled." -foreground "magenta"
+}
