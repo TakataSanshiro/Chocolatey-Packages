2e4fcc75fa9a3abd0677c8c005d8c821f9e2fc4c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-28 16:39:16 +0900

    updated one to 1.1.3

--- elements/tools/chocolateyinstall.ps1
+++ elements/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'elements'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/FlorianFe/Elements/releases/download/v1.1.2/Elements.exe'
-$checksum = 'B271AA26C6EA06B939D4A5E2986B2F1BC6A3661CD62190680FEDB4C351E44C1F'
+$url = 'https://github.com/FlorianFe/Elements/releases/download/v1.1.3/Elements.exe'
+$checksum = 'DA94EB9C1A6C2244B5373BFB5CBD39863D35566D52EB48197719A8B51A728D09'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
