21eb49546f59de21a85be636530b57cb0038b060
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-28 16:13:18 +0900

    updated one to 1.07

--- networkusageview/tools/chocolateyinstall.ps1
+++ networkusageview/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'networkusageview'
 $url = 'https://www.nirsoft.net/utils/networkusageview.zip' 
 $url64 = 'https://www.nirsoft.net/utils/networkusageview-x64.zip' 
-$checksum = 'CD02930A8F519261B845709704D7FB47A77218C636C4FE97942B1060C815C9E2'
-$checksum64 = 'E9A0C8D0A9BB758936B0693D12144E7022C9241CBB9CA8DFD235E25383A2CBC3'
+$checksum = 'E8B017FA4078E853725944B822EFF09867DA7E5E61FA2641AAD52046A43249CB'
+$checksum64 = '4EC6D75F30F5CB11CEE4CEE6862E4B505F5EDBD0BB492F8092306BEC6D158C0D'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
