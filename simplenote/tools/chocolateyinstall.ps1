b69ff9abdbb936eaafb23faea58157b6a63cc36f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-28 15:38:22 +0900

    updated one to 1.1.1

--- simplenote/tools/chocolateyinstall.ps1
+++ simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'simplenote'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.1.0/Simplenote-windows-1.1.0.exe'
+$url = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.1.1/Simplenote-windows-1.1.1.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum FD8B8F883D78DC4E4F99B2548CF44DB304A058E80901EF392830A45B2D78F8DC -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 264F6CDC1DBE4E3C046E62AA531D6EEA9E6DA2303930B61DB022EF8144ED4F7B -ChecksumType sha256
