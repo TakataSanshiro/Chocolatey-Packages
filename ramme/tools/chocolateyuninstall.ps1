250ec79d470888f5f50580ac132759cab24adc6a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-28 20:59:17 +0900

    added a test for whether the uninstall file exists

---
+++ ramme/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'ramme'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "$Env:USERPROFILE\AppData\Local\Programs\Ramme\Uninstall Ramme.exe"
+
+if(Test-Path "$file"){
+  Uninstall-ChocolateyPackage `
+    -PackageName $packageName `
+    -FileType $installerType `
+    -SilentArgs "$silentArgs" `
+    -ValidExitCodes $validExitCodes `
+    -File "$file"
+}else{
+  Write-Host "The uninstall script doesn't exist, the software has already been uninstalled." -foreground "magenta"
+}
