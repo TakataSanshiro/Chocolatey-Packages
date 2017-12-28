f2c48fbbeef5262ff18421a8ee34ca4cf170ec74
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-28 20:47:30 +0900

    added a test for whether the uninstall file exists

--- elements/tools/chocolateyuninstall.ps1
+++ elements/tools/chocolateyuninstall.ps1
@@ -6,12 +6,16 @@ $silentArgs = '/S'
 $validExitCodes = @(0)
 $file = "$Env:USERPROFILE\AppData\Local\Programs\Elements\Uninstall Elements.exe"
 
-Uninstall-ChocolateyPackage `
-  -PackageName $packageName `
-  -FileType $installerType `
-  -SilentArgs "$silentArgs" `
-  -ValidExitCodes $validExitCodes `
-  -File "$file"
- 
+if(Test-Path "$file"){
+  Uninstall-ChocolateyPackage `
+    -PackageName $packageName `
+    -FileType $installerType `
+    -SilentArgs "$silentArgs" `
+    -ValidExitCodes $validExitCodes `
+    -File "$file"
+}else{
+  Write-Host "The uninstall script doesn't exist, the software has already been uninstalled." -foreground "magenta"
+}
+
 # Delete junk files of Elements at $Env:USERPROFILE\AppData
 Remove-Item $Env:USERPROFILE\AppData\Roaming\Elements -recurse
