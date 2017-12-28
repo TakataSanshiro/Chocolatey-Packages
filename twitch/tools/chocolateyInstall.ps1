272cc18cd0e771c905599dc971983183708778d1
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-28 16:03:56 +0900

    updated one to 6.0.0.20171228

--- twitch/tools/chocolateyInstall.ps1
+++ twitch/tools/chocolateyInstall.ps1
@@ -6,4 +6,4 @@ $url = 'https://updates.twitchapp.net/windows/installer/TwitchSetup.exe'
 $silentArgs = '/silent'
 $validExitCodes = @(0, 1638)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 3ED7AD0D84852A8B2308F243BD66171C1B338CED8A6828757B25566ADD686D3E -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum C9D57E32EACFD5B27342AB0351DB46C655FB8CB69754A2F79EB098F397FA7777 -ChecksumType sha256
