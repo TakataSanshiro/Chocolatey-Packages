190bbae1503e424e294484c27a2fd4392d67c719
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-12-28 16:23:02 +0900

    updated one to 1.0.8

--- webrecorderplayer/tools/chocolateyinstall.ps1
+++ webrecorderplayer/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'webrecorderplayer'
-$url = 'https://github.com/webrecorder/webrecorderplayer-electron/releases/download/v1.0.7/webrecorderplayer-electron-win-x86-1.0.7.exe' 
-$url64 = 'https://github.com/webrecorder/webrecorderplayer-electron/releases/download/v1.0.7/webrecorderplayer-electron-win-x86_64-1.0.7.exe' 
-$checksum = '638048B396A2EFCAA0FC4F1506EB497CAF4C010EB6CF47F3A536915342C23710'
-$checksum64 = 'E5F3F42B4B9D51AA643591EBA2FB9BFF2927B761B831A4DCE81971D619D980B0'
+$url = 'https://github.com/webrecorder/webrecorderplayer-electron/releases/download/v1.0.8/webrecorderplayer-electron-win-x86-1.0.8.exe' 
+$url64 = 'https://github.com/webrecorder/webrecorderplayer-electron/releases/download/v1.0.8/webrecorderplayer-electron-win-x86_64-1.0.8.exe' 
+$checksum = 'E862B19D695F73340F97FA3E52D49F524574FACEC151C9FAC6B673BE34DBB435'
+$checksum64 = '3A01E803997F52FB04EE9261B7AEAAC911AE08F064A1011E61F8E7C2EC170AA0'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
