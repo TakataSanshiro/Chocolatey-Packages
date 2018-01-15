71f7497e57d834e05b20b9eafb4d7e3c75072a4e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-01-16 04:32:12 +0900

    updated to 0.4.2

--- glyphr-studio-desktop/tools/chocolateyinstall.ps1
+++ glyphr-studio-desktop/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $packageName = 'glyphr-studio-desktop'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.1/Glyphr.Studio-win32-ia32.zip'
-$checksum = '7E3ADCF0E5F913127DC80EFD2775179B50E5117119CFD841B0D93E420AE87F73'
+$url = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.2/Glyphr.Studio-win32-ia32.zip'
+$checksum = 'CCE9FB3836578C2947619FD28996FA94199ABBF631B5D69969A2B7B9235CC3E4'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.1/Glyphr.Studio-win32-x64.zip'
-$checksum64 = 'A9F2B5C829910B5F291BD4E8E16937B1D3973AF83476EA59491874C49CFA744A'
+$url64 = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.2/Glyphr.Studio-win32-x64.zip'
+$checksum64 = '8919A92FCAE916419B84EF380D1AC4CDAF8A1D558EC06A991665CDD2E6226CBF'
 $checksumType64 = 'sha256'
 
 $is64bit = Get-ProcessorBits 64
