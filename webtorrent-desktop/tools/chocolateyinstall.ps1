24b108180e1c5821cc6cf21a661b8826bc2a8d2c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-01-29 04:28:12 +0900

    updated to 0.19.0

--- webtorrent-desktop/tools/chocolateyinstall.ps1
+++ webtorrent-desktop/tools/chocolateyinstall.ps1
@@ -3,9 +3,23 @@
 $packageName = 'webtorrent-desktop'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.18.0/WebTorrentSetup-v0.18.0-ia32.exe'
-$url64 = 'https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.18.0/WebTorrentSetup-v0.18.0.exe'
+$url = 'https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.19.0/WebTorrentSetup-v0.19.0-ia32.exe'
+$checksum = 'E1D29517F9E15C593669A632FDB48606727CC1DFC37D9C7ADAF95D810B3BE562'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.19.0/WebTorrentSetup-v0.19.0.exe'
+$checksum64 = '5E682F62D55380C9632C2724DE5BC80D8D101BE5DF35C3CC1F519A167C6F15F4'
+$checksumType64 = 'sha256'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 0B53D6DE17C33C5E87E41D86F3EF8276BE0677B74707E8780594AA6F0BD10FCD -ChecksumType sha256 -checksum64 40F04854CD376AC686689B83E4D6208649493BD20E76E9CDE8CB8700ACEFB1DA -checksumtype64 sha256
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -Url64bit "$url64" `
+  -Checksum64 "$checksum64" `
+  -ChecksumType64 "$checksumType64" `
+  -ValidExitCodes "$validExitCodes"
