dfcdee58948ec3bfbfecd7588f63c76f1ba8fed6
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-02-23 04:18:43 +0900

    initial commit

---
+++ jpegtran/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'jpegtran'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://jpegclub.org/jpegtran.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+
+  checksum      = '8E587EF4DD73AEE34B3764B2B2ACDDFACF6DC0D53EFF2FC9D0BF05A48FA9EF99'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
