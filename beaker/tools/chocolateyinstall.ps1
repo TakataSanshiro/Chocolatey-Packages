8b9ebaffaffe1ba95e4f641beef1ab676899ad49
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2018-02-26 08:48:57 -0800

    updated to 0.7.11

--- beaker/tools/chocolateyinstall.ps1
+++ beaker/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'beaker'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/beakerbrowser/beaker/releases/download/0.7.10/beaker-browser-setup-0.7.10.exe' 
-$checksum = '3EBE0040451198A3653161DF02C7C89E65D2610CBF0C66930F76FAB01EDFE811'
+$url = 'https://github.com/beakerbrowser/beaker/releases/download/0.7.11/beaker-browser-setup-0.7.11.exe' 
+$checksum = '256700612164fbd29b79823ef94e211e41645ec708e65be2b5b04fd3e1c74401'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
