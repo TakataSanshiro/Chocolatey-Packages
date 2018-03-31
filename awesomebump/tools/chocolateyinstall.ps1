704471dd279ed0eb483cb5ceebc096b0983a9c55
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-04-01 03:45:00 +0900

    init

---
+++ awesomebump/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'awesomebump'
+$url = 'https://github.com/kmkolasinski/AwesomeBump/releases/download/Winx32v5.1/AwesomeBumpV5.1Bin32x64Win7.zip' 
+$checksum = '9AA2C80E1087871B31F6EC26ADFE8CB5D102B05AE147CC7755BF7DDA5118119D'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"					 
