5f1f1d713cbe431e791b725ffb16a0aa3db2e7ef
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-03-31 19:31:27 +0900

    updated to 3.8.25

--- now/tools/chocolateyinstall.ps1
+++ now/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'now' 
 $installerType = 'exe' 
-$url = 'https://github.com/zeit/now-desktop/releases/download/3.8.23/now-desktop-Setup-3.8.23.exe' 
+$url = 'https://github.com/zeit/now-desktop/releases/download/3.8.25/now-desktop-Setup-3.8.25.exe' 
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum EBCEEAB885031FD47A3F5BF09970A9AF6C968676E49487D4A0D4FFD3E5C7ABDD -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum C303B8D7ECE4D4381EB8C338057B401230365690614EC218E37E3FD2DEAED46A -ChecksumType sha256
