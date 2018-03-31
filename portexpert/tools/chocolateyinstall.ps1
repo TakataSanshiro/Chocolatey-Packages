ea7cf25f8573af305a71d8c4aec6498bd0c22a9f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-03-31 17:46:57 +0900

    init

---
+++ portexpert/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'portexpert'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url = 'https://kcsoftwares.com/files/portexpert_lite.exe'
+$checksum = '0C34A6B2044D5CFB9236D0FC9CD9A706D5603DB94B63804C1378A85C236FE58C'
+$checksumType = 'sha256'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -SilentArgs "$silentArgs" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -Url64bit "$url64" `
+  -Checksum64 "$checksum64" `
+  -ChecksumType64 "$checksumType64" `
+  -ValidExitCodes "$validExitCodes"
