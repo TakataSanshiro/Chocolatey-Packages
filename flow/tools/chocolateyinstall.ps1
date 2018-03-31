c0d5d7d26b6b4db0358c08c34954cc338dfec3c0
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-03-31 23:02:05 +0900

    fixed the exe path

--- flow/tools/chocolateyinstall.ps1
+++ flow/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $url = 'https://cdn.getflow.com/win32_ia32/production/release/Flow.zip'
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$exe = "$scriptDir\Flow Setup 6.0.11-ia32.exe"
+$exe = "$scriptDir\Flow Setup 6.0.17-ia32.exe"
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$scriptDir" -Checksum AF3A3CB95B06931ABA1544E1F9A64F2B7D6CD88664416FA70E0FF0A9FC530A12 -ChecksumType sha256
 Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$exe"
