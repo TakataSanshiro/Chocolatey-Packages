e98decb76682237a3b9908483c2f76d5e7c9d84b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-04-04 04:56:38 +0900

    init

---
+++ nordvpn/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'nordvpn'
+$installerType  = 'exe'
+$url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/latest/NordVPNSetup.exe'
+$checksum       = '29F6D5886B4A2093B8FB330832C465822C2E84810B54751445E26D4B089763D2'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$silentArgs     = '/quiet'
+$validExitCodes = @(0)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'ProtonVPN*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
