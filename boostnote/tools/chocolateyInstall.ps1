724098cdb3f77b3e345966f1f53fdad2ba2fce8d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-04-05 04:01:50 +0900

    fixed the $url

--- boostnote/tools/chocolateyInstall.ps1
+++ boostnote/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'boostnote'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github-production-release-asset-2e65be.s3.amazonaws.com/47141292/51734786-2716-11e8-971c-6f21639bba49?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A/20180331/us-east-1/s3/aws4_request&X-Amz-Date=20180331T121035Z&X-Amz-Expires=300&X-Amz-Signature=fc3fb97e2fa4d6feac780735d067d5475d453269adf245115fb328bc6d19c1b6&X-Amz-SignedHeaders=host&actor_id=25909315&response-content-disposition=attachment; filename=Boostnote-win.exe&response-content-type=application/octet-stream' 
+$url = 'https://github.com/BoostIO/boost-releases/releases/download/v0.11.2/Boostnote-win.exe' 
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
