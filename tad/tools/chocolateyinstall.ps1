dc57d5377558faefac848393a6080473faaae67b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-05-15 16:14:49 +0900

    tad init

---
+++ tad/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'tad'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/antonycourtney/tad/releases/download/v0.8.5/Tad.Setup.0.8.5.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  silentArgs  = "/S"
+  softwareName  = '*tad*'
+  checksum      = 'A853ACC1CAB5BFF3D79423A7B99F4C42C773E618FA7653594FFE71DD9370E1EA'
+  checksumType  = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tad/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'tad'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$file = "${env:ProgramFiles}\Tad\Uninstall Tad.exe"
+
+Uninstall-ChocolateyPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes `
+  -File "$file"
