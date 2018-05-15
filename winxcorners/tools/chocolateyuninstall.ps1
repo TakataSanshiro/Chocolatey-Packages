3c2a8b58992cd824fafb252b606f0064730e9012
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-05-15 17:08:27 +0900

    removed no need uninstall scirpt

--- winxcorners/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "winxcorners" `
-  -ZipFileName "WinXCorners*" 
-
-Remove-item -path "$Home\Desktop\WinXcorners.lnk" -Force -ErrorAction 'SilentlyContinue'
