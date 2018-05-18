fe22f4cdfd6d076de2cd9e32e0faebf338892f20
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-05-18 22:58:10 +0900

    updated lepton to 1.6.2

--- lepton/tools/chocolateyinstall.ps1
+++ lepton/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'lepton'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/hackjutsu/Lepton/releases/download/v1.6.1/Lepton-Setup-1.6.1.exe'
-$checksum = '13EF3CC5F94CC8924E8FFD3AD998F095D81A6DC866059F7E7B706F5343A2D53F'
+$url = 'https://github.com/hackjutsu/Lepton/releases/download/v1.6.2/Lepton-Setup-1.6.2.exe'
+$checksum = 'FD6702DAB02083A0B2576D490B3C22C08318F89C4B5D7752FB28CDB24F26E141'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
