f9a235259a0bdcfff1ed1979c64ae09c92f8a9e7
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-05-18 17:35:24 +0900

    updated $url

--- winxcorners/tools/chocolateyinstall.ps1
+++ winxcorners/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'winxcorners'
-$url = 'https://doc-0o-a8-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/30noeh2jkk213kfb6e5kk666644lhnqh/1524751200000/12606421919403606798/*/0B_iiJl28uix-ZlluRFZDVnFzRVU?e=download' 
+$url = 'https://doc-0o-a8-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/81gig9c0vprb9gjn2cqgicq9t9j5rsrk/1526630400000/12606421919403606798/*/0B_iiJl28uix-ZlluRFZDVnFzRVU?e=download' 
 $checksum = '5D2ECA3BE0CDA7C8DADF18D465C7E7B99B0EDE7979DBE65AF31CF9CE9A99BFEB'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
