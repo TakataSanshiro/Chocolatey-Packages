b4e3409c38f04819d805f74f678c1c4759c57957
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-05-18 21:45:06 +0900

    updated postbox to 6.0.8

--- postbox/tools/chocolateyinstall.ps1
+++ postbox/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'postbox'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/download.getpostbox.com/installers/5.0.20/1_9dabbd369c5779984b351fe768d906fec845cb23/postbox-5.0.20-win32.exe'
+$url        = 'https://s3.amazonaws.com/download.getpostbox.com/installers/6.0.8/1_2a08007f8e141ca2b84e20ac7f7ce60203b04054/postbox-6.0.8-win32.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   softwareName  = 'Postbox*'
-  checksum      = '26D749C79CD3704668ED1095A088DAFBC2F393F7DBCB99DFAA51400F28B72015'
+  checksum      = 'F999FBB0FF87E8C2587373A0BA1D07999934819297613BA57AD47113242487C1'
   checksumType  = 'sha256'
   silentArgs   = '/S'
 }
