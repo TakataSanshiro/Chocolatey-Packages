41587b0ad5e573175b04c892cef8a0b38911d9e9
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-05-18 22:16:41 +0900

    updated pycharm to 2018.1.3

--- pycharm/tools/chocolateyinstall.ps1
+++ pycharm/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'pycharm'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://download.jetbrains.com/python/pycharm-professional-2018.1.exe' 
+$url = 'https://download.jetbrains.com/python/pycharm-professional-2018.1.3.exe' 
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 650C4C7EA9CF19C659A7963577C3A78E73F27BFC98B198E5F42B9195F4301799 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum EE9E788F45E98BFCB7B9554657C86748911D2A17AA87A1BE43DF50457B4A9A60 -ChecksumType sha256
