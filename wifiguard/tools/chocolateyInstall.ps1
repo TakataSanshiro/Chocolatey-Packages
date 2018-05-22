e149a71a5fb53c113dfb7e11698d004d3e896f71
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-05-22 19:03:36 +0900

    updated wifiguard to 2.0.2.20180522

--- wifiguard/tools/chocolateyInstall.ps1
+++ wifiguard/tools/chocolateyInstall.ps1
@@ -6,4 +6,4 @@ $url = 'https://www.softperfect.com/download/files/wifiguard_windows_setup.exe'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum BB02618FB21CEAEE30C14B200342971FEDBDA901B7FD11549C94A1D958F3900E -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum FB05D9BE025B9689AAB87282869A5B9A5865A51653CC1413C90F04DD54007FBD -ChecksumType sha256
