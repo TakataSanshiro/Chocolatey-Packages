e1ef577f5c90f7a6493379a9b0dc4d48f38a698e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-05-26 03:25:33 +0900

    updated github-desktop to 1.2.0

--- github-desktop/tools/chocolateyinstall.ps1
+++ github-desktop/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'github-desktop'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://desktop.githubusercontent.com/releases/1.1.1-bf9cffc6/GitHubDesktopSetup.exe'
-$checksum = '6ADFC63AA7027090C79E3670068F1530A70E9AB837C5B9CF39A36B6FB767C8D8'
+$url = 'https://desktop.githubusercontent.com/releases/1.2.0-480dfdb1/GitHubDesktopSetup.exe'
+$checksum = 'B4406FA8F839294CBB53CC4EE8B3D8C36EE12957CB6B45F4CBBA7BACC9EB6D14'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
