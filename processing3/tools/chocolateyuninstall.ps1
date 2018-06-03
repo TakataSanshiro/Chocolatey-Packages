eab632f7ac2fa71cb7ddc7c2352c32d4dcdd28c9
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-06-03 20:07:13 +0900

    moved processing3 to processing

--- processing3/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "processing3" `
-  -ZipFileName "processing*" 
-
-Remove-item -path "$Home\Desktop\Processing3.lnk" -Force -ErrorAction 'SilentlyContinue'
