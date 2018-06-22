5e8a955ef7f4067fdd86f44c44fbeafe523e189c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-06-23 02:22:55 +0900

    removed no need pixelshop uninstall script

--- pixelshop/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "pixelshop" `
-  -ZipFileName "PixelShop.zip" 
-
-Remove-item -path "$Home\Desktop\PixelShop.lnk" -Force -ErrorAction 'SilentlyContinue'
