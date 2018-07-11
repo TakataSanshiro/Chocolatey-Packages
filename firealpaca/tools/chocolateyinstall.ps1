3b4870dcd4f8c167936f849080f5c9c14e05f6fb
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-07-11 17:23:14 +0900

    updated firealpaca to 2.1.5

--- firealpaca/tools/chocolateyinstall.ps1
+++ firealpaca/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'firealpaca' 
 $installerType = 'exe' 
-$url = 'http://115.146.50.91/firealpaca/bin/FireAlpaca64_setup_2_1_3.exe' 
+$url = 'http://115.146.50.91/firealpaca/bin/FireAlpaca64_setup_2_1_5.exe' 
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 0798E3975DF1974725D81F4BD4A544F8CEA76E49E1A5F783116331FAFC749535 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 1D4EC65DE6B1A4228A17D4B36745A56057ED633815533F542ABA712EB842EAD2 -ChecksumType sha256
