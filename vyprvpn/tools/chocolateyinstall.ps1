ea868b510644209575b6a44d6b2fce7e87a3f37e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-07-11 18:51:59 +0900

    vyprvpn init

---
+++ vyprvpn/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'vyprvpn'
+$installerType  = 'exe'
+$url            = 'https://www.goldenfrog.com/downloads/vyprvpn/desktop/windows/production/2.13.1.8166/VyprVPN-2.13.1.8166-installer.exe'
+$checksum       = 'BC865E29575F5E39F0802D1BB2AAEBED2E4D51B4285C0C927B68C660CBBB5E11'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$silentArgs     = '/S'
+$validExitCodes = @(0)
+
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "vyprvpnInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'vyprvpn*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ vyprvpn/tools/vyprvpnInstall.ahk
@@ -0,0 +1,14 @@
+#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+#NoTrayIcon
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+SetTitleMatchMode, 2 ; A window's title can contain WinTitle anywhere inside it to be a match. 
+
+Loop, 10000
+{
+IfWinExist, Windows Security
+  {
+  WinActivate
+  Send {i}
+  }
+}
