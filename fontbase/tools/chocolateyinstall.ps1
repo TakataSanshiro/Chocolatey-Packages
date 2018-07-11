3654d009bdb24d37e40ece24541907d17bef2692
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-07-11 17:22:04 +0900

    updated fontbase to 2.5.3, and removed the uninstall script

--- fontbase/tools/chocolateyinstall.ps1
+++ fontbase/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'fontbase'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://releases.fontba.se/win/FontBase-2.4.1.exe'
-$checksum = '1C1A6E0AA872CED581C1222ADC781D880949988A5DA66A20367522BC4BEF199C'
+$url = 'https://releases.fontba.se/win/FontBase-2.5.3.exe'
+$checksum = '55B280142B39FCAD32936AA417FA328DAC4C46573E351F80B8E34EAA4238B83C'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
