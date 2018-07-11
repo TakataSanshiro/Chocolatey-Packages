16c3aa915b35e951d680e84059809899ca053834
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-07-11 17:15:29 +0900

    updated ramme to 3.2.5, and removed the uninstall script

--- ramme/tools/chocolateyinstall.ps1
+++ ramme/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'ramme'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/terkelg/ramme/releases/download/v3.2.3/Ramme-Setup-3.2.3.exe'
+$url = 'https://github.com/terkelg/ramme/releases/download/v3.2.5/Ramme-Setup-3.2.5.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 707DD10D9D2C825FF353EC76418C18EEEB51C9D0776BD601C97F7772F8ACA8E7 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 39AAAAA20186DD1EE20E55AEF02D00752E68EA1D74268417303515BB33511457 -ChecksumType sha256
