99195c0d2105961fc20cc014ff04c8bbf6c16a26
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-07-11 21:09:11 +0900

    perfect-privacy init

---
+++ perfect-privacy/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'perfect-privacy'
+$installerType  = 'exe'
+$url            = 'https://www.perfect-privacy.com/downloads_public/Perfect-Privacy-VPN_Setup.exe'
+$checksum       = 'B39209648C6715730B53219FD8C912875D55EAAF6197FD28A245C9E296B08706'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$silentArgs     = '/S'
+$validExitCodes = @(0)
+
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "perfect-privacyInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'perfect-privacy*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
