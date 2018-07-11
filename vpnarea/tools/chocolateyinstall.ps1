085c4a691891890ae7b9f680e449e779edab7d82
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-07-11 19:56:24 +0900

    vpnarea init

---
+++ vpnarea/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'vpnarea'
+$installerType  = 'exe'
+$url            = 'https://vpnarea.com/VPNArea.exe'
+$checksum       = '145AF4ACBD58D3FE9E964CC22BD8176027F97FCA8BFEA0E8FF7F2E6893FE70E1'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$silentArgs     = '/S'
+$validExitCodes = @(0)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'vpnarea*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
