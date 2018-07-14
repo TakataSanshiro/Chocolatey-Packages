7d8ba0a3a7a84193e13432cdb0b1c0e5de2ed439
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-07-14 23:17:10 +0900

    dnspy init

---
+++ dnspy/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'dnspy'
+$url = 'https://github.com/0xd4d/dnSpy/releases/download/v5.0.0/dnSpy.zip' 
+$checksum = '2122B463BF021DDA1EA6A4D7332DCF2E6A37C6E189736BC1CB0BAAEDD901FEBF'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
