85b1e6c47ef87506a7cb3f6faad7dda09aacf606
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-08-04 02:49:45 +0900

    fixed pureref download error

--- pureref/tools/chocolateyinstall.ps1
+++ pureref/tools/chocolateyinstall.ps1
@@ -3,23 +3,24 @@ $ErrorActionPreference = 'Stop';
 $packageName = 'pureref'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://www.pureref.com/files/installer.php?build=WIN32&downloadKey=Vbf6Zl4vZUwoOoha2xKCmA=='
+$url = 'https://www.pureref.com/files/installer.php?build=WIN32&downloadKey=sX99zMlqHzmGl9G3De0c5A%3D%3D'
 $checksum = '3179FF9868BFE6B06A2D18E606ED4D13FDCBBC5702E26D55E0D96D6448D14231'
 $checksumType = 'sha256'
-$url64 = 'https://www.pureref.com/files/installer.php?build=WIN64&downloadKey=oaOGzK3EU3u0jDE3gARlkw=='
+$url64 = 'https://www.pureref.com/files/installer.php?build=WIN64&downloadKey=FHKAgZh%2B60%2F2pww9WM5Pow%3D%3D'
 $checksum64 = '2C6E729C8F55B48A66E67E67478B43B35941EE1597A204BBFC32AB5C6D871BDF'
 $checksumType64 = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage `
-  -PackageName "$packageName" `
-  -FileType "$installerType" `
-  -SilentArgs "$silentArgs" `
-  -Url "$url" `
-  -Checksum "$checksum" `
-  -ChecksumType "$checksumType" `
-  -Url64bit "$url64" `
-  -Checksum64 "$checksum64" `
-  -ChecksumType64 "$checksumType64" `
-  -ValidExitCodes "$validExitCodes"
+$path = "$toolsDir\pureref.exe"
+
+Get-ChocolateyWebFile        -PackageName "$packageName" `
+                             -FileFullPath "$path" `
+                             -Url "$url" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Url64bit "$url64" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$path"
