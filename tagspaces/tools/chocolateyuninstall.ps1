2430a25d088cdc3ac1b27b8bc6ae8e81b18a54e0
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-09-08 22:18:32 +0900

    removed unnecessary tagspaces uninstall script

--- tagspaces/tools/chocolateyuninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "tagspaces" `
-  -ZipFileName "tagspaces*" 
-
-Remove-item -path "$Home\Desktop\tagspaces.lnk" -Force -ErrorAction 'SilentlyContinue'
-Remove-Item $Env:USERPROFILE\AppData\Roaming\tagspaces -recurse
