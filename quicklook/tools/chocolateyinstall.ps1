627eb03dbd84a446d5f7f7c03fc5e5e0b6f9868e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-09-08 22:11:24 +0900

    updated quicklook to 0.3.6.1

--- quicklook/tools/chocolateyinstall.ps1
+++ quicklook/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'quicklook'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/QL-Win/QuickLook/releases/download/0.3.6/QuickLook-0.3.6.msi'
+$url = 'https://github.com/QL-Win/QuickLook/releases/download/0.3.6.1/QuickLook-0.3.6.1.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   url           = $url
   silentArgs  = "/qn"
   softwareName  = '*quicklook*'
-  checksum      = '5D1BC7AEE084AB8997FC413892A4AB3FB964A9D9D8E45B1D4F482DB8590CF009'
+  checksum      = '70EF8BC4E552C2BDD02EFF7482E72575677A9238FBC19743485D6EF76ABF399B'
   checksumType  = 'sha256'
   validExitCodes = @(0, 3010, 1641)
 }
