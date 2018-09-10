01aa2b7241438f3c52a43363f195f5d3b5b0d310
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-09-10 23:15:14 +0900

    updated protonvpn to 1.6.3

--- protonvpn/tools/chocolateyinstall.ps1
+++ protonvpn/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'protonvpn'
 $installerType  = 'exe'
-$url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.5.1.exe'
-$checksum       = '814308649002AA371073AC92A91B0D9A8DA30B3003F6D4EC7D9D4EEC020C3182'
+$url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.6.3.exe'
+$checksum       = 'FC1B8FD56DB63D37245B41E71928819AF7B572F1F8E7310D06577FA7D0CC8400'
 $toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs     = '/quiet'
 $validExitCodes = @(0)
