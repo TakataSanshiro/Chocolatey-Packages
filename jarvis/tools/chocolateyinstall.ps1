af9848de076e6b793f95c1c69ad00e2ba59814eb
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-09-15 02:19:20 +0900

    jarvis init

---
+++ jarvis/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'jarvis'
+  installerType  = 'exe'
+  url            = 'https://github.com/spectresystems/jarvis/releases/download/v0.8.0/Jarvis-0.8.0-x64.exe'
+  checksum       = '1EDA1110E864B519E90BE2938A4214E322887B196E667E295BEE8CD5B0A048E4'
+  checksumType   = 'sha256'
+  silentArgs     = '/quiet'
+  validExitCodes = @(0)
+  softwareName   = 'jarvis*'
+}
+Install-ChocolateyPackage @packageArgs
