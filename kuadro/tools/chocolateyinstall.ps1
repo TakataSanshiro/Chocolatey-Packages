3a239e293096856431baccd3a271a626d76572b2
Maxim Doucet <maxim.doucet@gmail.com>
2018-09-25 12:39:08 +0200

    Update kuadro to 0.9.5
    See http://kruelgames.com/tools/kuadro/

--- kuadro/tools/chocolateyinstall.ps1
+++ kuadro/tools/chocolateyinstall.ps1
@@ -1,18 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'kuadro'
-$url = 'http://kruelgames.com/tools/kuadro/latest' 
-$checksum = 'BEDD9F480D7DF47F1D6E14F37726A7DA105CC3641FB38B063138E91856594E6C'
+$url = 'http://kruelgames.com/static/kuadro-0.9.5.exe'
+$checksum = 'B54DBEF113D83A25973EAAA3EEB8B34A4AEAF4EE638444EAF05D0A212863AC29'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChocolateyWebFile -PackageName "$packageName" `
-                      -FileFullPath "$toolsDir\kuadro-0.8.5.exe" `
+                      -FileFullPath "$toolsDir\kuadro-0.9.5.exe" `
                       -Url "$url" `
                       -Checksum "$checksum" `
                       -ChecksumType "$checksumType"
 
 # Create an exe shotcut at the desktop.
 Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\kuadro.lnk" `
-						     -TargetPath "$toolsDir\kuadro-0.8.5.exe"
-							 
+						     -TargetPath "$toolsDir\kuadro-0.9.5.exe"
