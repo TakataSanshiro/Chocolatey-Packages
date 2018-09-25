4821dc10ca825791740ad20b41f404ad044456ea
Maxim Doucet <maxim.doucet@gmail.com>
2018-09-25 16:29:34 +0200

    Correct kuadro uninstall script
    kuadro is automatically deleted during "choco uninstall" because the
    executable is simply downloaded in the package's "tools" folder.
    
    Also, the command "Uninstall-ChocolateyZipPackage" was in fact failing
    because there was no zip file previously installed via "Install-ChocolateyZipPackage".

--- kuadro/tools/chocolateyuninstall.ps1
+++ kuadro/tools/chocolateyuninstall.ps1
@@ -1,7 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-Uninstall-ChocolateyZipPackage `
-  -PackageName "kuadro" `
-  -ZipFileName "kuadro*" 
-
 Remove-item -path "$Home\Desktop\kuadro.lnk" -Force -ErrorAction 'SilentlyContinue'
