79f77d98fbbfc71bb9ae1c4432ac5c529eb4c466
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-10-01 20:26:40 +0900

    updated blackbird to 1.0.28 and removed the shortcut

--- blackbird/tools/chocolateyinstall.ps1
+++ blackbird/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'blackbird'
-$url = 'https://www.getblackbird.net/download/Blackbird_v1.0.15_x32.zip' 
-$url64 = 'https://www.getblackbird.net/download/Blackbird_v1.0.15_x64.zip'
-$checksum = 'E6CF584DECC9BCCFA11FBB153C154C0CC339CD58ACDFDE455FF67111C89102DB'
-$checksum64 = 'DAE457D87B316917B60BE9FBAAB52AB733F4085752360C46512A024442C0E5B2'
+$url = 'https://www.getblackbird.net/download/Blackbird_v1.0.28_x32.zip' 
+$url64 = 'https://www.getblackbird.net/download/Blackbird_v1.0.28_x64.zip'
+$checksum = '9A2AE0006707DCA44699BBFF4F11C8735EB6130649F3DA35EA721241EFA02A2F'
+$checksum64 = 'FA27EAC556546D0A313F8EFD254D520456DE30CD39767252AD5C87317804F9F9'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
- 
+
 $is64bit = Get-ProcessorBits 64
 if ($is64bit) {
 	$path = "$toolsDir\blackbird_v0.9.98_64\blackbird.exe"
@@ -16,17 +16,12 @@ if ($is64bit) {
 	else{
 	$path = "$toolsDir\blackbird_v0.9.98_32\blackbird.exe"
 }
- 
+
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType" `
-							 -Url64bit "$url64" `
-							 -Checksum64 "$checksum64" `
-							 -ChecksumType64 "$checksumType64"
-
-# Create an .exe shotcut at the desktop.
-Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\blackbird.lnk" `
-						     -TargetPath "$path"
-							 
+							               -Url64bit "$url64" `
+							               -Checksum64 "$checksum64" `
+							               -ChecksumType64 "$checksumType64"
