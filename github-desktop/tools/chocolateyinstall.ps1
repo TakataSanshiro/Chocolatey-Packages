b0d8e7a0d42a6e48f5285fbeb6d0ef7080dce95f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-10-08 21:38:58 +0900

    updated github-desktop to 1.4.2, and added an uninstall script

--- github-desktop/tools/chocolateyinstall.ps1
+++ github-desktop/tools/chocolateyinstall.ps1
@@ -1,19 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'github-desktop'
-$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$installerType = 'exe'
-$url = 'https://desktop.githubusercontent.com/releases/1.3.5-77f023f8/GitHubDesktopSetup.exe'
-$checksum = '0B8817AC3F8EE514E35373B294D5525FCDB5780CA8A7ED6B8E6862FE30ACC3A9'
-$checksumType = 'sha256'
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage `
-  -PackageName "$packageName" `
-  -FileType "$installerType" `
-  -SilentArgs "$silentArgs" `
-  -Url "$url" `
-  -Checksum "$checksum" `
-  -ChecksumType "$checksumType" `
-  -ValidExitCodes "$validExitCodes"
+$packageArgs = @{
+  packageName    = 'github-desktop'
+  installerType  = 'exe'
+  url            = 'https://desktop.githubusercontent.com/releases/1.4.2-e61693a1/GitHubDesktopSetup.exe'
+  checksum       = '3BE2A7FA5E342D5677C6B55C7EDA2977302C8F13C54056D127B0AB660F47FBA3'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'GitHub Desktop'
+}
+Install-ChocolateyPackage @packageArgs
