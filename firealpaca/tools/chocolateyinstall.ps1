80db84fcb23749eb883872c1f7599498dfe5e114
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-10-15 23:20:04 +0900

    updated firealpaca to 2.1.10

--- firealpaca/tools/chocolateyinstall.ps1
+++ firealpaca/tools/chocolateyinstall.ps1
@@ -1,9 +1,31 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'firealpaca' 
-$installerType = 'exe' 
-$url = 'http://115.146.50.91/firealpaca/bin/FireAlpaca64_setup_2_1_5.exe' 
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
-$validExitCodes = @(0) 
+$packageName    = 'firealpaca'
+$installerType  = 'exe'
+$url            = 'https://firealpaca.com/download/win64'
+$checksum       = 'A266A7C62C58053225755009717D6BF86DFD5CEDBA4760E079E5F64E1F418DDD'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+$options = @{
+  Headers = @{
+    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+    'Accept-Charset' = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3';
+    'Accept-Language' = 'en-US,en;q=0.5';
+    Referer = 'http://firealpaca.com/';
+  }
+}
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 1D4EC65DE6B1A4228A17D4B36745A56057ED633815533F542ABA712EB842EAD2 -ChecksumType sha256
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'pia*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  Options       = $options
+}
+
+Install-ChocolateyPackage @packageArgs
