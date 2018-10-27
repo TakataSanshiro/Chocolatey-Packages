c10f1c9ea83b9340d9f6250ba0405b1073065be6
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-10-28 02:04:05 +0900

    updated github-desktop to 1.4.3

--- github-desktop/tools/chocolateyinstall.ps1
+++ github-desktop/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'github-desktop'
   installerType  = 'exe'
-  url            = 'https://desktop.githubusercontent.com/releases/1.4.2-e61693a1/GitHubDesktopSetup.exe'
-  checksum       = '3BE2A7FA5E342D5677C6B55C7EDA2977302C8F13C54056D127B0AB660F47FBA3'
+  url            = 'https://desktop.githubusercontent.com/releases/1.4.3-f0beb6ed/GitHubDesktopSetup.exe'
+  checksum       = 'D52278949149C38ED64CDCD3651EE84BB4DA8FCBA4C39F8AD9BCF68F4228E95A'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
