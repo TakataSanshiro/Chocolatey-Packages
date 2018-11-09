89c03ada04560ef3ee7db257f38bb801df8d556d
MagicLegend <MagicLegend@users.noreply.github.com>
2018-11-09 14:58:13 +0100

    Update simplenote to 1.2.1
    Used sha1 hash because I didn't have a tool handy to calculate a SHA256 checksum.

--- simplenote/tools/chocolateyinstall.ps1
+++ simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'simplenote'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.1.7/Simplenote-1.1.7-Setup.exe'
+$url = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.2.1/Simplenote-win-1.2.1.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum CBD517C0537E88D27AFD93D84C91A36BF70AB1ED6FF8C03716A18A420C5B176D -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 4d74fd4e3fc4006c52b413cac4294ef40aa8c5ff -ChecksumType sha1
