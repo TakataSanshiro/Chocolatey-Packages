3a224704d2d869992591aa33f0eee33864ebace5
Brendan Forster <brendan@github.com>
2018-11-20 15:18:49 -0400

    update install details

--- github-desktop/tools/chocolateyinstall.ps1
+++ github-desktop/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = 'github-desktop'
   installerType  = 'exe'
-  url            = 'https://desktop.githubusercontent.com/releases/1.4.3-f0beb6ed/GitHubDesktopSetup.exe'
+  url            = 'https://desktop.githubusercontent.com/releases/1.5.0-2f0c701f/GitHubDesktopSetup.exe'
   checksum       = 'D52278949149C38ED64CDCD3651EE84BB4DA8FCBA4C39F8AD9BCF68F4228E95A'
   checksumType   = 'sha256'
   silentArgs     = '/S'
