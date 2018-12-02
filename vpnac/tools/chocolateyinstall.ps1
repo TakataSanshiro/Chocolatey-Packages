d7fbe453e4384840ab0cf9a275a79ac246e2a623
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-12-03 00:39:13 +0900

    update vpnac to 4.0.8

--- vpnac/tools/chocolateyinstall.ps1
+++ vpnac/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName    = 'vpnac'
 $installerType  = 'exe'
 $url            = 'https://vpn.ac/download/vpn.ac-setup.exe'
-$checksum       = 'AEEE7F0C8D2F82D26AC3812A625D9FAA4795E3B4D3BDAF07C07DCAF0A20B8ECA'
+$checksum       = '7D0B80E87BE9F079467D4CECCE746EFCD66D591A3A4026280BEB470274E8BAA6'
 $toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
