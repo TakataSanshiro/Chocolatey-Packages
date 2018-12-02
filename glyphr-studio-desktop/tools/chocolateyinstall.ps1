a37b9c6369e2c61a0596eef15cde25074617d71f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-12-03 02:50:57 +0900

    update glyphr-studio-desktop to 0.4.4

--- glyphr-studio-desktop/tools/chocolateyinstall.ps1
+++ glyphr-studio-desktop/tools/chocolateyinstall.ps1
@@ -2,20 +2,12 @@
 
 $packageName = 'glyphr-studio-desktop'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.3/Glyphr.Studio-win32-ia32.zip'
-$checksum = '12524BE99DE6771C96DFB38E652BFA7DB1393E8DAEA1E92B48D653FACE3D1F40'
+$url = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.4/Glyphr.Studio-win32-ia32.zip'
+$checksum = '911851EDAA1166B3FA84E360BB9863272F8427A980C134F17BDA26C73BA3DE2F'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.3/Glyphr.Studio-win32-x64.zip'
-$checksum64 = 'AAC05AB7B37AE40B7DAEED5BAD94B54A1CF3FD5CBDD479A49BAA7712B4E8AF23'
+$url64 = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.4/Glyphr.Studio-win32-x64.zip'
+$checksum64 = '478F10F54FE64247A86CB47FC99D848C816FA831F341C038F762DDC240E03134'
 $checksumType64 = 'sha256'
-
-$is64bit = Get-ProcessorBits 64
-if ($is64bit) {
-	$path = "$toolsDir\Glyphr Studio-win32-x64\Glyphr Studio.exe"
-	}
-	else{
-	$path = "$toolsDir\Glyphr Studio-win32-ia32\Glyphr Studio.exe"
-}
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
@@ -25,7 +17,3 @@ Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url64bit "$url64" `
                              -Checksum64 "$checksum64" `
                              -ChecksumType64 "$checksumType64"
-
-# Create an exe shotcut at the desktop.
-Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\Glyphr Studio Desktop.lnk" `
-						     -TargetPath "$path"
--- glyphr-studio-desktop/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "glyphr-studio-desktop" `
-  -ZipFileName "Glyphr*" 
-
-Remove-item -path "$Home\Desktop\Glyphr Studio Desktop.lnk" -Force -ErrorAction 'SilentlyContinue'
