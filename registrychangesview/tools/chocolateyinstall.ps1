6f6e775470cabf67c8d45d8aa0ed6aa0415b18d4
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-12-03 02:04:43 +0900

    update registrychangesview to 1.08

--- registrychangesview/tools/chocolateyinstall.ps1
+++ registrychangesview/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'registrychangesview'
 $url = 'https://www.nirsoft.net/utils/registrychangesview.zip' 
 $url64 = 'https://www.nirsoft.net/utils/registrychangesview-x64.zip' 
-$checksum = 'BEF0B53CBCC618B56371CFDFA6025E2B19AD8D72ACBEB1F7A49FCC879034CB1B'
-$checksum64 = 'D5A9417EA67A5EAF00F89623F5B913ECED99F7D3DF5279791063E9BB7DB65D78'
+$checksum = '8109F519BB235672CEDDFD01F5214BCB90D8EE0351C61D9DD8A8AF48266307CA'
+$checksum64 = '6A0B74EBEF191EC5BC90D6F3242C259F694CC68795D6DF0910208104B7A8BFD2'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -14,6 +14,6 @@ Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -UnzipLocation "$toolsDir\RegistryChangesView" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType"`
-							 -Url64bit "$url64" `
-							 -Checksum64 "$checksum64" `
-							 -ChecksumType64 "$checksumType64"
+							               -Url64bit "$url64" `
+							               -Checksum64 "$checksum64" `
+							               -ChecksumType64 "$checksumType64"
