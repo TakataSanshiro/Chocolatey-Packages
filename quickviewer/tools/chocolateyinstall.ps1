5c2ead43c150b21314ab0d2964c961030766f07f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-12-03 02:07:48 +0900

    update quickviewer to 1.1.2

--- quickviewer/tools/chocolateyinstall.ps1
+++ quickviewer/tools/chocolateyinstall.ps1
@@ -1,32 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'quickviewer'
-$url = 'https://github.com/kanryu/quickviewer/releases/download/v1.1.0/QuickViewer-1.1.0-x86.zip' 
-$url64 = 'https://github.com/kanryu/quickviewer/releases/download/v1.1.0/QuickViewer-1.1.0-x64.zip' 
-$checksum = 'A0AB32A624F52C8EB27D7A44CF0343F41DACAFA4CC3F2438C041E13C08199D6E'
-$checksum64 = 'E63833BB4198EF2CF6071B3A57456C3AFBD1139491C9C796A1E92E81BD24B294'
+$url = 'https://github.com/kanryu/quickviewer/releases/download/v1.1.2/QuickViewer-portable-1.1.2-x86.zip' 
+$url64 = 'https://github.com/kanryu/quickviewer/releases/download/v1.1.2/QuickViewer-portable-1.1.2-x64-rev2.zip' 
+$checksum = '58C929C81BE6BC6355025C3B9B21C886F49884DC6B6B6183C4E559EAACD1F6E1'
+$checksum64 = '859F34E4D2BEB196F734856391FDE3F0B77E2C92F0F09BF46CC1F2C0832E962E'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$is64bit = Get-ProcessorBits 64
-if ($is64bit) {
-	$path = "$toolsDir\QuickViewer-0.7.6-x64\QuickViewer.exe"
-	}
-	else{
-	$path = "$toolsDir\QuickViewer-0.7.6-x86\QuickViewer.exe"
-}
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType"`
-							 -Url64bit "$url64" `
-							 -Checksum64 "$checksum64" `
-							 -ChecksumType64 "$checksumType64"
-
-# Create a portable execution file shotcut at the desktop.
-Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\QuickViewer.lnk" `
-						     -TargetPath "$path"
-							 
+							               -Url64bit "$url64" `
+							               -Checksum64 "$checksum64" `
+							               -ChecksumType64 "$checksumType64"
--- quickviewer/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "quickviewer" `
-  -ZipFileName "QuickViewer*.zip" 
-
-Remove-item -path "$Home\Desktop\QuickViewer.lnk" -Force -ErrorAction 'SilentlyContinue'
