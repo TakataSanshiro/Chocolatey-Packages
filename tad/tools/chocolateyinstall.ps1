d966e74d4f2deec94dcb4a3d0a8c8fb3736a5f73
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-12-03 02:32:54 +0900

    update tad to 0.9.0

--- tad/tools/chocolateyinstall.ps1
+++ tad/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'tad'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/antonycourtney/tad/releases/download/v0.8.5/Tad.Setup.0.8.5.exe'
+$url = 'https://github.com/antonycourtney/tad/releases/download/v0.9.0/Tad.Setup.0.9.0.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   url           = $url
   silentArgs  = "/S"
   softwareName  = '*tad*'
-  checksum      = 'A853ACC1CAB5BFF3D79423A7B99F4C42C773E618FA7653594FFE71DD9370E1EA'
+  checksum      = '8F5C469FFACDAFE7CEDB23975C565C9D76D0B0CEC311B2E56CCD352C2734D02A'
   checksumType  = 'sha256'
   validExitCodes = @(0, 3010, 1641)
 }
