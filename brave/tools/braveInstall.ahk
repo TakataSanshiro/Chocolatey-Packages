eb6f0f13d8b0d1e5089357d3564117589d02bdfa
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-12-17 03:47:52 +0900

    update brave to 0.60.2

---
+++ brave/tools/braveInstall.ahk
@@ -0,0 +1,17 @@
+#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+#NoTrayIcon
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+SetTitleMatchMode, 2 ; A window's title can contain WinTitle anywhere inside it to be a match. 
+
+
+Loop, 10000
+{
+sleep 10000
+
+IfWinExist, Brave-Release Installer
+  {
+  WinActivate
+  Send {enter}
+  }
+}
--- brave/tools/chocolateyinstall.ps1
+++ brave/tools/chocolateyinstall.ps1
@@ -3,14 +3,18 @@
 $packageName = 'brave'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/brave/browser-laptop/releases/download/v0.20.30dev/Brave-Setup-ia32.exe'
-$checksum = '011E6C5319D51EB865418A5D8F8CBEE080F6573EC882DE1F59FEC0333DEDA6AC'
+$url = 'https://laptop-updates.brave.com/latest/winia32'
+$checksum = '217C9CA3AA78B854A4E9CE91E177E6F20214BAEF514BA3A5D0B991E0206883FE'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/brave/browser-laptop/releases/download/v0.20.30dev/Brave-Setup-x64.exe'
-$checksum64 = 'CF72EE6A4C0EB0C6396FAA5CC5D95A19B40B43E71AB346254AB0EDB0AAE995EC'
+$url64 = 'https://laptop-updates.brave.com/latest/winx64'
+$checksum64 = 'E272E22B7E0D0214E9689A96C2A97F200475C158F6E43FAB9D470D07F46B22FB'
 $checksumType64 = 'sha256'
-$silentArgs = '/VERYSILENT'
+$silentArgs = ''
 $validExitCodes = @(0) 
+
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "braveInstall.ahk"
+Start-Process $ahkExe $ahkFile
 
 Install-ChocolateyPackage `
   -PackageName "$packageName" `
