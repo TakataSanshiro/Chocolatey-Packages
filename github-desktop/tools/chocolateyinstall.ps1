c5da18098e4f77a29a2c08abd9c20cebdef17390
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2018-12-23 17:14:50 +0900

    update github-desktop to 1.5.1

--- github-desktop/tools/chocolateyinstall.ps1
+++ github-desktop/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'github-desktop'
   installerType  = 'exe'
-  url            = 'https://desktop.githubusercontent.com/releases/1.5.0-2f0c701f/GitHubDesktopSetup.exe'
-  checksum       = 'BBAB9098B4422288C6CE42E92FC12BE85E8C79757CE5C676856B6401A24DD5B3'
+  url            = 'https://desktop.githubusercontent.com/releases/1.5.1-b1e34b0f/GitHubDesktopSetup.exe'
+  checksum       = '3A2312627E43F84200D01200B8FB128F774B2631509EBBDC96061E2313CB79A0'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
