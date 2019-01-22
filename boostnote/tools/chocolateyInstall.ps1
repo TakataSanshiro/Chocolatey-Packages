5fb5c9ca7330bf6d19314fb4cd77622931209e75
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-01-23 03:15:35 +0900

    update boostnote to 0.11.12

--- boostnote/tools/chocolateyInstall.ps1
+++ boostnote/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'boostnote'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/BoostIO/boost-releases/releases/download/v0.11.11/BoostnoteSetup.exe' 
+$url = 'https://github.com/BoostIO/boost-releases/releases/download/v0.11.12/BoostnoteSetup.exe' 
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 521C5A584D12C7EA8AE201A29E9F5E1F2BDCE0967887C14AE418DB6736DFA177 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 342D8FC92262927CCC21398C7F2ABE164F082B876B9F395AF8C1C72AA706024A -ChecksumType sha256
