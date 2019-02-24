8b615257bdb1636e0ff9b6476bb3f4e908d8693b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-02-25 03:01:03 +0900

    update appaudioconfig to 1.10

--- appaudioconfig/tools/chocolateyinstall.ps1
+++ appaudioconfig/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'appaudioconfig'
 $url = 'https://www.nirsoft.net/utils/appaudioconfig.zip' 
 $url64 = 'https://www.nirsoft.net/utils/appaudioconfig-x64.zip' 
-$checksum = '95D713C2ACB34E0BEE47E8BD79D7F7BDB9CEEBE0C217C65271ABF495EFAE4527'
-$checksum64 = '59168C8031D178BDE27D036B0747983EC61E401C54846214EE2C11820274A5BB'
+$checksum = '1FDE69EFF818205FA3DBA8EAC0C4CF8409791D03EEEAB09306B90B484D804ABB'
+$checksum64 = '770A790A1A36C1374E2761EEF7CC3C7C08DD91E5FE26503EFB831EE66D5A2A1A'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
