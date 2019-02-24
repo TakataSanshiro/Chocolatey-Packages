f65410b48b5c75f6b22cfe310b0b524591d76001
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-02-25 03:05:10 +0900

    update defender-injector to 1.1

--- defender-injector/tools/chocolateyinstall.ps1
+++ defender-injector/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'defender-injector'
 $url = 'https://www.sordum.org/files/small-tools/dInjector.zip' 
-$checksum = '33D207F30C8E105603A9C51F522BF15D572676FED0E1A13F6341BF56EC481848'
+$checksum = '33F490C67CC02F5701F518E150A166C2D305ED2E31D77EB7E868C7886677666A'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
