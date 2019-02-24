80ab80cd8f6a0775183fb068974ef2c58c582351
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-02-25 02:56:23 +0900

    update processing to 3.5.3

--- processing/tools/chocolateyinstall.ps1
+++ processing/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName = 'processing'
-$url = 'https://github.com/processing/processing/releases/download/processing-0265-3.4/processing-3.4-windows32.zip'
-$checksum = '971FB0360E1A5ED51EA300E91651BB8BA6E95B5BEB983E9847BBEC267621049E'
+$url = 'https://github.com/processing/processing/releases/download/processing-0269-3.5.3/processing-3.5.3-windows32.zip'
+$checksum = 'DC88E5BFF6B74ED8C4E17E0B991F32D2952DD82510E614954A665E88FB4BE744'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/processing/processing/releases/download/processing-0265-3.4/processing-3.4-windows64.zip'
-$checksum64 = '5EF6B2D59A82B3900F3FBDC6A816CCEA9BCC67DFCE8A7A240476CB26A74DAC80'
+$url64 = 'https://github.com/processing/processing/releases/download/processing-0269-3.5.3/processing-3.5.3-windows64.zip'
+$checksum64 = 'AF4DB4AF445C10E0B48BE2D0CA4798FD44BFD94156A670AC15BE2B275F975137'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
