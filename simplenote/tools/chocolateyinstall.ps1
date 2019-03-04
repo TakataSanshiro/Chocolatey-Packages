9e20c7b15ac85c59f50b7fe886dc65b75d6d690c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-05 03:28:52 +0900

    update simplenote to 1.5.0

--- simplenote/tools/chocolateyinstall.ps1
+++ simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'simplenote'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.3.4/Simplenote-win-1.3.4.exe'
+$url = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.5.0/Simplenote-win-1.5.0.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum AB90E364EFC309AB75A9941E040461EB0B6A9387F041C0847DD2480AF4ED36B0 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum B86A7B5F5E81E65D8E999E8A3D36C4B4484DB9A11ADF818FC714A267A40F3AB5 -ChecksumType sha256
