98e55fd377d4a2c8f31270db9861bec22e288c69
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-05 03:52:46 +0900

    update flow to 6.0.26 and remove the uninstall script

--- flow/tools/chocolateyinstall.ps1
+++ flow/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $url = 'https://cdn.getflow.com/win32_ia32/production/release/Flow.zip'
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$exe = "$scriptDir\Flow Setup 6.0.17-ia32.exe"
+$exe = "$scriptDir\Flow Setup 6.0.26.exe"
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$scriptDir" -Checksum AF3A3CB95B06931ABA1544E1F9A64F2B7D6CD88664416FA70E0FF0A9FC530A12 -ChecksumType sha256
+Install-ChocolateyZipPackage "$packageName" "$url" "$scriptDir" -Checksum BF4C01E6857B28B380C2AA41D37321BD28C45FE6CAA3E20A2D3B03A5F132B688 -ChecksumType sha256
 Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$exe"
