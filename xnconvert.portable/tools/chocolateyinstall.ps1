8336ddcf41b20948581fecd40afce1bd8bc0b4c7
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-05 03:42:54 +0900

    update xnconvert.portable to 1.79

--- xnconvert.portable/tools/chocolateyinstall.ps1
+++ xnconvert.portable/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'xnconvert.portable'
 $url = 'http://download.xnview.com/XnConvert-win.zip' 
 $url64 = 'http://download.xnview.com/XnConvert-win-x64.zip'
-$checksum = '9BCE1B31EBAD1BCFD1BBCDAC2900E6F448727FDB3DD815BE0DCC73808A7A90D7'
-$checksum64 = '40FCC56C2852280A88AC100FDBE49D20D20999998E788E48641FE85D7EE4C0EB'
+$checksum = '1991BAF5F37F1FD764D480056A94DF17D81DC505A81E2EA16F22390C24FCC859'
+$checksum64 = 'A695F1B8E6F1966A41F86E245ADE1B3F525C31170CF266A5E9418CF2040789B4'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
