aaf4cd0c021077104d9987be277abe2ab9133c64
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-06 02:56:55 +0900

    fix the disable-nvidia-telemetry uninstall script

--- disable-nvidia-telemetry/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "disable-nvidia-telemetry" `
-  -ZipFileName "Disable.Nvidia.Telemetry*.zip" 
-
-Remove-item -path "$Home\Desktop\Disable Nvidia Telemetry.lnk" -Force -ErrorAction 'SilentlyContinue'
