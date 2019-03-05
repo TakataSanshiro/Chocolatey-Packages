9aae24967584619cf7eaf9e57587cb6517931152
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-06 02:36:21 +0900

    fix defender-injector install error

--- defender-injector/tools/chocolateyinstall.ps1
+++ defender-injector/tools/chocolateyinstall.ps1
@@ -1,13 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'defender-injector'
-$url = 'https://www.sordum.org/files/small-tools/dInjector.zip' 
+$url = 'https://www.sordum.org/files/download/defender-injector/dInjector.zip' 
 $checksum = '33F490C67CC02F5701F518E150A166C2D305ED2E31D77EB7E868C7886677666A'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$options = @{
+  Headers = @{
+    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+    'Accept-Charset' = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3';
+    'Accept-Language' = 'en-US,en;q=0.5';
+    Referer = 'http://firealpaca.com/';
+  }
+}
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  UnzipLocation = $toolsDir
+  Options       = $options
+}
+
+Install-ChocolateyZipPackage @packageArgs
