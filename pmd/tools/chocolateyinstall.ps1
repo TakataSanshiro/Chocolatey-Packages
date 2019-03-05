b6d2cdc25229e2f0a80c423e801e04d60a56978b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-06 02:21:26 +0900

    update pmd to 6.12.0

--- pmd/tools/chocolateyinstall.ps1
+++ pmd/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$pmd_home = Join-path $package 'pmd-bin-6.11.0'
+$pmd_home = Join-path $package 'pmd-bin-6.12.0'
 $pmd_bin = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin = Join-Path $pmd_home 'bin/cpd.bat'
  
 Install-ChocolateyZipPackage `
     -PackageName 'pmd' `
-    -Url 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.11.0/pmd-bin-6.11.0.zip' `
-    -Checksum '8BE994696DA17C393056B446F918BC7E631A1F07CCC1365509974843ED12E355' `
+    -Url 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.12.0/pmd-bin-6.12.0.zip' `
+    -Checksum 'E61B3855BD94C1A3875B788CFD252A2D4F1232D47A41479078640DA9AA3F7ECB' `
     -ChecksumType 'SHA256'`
     -UnzipLocation $package `
  
