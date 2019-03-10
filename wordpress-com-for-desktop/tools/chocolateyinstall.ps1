98bcad163a9a4fd3b0517177f24e014c395b682d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 02:39:00 +0900

    update wordpress-com-for-desktop to 3.9.0

--- wordpress-com-for-desktop/tools/chocolateyinstall.ps1
+++ wordpress-com-for-desktop/tools/chocolateyinstall.ps1
@@ -1,10 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'wordpress-com-for-desktop'
-$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$installerType = 'exe'
-$url = 'https://github.com/Automattic/wp-desktop/releases/download/v3.7.0/WordPressDesktop-Setup-3.7.0.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 51E8758266D72F0915D5B2B7C06C76B0A712310B8D20AE66E3A8515D8F03863B -ChecksumType sha256
+$packageArgs = @{
+  packageName    = 'wordpress-com-for-desktop'
+  installerType  = 'exe'
+  url            = 'https://github.com/Automattic/wp-desktop/releases/download/v3.9.0/WordPressDesktop-Setup-3.9.0.exe'
+  checksum       = 'F467AD96F8A5159652E844AD7A8454D2D7E3792194236F1BAF2CA16F0CE5DFC0'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'WordPress.com Desktop*'
+}
+Install-ChocolateyPackage @packageArgs
