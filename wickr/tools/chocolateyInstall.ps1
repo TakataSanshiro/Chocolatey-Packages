f6458008ae435d8a1eeca8624a93879a8ebc8158
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 22:32:49 +0900

    update wickr to 5.4.4

--- wickr/tools/chocolateyInstall.ps1
+++ wickr/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'wickr'
 $installerType = 'msi'
-$url = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.2.4.msi'
+$url = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.4.4.msi'
 $silentArgs = '/qb'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -Checksum 9F220BA4F396064F5D60850E16B045F55F45DE3910611CF0EA42B46DFCDE0AD5 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -Checksum E97C83A41DF43EDA924BA88EF6C709F625EF1210E84A64CA86B9241507F0B22C -ChecksumType sha256
