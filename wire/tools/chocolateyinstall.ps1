ed741f71927eb6471b602e2d4298f20731365e7c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 22:04:29 +0900

    update wire to 3.6.2908

--- wire/tools/chocolateyinstall.ps1
+++ wire/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 $packageName = 'wire'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/wireapp/wire-desktop/releases/download/windows%2F3.5.2899/wire-3.5.2899.exe'
+$url = 'https://github.com/wireapp/wire-desktop/releases/download/windows%2F3.6.2908/WireSetup.3.6.2908.exe'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 6D75935E1E4A86D2F39B2EBCE259ABF2B78C9A334CC02E6EA6FF13A5A365E1FE -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 0A08C6B03935FC52D267D769B400C44DEA5EBAFDBE103FB01D999597C7A8A619 -ChecksumType sha256
