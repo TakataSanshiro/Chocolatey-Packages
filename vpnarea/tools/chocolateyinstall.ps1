0b1744b36f11c1b923c5257ba20aa7fcb757c6d8
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 21:52:59 +0900

    update vpnarea to 2.1.0.20190311

--- vpnarea/tools/chocolateyinstall.ps1
+++ vpnarea/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName    = 'vpnarea'
 $installerType  = 'exe'
 $url            = 'https://vpnarea.com/VPNArea.exe'
-$checksum       = '63EBD171AF7DB5D70F8062861DF13C38D2FE95FF3AB8BF180E4704ED3532F001'
+$checksum       = '995FF02E2A4B0B706C4DBDDC3F9C874F748261A228C4B72CDCA0BD75D39B9CF7'
 $toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs     = '/S'
 $validExitCodes = @(0)
