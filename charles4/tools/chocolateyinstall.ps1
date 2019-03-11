d7123e0cfcda480c20ba4dbc47f836c29cd89394
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 22:15:32 +0900

    update charles4 to 4.2.8

--- charles4/tools/chocolateyinstall.ps1
+++ charles4/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 
 $packageName= 'charles4' 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.charlesproxy.com/assets/release/4.2.7/charles-proxy-4.2.7-win32.msi'
-$url64bit   = 'https://www.charlesproxy.com/assets/release/4.2.7/charles-proxy-4.2.7-win64.msi'
+$url        = 'https://www.charlesproxy.com/assets/release/4.2.8/charles-proxy-4.2.8-win32.msi'
+$url64bit   = 'https://www.charlesproxy.com/assets/release/4.2.8/charles-proxy-4.2.8-win64.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -17,9 +17,9 @@ $packageArgs = @{
   validExitCodes= @(0) 
 
   softwareName  = 'Charles' 
-  checksum      = '5F085A41322E11820D918FB89EF0394AFE1B8C9354765F7A491D7402087C7174'
+  checksum      = 'FDFEC8E011170F970C4D7104B7E3D8FB5CDE2AA47E344BF4D35FEB2F785937D8'
   checksumType  = 'sha256'
-  checksum64    = '5A088C22E11D34469655D4D2048B239C883D49F1C9025F8160BB835CB9DE8D8E'
+  checksum64    = '448FB0B76A0ECCD93580C986F77D5621C7CA7B2E138588436DC30DE67436308E'
   checksumType64= 'sha256' 
 }
 
