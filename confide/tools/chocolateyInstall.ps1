d29508ca2c8fdcb02be11730f969780e11b537fd
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 21:37:02 +0900

    update confide to 1.7.1

--- confide/tools/chocolateyInstall.ps1
+++ confide/tools/chocolateyInstall.ps1
@@ -6,4 +6,4 @@ $url = 'https://d3kedtkr0tjlm2.cloudfront.net/ConfideSetup.exe'
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 82651410B48E480265B55F37FD5F6CC775CB38BCE360ED76C8CF43D45A563F64 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 8ADED7A45861C689202380678A089EC442197DBC70242690E978CCE34DDA961D -ChecksumType sha256
