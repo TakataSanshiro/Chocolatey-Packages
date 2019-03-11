5706f6fd44a4d5d98db2a84b86436411b35a580d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 20:54:00 +0900

    update whale to 0.16.0 and remove no need the uninstall script

--- whale/tools/chocolateyinstall.ps1
+++ whale/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'whale'
-$url = 'https://github.com/1000ch/whale/releases/download/v0.15.0/Whale-windows-v0.15.0.zip'
-$checksum = 'EA5401BB7E02B4184AEBD1F26A381C5E4D52206AEE02829C4D4E1C185D37C270'
+$url = 'https://github.com/1000ch/whale/releases/download/v0.16.0/Whale-windows-v0.16.0.zip'
+$checksum = 'E87C3EB2ECD192A6F49A1218507B22F9113515E35DE18B0A9BC8D5B815282880'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
@@ -11,8 +11,3 @@ Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType"
-
-# Create an .exe shotcut at the desktop.
-Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\Whale.lnk" `
-						     -TargetPath "$toolsDir\Whale.exe"
-							 
