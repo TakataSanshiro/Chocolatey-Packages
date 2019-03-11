7ac6229a8bc9e58ba0093db3f2c367c1dbdb3ba8
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 22:47:55 +0900

    update wingide101 to 6.1.5

--- wingide101/tools/chocolateyInstall.ps1
+++ wingide101/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'wingide101' 
 $installerType = 'exe' 
-$url = 'https://wingware.com/pub/wingide-101/6.0.12/wingide-101-6.0.12-1.exe' 
+$url = 'https://wingware.com/pub/wingide-101/6.1.5/wingide-101-6.1.5-1.exe' 
 $silentArgs = '/VERYSILENT' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 70B03C2B29ABD92C3944ED5B161E698B231CE7F5C79EFC9CC1303E36E56E0C54 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 88FC554BB4191745F78C1953B9668600AB64726D306AD3DAD31A8370910E4F43 -ChecksumType sha256
