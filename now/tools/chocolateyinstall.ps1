ca7d86681fba0bf13dea2f42dfa226a3fa527a7f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 22:11:57 +0900

    update now to 4.0.18

--- now/tools/chocolateyinstall.ps1
+++ now/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'now' 
 $installerType = 'exe' 
-$url = 'https://github.com/zeit/now-desktop/releases/download/4.0.12/now-desktop-Setup-4.0.12.exe' 
+$url = 'https://github.com/zeit/now-desktop/releases/download/4.0.18/now-desktop-Setup-4.0.18.exe' 
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 639B4D3EB0531F5189A7CE1F934FF6582C6E414352564FFF8B470DECB1D7AE05 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum C821E2621613F1175888415B5A0965965055297FBD6FBDE8940D6360A12FD7C0 -ChecksumType sha256
