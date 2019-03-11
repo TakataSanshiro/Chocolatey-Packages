ccff63360ddd67cda1d6cb7507f905f516bc6afd
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 22:08:03 +0900

    update mamp to 4.1.0

--- mamp/tools/chocolateyinstall.ps1
+++ mamp/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'mamp'
 $installerType  = 'exe'
-$url            = 'https://downloads10.mamp.info/MAMP-PRO-WINDOWS/releases/4.0.1/MAMP_MAMP_PRO_4.0.1.exe'
-$checksum       = '077a1607141eb0df09c819fb9721b39229c6ec07e7fa5fe92f490e9060f01b59'
+$url            = 'https://downloads.mamp.info/MAMP-PRO-WINDOWS/releases/4.1/MAMP_MAMP_PRO_4.1.exe'
+$checksum       = '5AF38B47A42484E907B995D9FC05F579E6A22A896D4BEAAEABDC441ACDA54F22'
 $toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
