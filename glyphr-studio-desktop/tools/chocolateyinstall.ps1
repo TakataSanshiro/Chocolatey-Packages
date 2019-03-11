7d407fd70c668d6b9c72a723f674ba04643c375c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 20:43:59 +0900

    update glyphr-studio-desktop to 0.5.0

--- glyphr-studio-desktop/tools/chocolateyinstall.ps1
+++ glyphr-studio-desktop/tools/chocolateyinstall.ps1
@@ -2,18 +2,12 @@
 
 $packageName = 'glyphr-studio-desktop'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.4/Glyphr.Studio-win32-ia32.zip'
-$checksum = '911851EDAA1166B3FA84E360BB9863272F8427A980C134F17BDA26C73BA3DE2F'
+$url = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.5.0/Glyphr.Studio.Setup.0.5.0.exe'
+$checksum = 'A9349E4C47837BC5C6E57019778B4E338450D9E16DA21C04BFB3305CD0F630B4'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.4.4/Glyphr.Studio-win32-x64.zip'
-$checksum64 = '478F10F54FE64247A86CB47FC99D848C816FA831F341C038F762DDC240E03134'
-$checksumType64 = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
-                             -Url64bit "$url64" `
-                             -Checksum64 "$checksum64" `
-                             -ChecksumType64 "$checksumType64"
+                             -ChecksumType "$checksumType"
