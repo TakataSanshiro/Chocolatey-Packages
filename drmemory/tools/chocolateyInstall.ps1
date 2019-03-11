c697cf82458f0e57063a935b88f80926e5652f44
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-11 23:35:58 +0900

    update drmemory to cronbuild-2.0.17959 and remove the virtual packages

--- drmemory/tools/chocolateyInstall.ps1
+++ drmemory/tools/chocolateyInstall.ps1
@@ -1,1 +1,13 @@
-﻿#Install-VirtualPackage 'drmemory.portable' 'drmemory.install'
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'drmemory'
+$url = 'https://github.com/DynamoRIO/drmemory/releases/download/cronbuild-2.0.17959/DrMemory-Windows-1.11.17959-1.zip' 
+$checksum = '2B94346B80C5388EC480EAD15247F0CB5FB21580E2870EFD1BC8EAE01B2BAD45'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
