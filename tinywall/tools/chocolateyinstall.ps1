21a91409c98034ac1f59bf8abd7eac7d6a83cd72
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-03-16 19:32:43 +0900

    rewrite tinywall package

--- tinywall/tools/chocolateyinstall.ps1
+++ tinywall/tools/chocolateyinstall.ps1
@@ -1,9 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'tinywall' 
-$installerType = 'msi' 
-$url = 'https://tinywall.pados.hu/ccount/click.php?id=3' 
-$silentArgs = '/qn /norestart' 
-$validExitCodes = @(0, 3010, 1641) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 05D348D18F99FED60E4ED82370EC3690655D0170F9034E1D06094F563190D64A -ChecksumType sha256
+$packageArgs = @{
+  packageName    = 'tinywall'
+  installerType  = 'msi'
+  url            = 'https://tinywall.pados.hu/ccount/click.php?id=3'
+  checksum       = '05D348D18F99FED60E4ED82370EC3690655D0170F9034E1D06094F563190D64A'
+  checksumType   = 'sha256'
+  silentArgs     = '/qn /norestart'
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'TinyWall*'
+}
+Install-ChocolateyPackage @packageArgs
