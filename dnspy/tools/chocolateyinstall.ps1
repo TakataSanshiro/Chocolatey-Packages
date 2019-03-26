cf12c2c189aa54d21313675edd686db1aa7936fa
Wiktor Bednarz <wiktor@wiktor.be>
2019-03-26 12:50:27 +0100

    Update dnSpy to 6.0.4

--- dnspy/tools/chocolateyinstall.ps1
+++ dnspy/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'dnspy'
-$url = 'https://github.com/0xd4d/dnSpy/releases/download/v6.0.3/dnSpy-net472.zip' 
-$checksum = 'BE368CE75245BFAFDA75335DC18AA82F763CFB3137B40A039ECAD375575E20B6'
+$url = 'https://github.com/0xd4d/dnSpy/releases/download/v6.0.4/dnSpy-net472.zip' 
+$checksum = '68C0334F921F542B29CC9DB670018C46A34E1E9AA81F996C51A3A9EA656479F1'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
