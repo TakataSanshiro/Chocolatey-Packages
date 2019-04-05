2471d68e2e3b191bc3c547fbcfee25fcf665e06c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-04-06 02:23:48 +0900

    fix the wifiguard checksum

--- wifiguard/tools/chocolateyInstall.ps1
+++ wifiguard/tools/chocolateyInstall.ps1
@@ -6,4 +6,4 @@ $url = 'https://www.softperfect.com/download/files/wifiguard_windows_setup.exe'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 8EF19F635DF99070F16A60B3813A2AEAD8A27A65983FCC02E12CA9176F73507D -ChecksumType sha256
