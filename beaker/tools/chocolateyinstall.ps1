7b388d25d9539a62c06ece18aaa5a5bdf5e614d5
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-04-06 02:22:21 +0900

    update beaker to 0.8.8

--- beaker/tools/chocolateyinstall.ps1
+++ beaker/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'beaker'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/beakerbrowser/beaker/releases/download/0.8.5/beaker-browser-setup-0.8.5.exe' 
-$checksum = '6A700287DFC70AB23E70DEE7545ECBF398EE43EB5344E2F21AA4FFFF8C845FAD'
+$url = 'https://github.com/beakerbrowser/beaker/releases/download/0.8.8/beaker-browser-setup-0.8.8.exe' 
+$checksum = '6662842F36E3D4E5E52AA8CA209D6727850A97831980214ED749829409A2D886'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0) 
