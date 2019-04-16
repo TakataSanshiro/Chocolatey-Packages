e9c12b873858e2a941999e33abdd388f8969bdff
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-04-17 02:30:00 +0900

    fix twitch url and checksum

--- twitch/tools/chocolateyInstall.ps1
+++ twitch/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'twitch'
 $installerType = 'exe'
-$url = 'https://updates.twitchapp.net/windows/installer/TwitchSetup.exe'
+$url = 'https://desktop.twitchsvc.net/installer/windows/TwitchSetup.exe'
 $silentArgs = '/silent'
 $validExitCodes = @(0, 1638)
 
@@ -22,4 +22,4 @@ if (-not $env:ChocolateyForce) {
   }
 }
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum FBEAFC0C3A006C44CF67105D2EE458F426A00F25C52D5BAE7159B04967CE90DF -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 075A3DFA76461D6C95FAAFD948B118665369D97E2191A37479B5C84E9E38AA0B -ChecksumType sha256
