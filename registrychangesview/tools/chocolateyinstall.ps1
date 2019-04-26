a8db3b9826492d841d58a346a970eb8f479c63bc
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-04-27 00:55:34 +0900

    update registrychangesview to 1.12

--- registrychangesview/tools/chocolateyinstall.ps1
+++ registrychangesview/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'registrychangesview'
 $url = 'https://www.nirsoft.net/utils/registrychangesview.zip' 
 $url64 = 'https://www.nirsoft.net/utils/registrychangesview-x64.zip' 
-$checksum = '7D37C75B35C3B78B2CBFE26C8B7795F47F595FEB9BAE299DAA18AF9D93AE1284'
-$checksum64 = '975FC9EA22D59644D6927C40F9E24A754057E14FEC94C206549FF5614BF5FA6D'
+$checksum = '8FCCDFEE1914212706E07ABCE39F0FD116C9D1BED08EB9C3CF9925AA8FA667B3'
+$checksum64 = 'E52DC1BC29219BD19384E8986F1E81834C1352FB68ECC1410BCC53C0785C8007'
 $checksumType = 'sha256'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
