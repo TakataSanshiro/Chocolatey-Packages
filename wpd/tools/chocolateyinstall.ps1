bd480edd373542afdc171e8b81b27bfb975d12af
Barnacle <martsevsoft@mail.ru>
2019-05-11 00:00:50 +0700

    Update info of WPD
    * Delete wpd.1.1.475.nupkg
    
    * Update chocolateyinstall.ps1
    
    * Update wpd.nuspec

--- wpd/tools/chocolateyinstall.ps1
+++ wpd/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'wpd'
-$url = 'https://getwpd.com/get/latest.zip' 
+$url = 'https://wpd.app/get/latest.zip' 
 $checksum = '6400088D0C330A327AEB333926F493608D934A3936CCDD07C9A1814D2849726C'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
