dc3d6c263d59b83848794109aaa06e4e9f2c0765
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-05-12 01:37:39 +0900

    update grammarly to 1.5.49

--- grammarly/tools/chocolateyInstall.ps1
+++ grammarly/tools/chocolateyInstall.ps1
@@ -6,4 +6,4 @@ $url = 'https://download-editor.grammarly.com/windows/GrammarlySetup.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 5F852F5F06D8B5689522EF43782E693D112E95B431FA55510ABDA146CC1CA576 -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 5E5CC8985650DE79F18EDED3100C7D647A599BBB441EAD7DBD2396A1A5E89C96 -ChecksumType sha256
