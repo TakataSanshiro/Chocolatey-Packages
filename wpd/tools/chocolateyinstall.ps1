6ed78751089fd2cf09deee0a5132908eb3a6a695
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-05-24 02:35:38 +0900

    update wpd checksum

--- wpd/tools/chocolateyinstall.ps1
+++ wpd/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'wpd'
 $url = 'https://wpd.app/get/latest.zip' 
-$checksum = '6400088D0C330A327AEB333926F493608D934A3936CCDD07C9A1814D2849726C'
+$checksum = 'A4537EA6AE73DE5D29367B802C2DE0D191109BC7E210BF9E46E6BA70C1628E74'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
