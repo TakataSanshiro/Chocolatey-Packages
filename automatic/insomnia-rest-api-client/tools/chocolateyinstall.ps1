16cb19e0ef2b4e99465fb68dd20b960083570734
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-05-26 01:21:58 +0900

    create AU for insomnia-rest-api-client

---
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'insomnia-rest-api-client'
+  installerType = 'exe'
+  url           = 'https://github.com/getinsomnia/insomnia/releases/download/v6.5.3/Insomnia.Setup.6.5.3.exe'
+  checksum      = 'be023a82ed3979ecd3585b9b1b3c38b520df9a3d22d162407e1efbde9ab73601'
+  checksumType  = 'sha256'
+  silentArgs    = '--silent'
+  validExitCodes= @(0)
+  softwareName   = 'Insomnia'
+}
+Install-ChocolateyPackage @packageArgs
