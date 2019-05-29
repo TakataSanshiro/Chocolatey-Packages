7694a283d0c397f029030cbfb6a27fc4a97ff151
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-05-30 01:21:07 +0900

    update shadowsocks to 4.1.6

--- shadowsocks/tools/chocolateyinstall.ps1
+++ shadowsocks/tools/chocolateyinstall.ps1
@@ -1,13 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'shadowsocks'
-$url = 'https://github.com/shadowsocks/shadowsocks-windows/releases/download/4.1.5/Shadowsocks-4.1.5.zip' 
-$checksum = 'D39F61DBF2A753769C0EFB7712DD7BFA6E1D1593EBAED06150F206F3B6FF7DE2'
-$checksumType = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$packageArgs = @{
+  packageName    = 'shadowsocks'
+  url            = 'https://github.com/shadowsocks/shadowsocks-windows/releases/download/4.1.6/Shadowsocks-4.1.6.zip'
+  checksum       = 'e9fad6bcba22427d7efb3d9b341d11173659a06cc12670ba9d542aeb670284b8'
+  checksumType   = 'sha256'
+  softwareName   = 'Shadowsocks'
+  unzipLocation  = '$(Split-Path -parent $MyInvocation.MyCommand.Definition)'
+}
+Install-ChocolateyZipPackage @packageArgs
