592b282eb0f146394cd53fe81969dc5c4f05605c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-05-30 01:21:35 +0900

    add AU to shadowsocks package

---
+++ shadowsocks/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'https://github.com/shadowsocks/shadowsocks-windows/tags'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.links.href -match 'tag/' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
+
+    @{
+        Version = $version
+        URL64   = "https://github.com/shadowsocks/shadowsocks-windows/releases/download/$version/Shadowsocks-$version.zip"
+    }
+}
+
+try {
+    update
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
