6e075f36a0eca1fb8288129070bebdbcb7ac5341
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-01 00:22:19 +0900

    replace portexpert rawgit with jsdelivr

--- portexpert/tools/chocolateyinstall.ps1
+++ portexpert/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'portexpert'
-$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$installerType = 'exe'
-$url = 'https://kcsoftwares.com/files/portexpert_lite.exe'
-$checksum = '333314F5241240563018FA9F57BB91396F80286A3D2226F1EAE014C4722D5E8A'
-$checksumType = 'sha256'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$packageName    = 'portexpert'
+$toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType  = 'exe'
+$url            = 'https://kcsoftwares.com/files/portexpert_lite.exe'
+$checksum       = '333314F5241240563018FA9F57BB91396F80286A3D2226F1EAE014C4722D5E8A'
+$checksumType   = 'sha256'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) 
 
 Install-ChocolateyPackage `
