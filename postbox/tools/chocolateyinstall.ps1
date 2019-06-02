7664794a64a2555facc97f842eff19c6b67a552c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-03 00:16:40 +0900

    update postbox to 6.1.18

--- postbox/tools/chocolateyinstall.ps1
+++ postbox/tools/chocolateyinstall.ps1
@@ -1,18 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= 'postbox'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-6.1.11-win32.exe'
-
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'Postbox*'
-  checksum      = '5B72D8ED32AF186E1158315EE0FC4308DD690968C38C98088613F4384A0106A2'
+  packageName   = 'postbox'
+  installerType = 'exe'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-6.1.18-win32.exe'
+  checksum      = '0885ACFF8732F5F50AFA6AA103F5623CFE1F556C7D551AD563D6241D1B33452C'
   checksumType  = 'sha256'
-  silentArgs   = '/S'
+  silentArgs    = '/S'
+  toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  unzipLocation = $toolsDir
+  softwareName  = 'Postbox*'
 }
 
 Install-ChocolateyPackage @packageArgs
