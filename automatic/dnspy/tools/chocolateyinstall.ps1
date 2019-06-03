46a0d11b5a7447efea3dc0dd9ffb8780c3aab7df
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-04 01:05:06 +0900

    move dnspy to automatic directory and update one to 6.0.5

---
+++ automatic/dnspy/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'dnspy'
+  url            = 'https://github.com/0xd4d/dnSpy/releases/download/v6.0.5/dnSpy-net472.zip'
+  checksum       = '7fc52afe2728f2b02d31a98ce0b26ad10295b1f93fa6a944350acfd070a40baa'
+  checksumType   = 'sha256'
+  softwareName   = 'dnSpy'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/dnspy/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'https://github.com/0xd4d/dnSpy/tags'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.links.href -match 'tag/v' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
+    $version = $version.Substring(1)
+
+    @{
+        Version = $version
+        URL64   = "https://github.com/0xd4d/dnSpy/releases/download/v$version/dnSpy-net472.zip"
+    }
+}
+
+try {
+    update
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
