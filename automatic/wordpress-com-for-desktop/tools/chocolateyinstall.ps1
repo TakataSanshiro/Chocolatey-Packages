b49fcc8940c9a6f2f65efd048d0f4e1d583ac145
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-05 23:19:09 +0900

    addd AU to wordpress-com-for-desktop and update one to 4.1.0

---
+++ automatic/wordpress-com-for-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'wordpress-com-for-desktop'
+  installerType  = 'exe'
+  url            = 'https://github.com/Automattic/wp-desktop/releases/download/v4.1.0/WordPressDesktop-Setup-4.1.0.exe'
+  checksum       = '5ba86a51fce06a608c76b71adc689a8054b5ce358ac4df6090ab303bd4fc7082'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'WordPress.com Desktop*'
+}
+Install-ChocolateyPackage @packageArgs
