09900ca8b37a741a939b6dddfd3e554d11723a88
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-09 02:00:26 +0900

    add AU to wire and update one to 3.9.2928

---
+++ automatic/wire/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'wire'
+  installerType  = 'exe'
+  url            = 'https://github.com/wireapp/wire-desktop/releases/download/windows/3.9.2928/WireSetup.3.9.2928.exe'
+  checksum       = '0c38350fa2825dc99059eca69ca2a5d19c37c4f79e32b996e3f3a240a228f009'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0)
+  softwareName   = 'Wire'
+}
+Install-ChocolateyPackage @packageArgs
