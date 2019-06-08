2064e6a2bc82e6dc787569f8ca7a3da43b601c40
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-09 01:36:58 +0900

    add AU to quicklook and update one to 3.6.5

--- quicklook/tools/chocolateyinstall.ps1
+++ quicklook/tools/chocolateyinstall.ps1
@@ -1,18 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'quicklook'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/QL-Win/QuickLook/releases/download/3.6.4/QuickLook-3.6.4.msi'
-
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'msi'
-  url           = $url
-  silentArgs  = "/qn"
-  softwareName  = '*quicklook*'
-  checksum      = '6EC2EEC87060EB660FF12A2C66013331D84D6BCE5A7DCE41E97D6AC3568FF9F7'
+  packageName   = 'quicklook'
+  installerType = 'msi'
+  url           = 'https://github.com/QL-Win/QuickLook/releases/download/3.6.5/QuickLook-3.6.5.msi'
+  silentArgs    = '/qn'
+  softwareName  = 'quicklook*'
+  checksum      = '98252c187fd37810c1638e49b3a283fbd97b42c1f21784bd808db4831aa971f1'
   checksumType  = 'sha256'
   validExitCodes = @(0, 3010, 1641)
 }
-
 Install-ChocolateyPackage @packageArgs
