d386ab3401cd6cf96a01023a94b22145648fe86b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-12 01:50:30 +0900

    add AU to tagspaces and update one to 3.1.4

---
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'tagspaces'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v3.1.4/tagspaces-win-x64-3.1.4.zip'
+  checksum       = '165763b68c7964912fe0a058e94c953a5115bb53ca321cf6ded55d5e22db4dad'
+  checksumType   = 'sha256'
+  softwareName   = 'Tagspaces'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyZipPackage @packageArgs
