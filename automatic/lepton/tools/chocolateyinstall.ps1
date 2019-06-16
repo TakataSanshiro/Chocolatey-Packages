0d8ee00b527d32f0461b301b17efa0737ae43ff9
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-17 02:10:38 +0900

    add AU to lepton and update one to 1.8.0

---
+++ automatic/lepton/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'lepton'
+  installerType  = 'exe'
+  url            = 'https://github.com/hackjutsu/Lepton/releases/download/v1.8.0/Lepton-Setup-1.8.0.exe'
+  checksum       = '11c9deafcd57a1de105ff2fe369bf0b2ee8b1027d2bd0541c909f66a99223af6'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Lepton'
+}
+Install-ChocolateyPackage @packageArgs
