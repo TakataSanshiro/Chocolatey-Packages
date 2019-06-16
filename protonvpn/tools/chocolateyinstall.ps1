20efdf469fdf713f8ca51a03af7e4069a8e29b64
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-17 02:00:14 +0900

    update protonvpn to 1.8.1

--- protonvpn/tools/chocolateyinstall.ps1
+++ protonvpn/tools/chocolateyinstall.ps1
@@ -1,22 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName    = 'protonvpn'
-$installerType  = 'exe'
-$url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.7.4.exe'
-$checksum       = '05387CED342C4BA1275FAA5126279CFDC15A2FCCBC9BC1A8A3945CEBC182642C'
-$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$silentArgs     = '/quiet'
-$validExitCodes = @(0)
-
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = $installerType
-  url           = $url
-  silentArgs    = $silentArgs
-  validExitCodes= $validExitCodes
+  packageName   = 'protonvpn'
+  fileType      = 'exe'
+  url           = 'https://protonvpn.com/download/ProtonVPN_win_v1.8.1.exe'
+  silentArgs    = '/quiet'
+  validExitCodes= @(0)
   softwareName  = 'ProtonVPN*'
-  checksum      = $checksum
+  checksum      = '29B7DCAC74FDC211D7F31AF228DCCB6D58CB0C62CA2C20A21344D21EE05D0970'
   checksumType  = 'sha256'
 }
-
 Install-ChocolateyPackage @packageArgs
