5af96b74ad1ddc6a2d73e156081966720ac1f526
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-23 02:37:27 +0900

    add AU to tinc and update one to 1.0.35

---
+++ automatic/tinc/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'tinc'
+  installerType  = 'exe'
+  url            = 'https://www.tinc-vpn.org/packages/windows/tinc-1.0.35-install.exe'
+  checksum       = '0f8b6d9129d0f8e4603b4e96be70d6ba518bb2e2e6c695d6bb83e92d80d8c685'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'tinc'
+}
+Install-ChocolateyPackage @packageArgs
