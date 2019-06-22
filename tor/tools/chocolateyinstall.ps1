ff35b733d735e0c549d29437930c0874530da0e6
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-23 03:06:21 +0900

    change tor url from www to archive

--- tor/tools/chocolateyinstall.ps1
+++ tor/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName    = 'tor'
-  url            = 'https://www.torproject.org/dist/torbrowser/8.5/tor-win32-0.3.5.8.zip'
+  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/8.5.3/tor-win32-0.3.5.8.zip'
   checksum       = '012D5495AE9CAC276414DFA15E8BE558542B447FA4EFE4D380B0061A992DBE0A'
   checksumType   = 'sha256'
   softwareName   = 'Tor'
