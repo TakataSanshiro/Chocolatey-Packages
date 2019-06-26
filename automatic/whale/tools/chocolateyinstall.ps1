adca102c6eeed7837fc313ed3b9bf90b286701b8
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-06-27 02:28:29 +0900

    add AU to whale and update one to 0.17.0

---
+++ automatic/whale/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'whale'
+  installerType  = 'exe'
+  url            = 'https://github.com/1000ch/whale/releases/download/v0.17.0/Whale.Setup.0.17.0.exe'
+  checksum       = '94aeece603de77c617a0c8f6d9dfe36fa6101648091de0a032732aab3da40e15'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Whale'
+}
+Install-ChocolateyPackage @packageArgs
