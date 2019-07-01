ee82e0195e85a892736007ba34026d1483d7740a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-07-02 03:08:11 +0900

    update the wpd checksum

--- wpd/tools/chocolateyinstall.ps1
+++ wpd/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'wpd'
-$url = 'https://wpd.app/get/latest.zip' 
-$checksum = 'A4537EA6AE73DE5D29367B802C2DE0D191109BC7E210BF9E46E6BA70C1628E74'
+$packageName  = 'wpd'
+$url          = 'https://wpd.app/get/latest.zip' 
+$checksum     = 'B94412C79111DFAD5CE68F69ACC6DF0667A3FD740ACB31F0CEF75AEF80810109'
 $checksumType = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
