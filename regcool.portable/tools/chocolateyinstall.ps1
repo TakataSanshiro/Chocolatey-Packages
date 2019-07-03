4405c0ade63bef6279540e3e2ad129ee4d787b17
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-07-04 01:12:45 +0900

    update regcool.portable to 1.106

--- regcool.portable/tools/chocolateyinstall.ps1
+++ regcool.portable/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'regcool.portable'
-$url = 'https://kurtzimmermann.com/files/RegCoolX32.zip' 
-$url64 = 'https://kurtzimmermann.com/files/RegCoolX64.zip'
-$checksum = 'DF5DFF578AAB42CEDC431AC54BA8C8F2103EDC1E000D93DD6E267880C8961BF6'
-$checksum64 = '2D0C0BD5EA1BB9C247C8093B77EC85F4F50963D9FA60E8B5E5DD649A3B147E89'
-$checksumType = 'sha256'
+$packageName    = 'regcool.portable'
+$url            = 'https://kurtzimmermann.com/files/RegCoolX32.zip' 
+$url64          = 'https://kurtzimmermann.com/files/RegCoolX64.zip'
+$checksum       = 'C6A4C8C6508C166FF34C0F191435D148F93B3A25B4ECE3C3EF1BC610E9CF19DF'
+$checksum64     = '3C91FAF526169C2A73A6388B6BDE05033D7518CC228C8A00434E413B90066B4D'
+$checksumType   = 'sha256'
 $checksumType64 = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
