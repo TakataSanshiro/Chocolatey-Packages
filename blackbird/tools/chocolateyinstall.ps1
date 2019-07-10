9fb212ea1ac1953fbbc711216eca8f8d1b44ac28
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-07-11 01:38:02 +0900

    update blackbird to 1.0.7
    remove no need $path

--- blackbird/tools/chocolateyinstall.ps1
+++ blackbird/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'blackbird'
-$url = 'https://www.getblackbird.net/download/Blackbird_v1.0.6_x32.zip' 
-$url64 = 'https://www.getblackbird.net/download/Blackbird_v1.0.6_x64.zip'
-$checksum = '6CD237F2962140A9E581DCFF87B6B39219E7C6E95336B5E896C407864FB14811'
-$checksum64 = '7B605C6DFAABD35C675F0E65262363F26F549F2E0A4E9FCE539030691E1EE8BD'
-$checksumType = 'sha256'
+$packageName    = 'blackbird'
+$url            = 'https://getblackbird.net/download/Blackbird_v1.0.7_x32.zip' 
+$url64          = 'https://getblackbird.net/download/Blackbird_v1.0.7_x64.zip'
+$checksum       = '58AB14797BD7B136F7C3397431EFC85B19167984001807EA8F7A18EFF64DF3B9'
+$checksum64     = '90152D6B663923BCA8DAED18BA65B84FBD9CF6DC31C2ADC23E7FE72F7E83B21F'
+$checksumType   = 'sha256'
 $checksumType64 = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$options = @{
-  Headers = @{
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$options        = @{
+    Headers = @{
     Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
     'Accept-Charset' = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3';
     'Accept-Language' = 'en-US,en;q=0.5';
@@ -17,24 +17,16 @@ $options = @{
   }
 }
 
-$is64bit = Get-ProcessorBits 64
-if ($is64bit) {
-	$path = "$toolsDir\blackbird_v1.0.4_64\blackbird.exe"
-	}
-	else{
-	$path = "$toolsDir\blackbird_v1.0.4_32\blackbird.exe"
-}
-
 $packageArgs = @{
-  packageName   = $packageName
-  url           = $url
-  Url64bit      = $url64
-  checksum      = $checksum
-  Checksum64    = $checksum64
-  checksumType  = $checksumType
-  ChecksumType64= $checksumType64
-  UnzipLocation = $toolsDir
-  Options       = $options
+  packageName    = $packageName
+  url            = $url
+  Url64          = $url64
+  checksum       = $checksum
+  Checksum64     = $checksum64
+  checksumType   = $checksumType
+  ChecksumType64 = $checksumType64
+  UnzipLocation  = $toolsDir
+  Options        = $options
 }
 
 Install-ChocolateyZipPackage @packageArgs
