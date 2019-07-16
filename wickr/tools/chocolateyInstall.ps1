bd098f093fe6f1f40aac4a0bf020754095183473
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-07-17 01:33:37 +0900

    update wickr to 5.22.3

--- wickr/tools/chocolateyInstall.ps1
+++ wickr/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = 'wickr'
-$installerType = 'msi'
-$url = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.18.7.msi'
-$silentArgs = '/qb'
+﻿$packageName    = 'wickr'
+$installerType  = 'msi'
+$url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.22.3.msi'
+$silentArgs     = '/qb'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -Checksum DF8E8C460BF4BD0E46755A6FF3F8304ABB7D3EFCCF746A7EF1BD6BF2DC6046CB -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -Checksum 4F70A33A2D2FC88C1FA5D96632625A8C65D97772F600C9797369500B0B9B1C58 -ChecksumType sha256
