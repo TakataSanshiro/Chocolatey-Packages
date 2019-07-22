e918352330c2a18d898de0af55c7dcdd90d074c7
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-07-23 02:47:49 +0900

    add AU to simplenote and update one to 1.6.0

---
+++ automatic/simplenote/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'simplenote'
+  installerType  = 'exe'
+  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.6.0/Simplenote-win-1.6.0.exe'
+  checksum       = '86204b4413d8e77f4cedc77754e14788138e676377cf8d1d1940a49e76184371'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Simplenote*'
+}
+Install-ChocolateyPackage @packageArgs
