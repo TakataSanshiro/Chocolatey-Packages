5cf171e2a877fdaf22babbb11962300de17ad8fb
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-08-09 01:16:51 +0900

    update insomnia-rest-api-client to 6.6.2

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/getinsomnia/insomnia/releases/download/v6.5.4/Insomnia.Setup.6.5.4.exe'
-  checksum      = 'a41f606b5a1731ef6c50fb3aa0aba8b729726592c9abccdf98ea05b98b07f6fa'
+  url           = 'https://github.com/getinsomnia/insomnia/releases/download/v6.6.2/Insomnia.Setup.6.6.2.exe'
+  checksum      = '695559abf304d84a920c613bba6a982ef82e0bb9b3bc060f09c0266007587a96'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
