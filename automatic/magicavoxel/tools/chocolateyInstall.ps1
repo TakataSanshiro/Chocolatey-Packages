dd0f401ce88fd197a136082c6ba703bcdc394e6e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-08-13 02:29:00 +0900

    fix typo

--- automatic/magicavoxel/tools/chocolateyInstall.ps1
+++ automatic/magicavoxel/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType" `
-							               -Url64bit "$url64" `
+							               -Url64 "$url64" `
 							               -Checksum64 "$checksum64" `
 							               -ChecksumType64 "$checksumType64"
 							 
