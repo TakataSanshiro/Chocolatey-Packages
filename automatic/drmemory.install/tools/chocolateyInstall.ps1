7104f5ed4884168128d0d2f0d7c5cfb214190cbc
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-08-18 02:20:56 +0900

    add AU to drmemory.install and update one to 2.2.0

---
+++ automatic/drmemory.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'drmemory.install'
+  installerType  = 'msi'
+  url            = 'https://github.com/DynamoRIO/drmemory/releases/download/release_2.2.0/DrMemory-Windows-2.2.0-1.msi'
+  checksum       = '4847a0134e998ded9f826910c00b998e1f32f814e1cbd2873120a59017b433e0'
+  checksumType   = 'sha256'
+  silentArgs     = '/qn'
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'drmemory*'
+}
+Install-ChocolateyPackage @packageArgs
