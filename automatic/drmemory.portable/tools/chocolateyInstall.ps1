25877007c8acff43038a055c2a292c3e68cdf065
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-08-18 02:16:34 +0900

    add AU to drmemory.portable and update one to 2.2.0
    I messed up. The drmemory cronbuild and releases are different. Not dropped msi installer version.

---
+++ automatic/drmemory.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'drmemory.portable'
+  url            = 'https://github.com/DynamoRIO/drmemory/releases/download/release_2.2.0/DrMemory-Windows-2.2.0-1.zip'
+  checksum       = 'fb94511f05375b669f7ee90f374483e2eb199b82004404753fe10a895ca4d9c4'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'drmemory.portable*'
+}
+Install-ChocolateyZipPackage @packageArgs
