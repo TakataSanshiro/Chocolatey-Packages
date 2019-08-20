3d5b2343d2a78e331130a47577f28419c907f964
C4illin <c@illin.cf>
2019-08-20 20:45:33 +0200

    added AirExplorer

--- airexplorer/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'airexplorer' 
-$installerType = 'exe' 
-$url = 'http://www.airexplorer.net/downloads/AirExplorer-Installer.exe' 
-$silentArgs = '/VERYSILENT'
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"   -validExitCodes $validExitCodes -Checksum 7C1AF260C2B38BD3B7612944BD4029088F9233977507F793D43084845289CE01 -ChecksumType sha256
---
+++ automatic/airexplorer/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName            = 'airexplorer'
+  fileType               = 'EXE'
+  url                    = 'https://www.airexplorer.net/downloads/AirExplorer-OnlineInstaller.exe'
+  checksum               = '1d7f0bb17407cd2b09ce02f7e83cd75df4796020ed2e64bb1530ab154e0c7d08'
+  checksumType           = 'sha256'
+  softwareName           = 'AirExplorer*'
+  silentArgs             = '/S'
+  validExitCodes         = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
