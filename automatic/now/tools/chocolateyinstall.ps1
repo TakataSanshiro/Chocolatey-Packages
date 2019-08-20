247427f44b467ca12de1186b42238d9659f8284a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-08-21 03:00:42 +0900

    update now to 5.1.0

--- automatic/now/tools/chocolateyinstall.ps1
+++ automatic/now/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'now'
   installerType  = 'exe'
-  url            = 'https://github.com/zeit/now-desktop/releases/download/5.0.1/now-desktop-Setup-5.0.1.exe'
-  checksum       = '60cedb7b7d45889174e7000d3fff35562ef765b23e34f52fbcc1eeb35b08f778'
+  url            = 'https://github.com/zeit/now-desktop/releases/download/5.1.0/now-desktop-Setup-5.1.0.exe'
+  checksum       = '29070b7d119814a842b5f6c34a5c8d9728bc091ea4b62a0da5f6f905e8071fa3'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
