0ff8ddf0c04eae2a788c43f5e45434cae8d6bdb8
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-08-22 03:15:42 +0900

    takeover terminus from dbkblk
    add AU to terminus and update one to 1.0.88

---
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'terminus'
+  installerType  = 'exe'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.88/terminus-1.0.88-setup.exe'
+  checksum       = '210832fb26d544ec9f3eb1c9f714524c781b52a044e6e3b765a5c45b0105b6d1'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Terminus*'
+}
+Install-ChocolateyPackage @packageArgs
