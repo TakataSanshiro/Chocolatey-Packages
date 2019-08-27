ad267935044b74e6dc68850eedcfb55edcdf094c
Caburn <caburnit@gmail.com>
2019-08-27 15:33:19 +0100

    update uget url and checksum to 2.2.2 stable

--- uget/tools/chocolateyinstall.ps1
+++ uget/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 $packageName = 'uget'
-$url = 'https://downloads.sourceforge.net/project/urlget/uget (stable)/2.2.1/uget-2.2.1-win32+gtk3.7z?r=&ts=1522519163&use_mirror=svwh' 
-$checksum = 'A722BD02A6D77AC24B8BB7D1B3A6C7F37CA4DBAB40859264B1E50DEDBCDA584A'
+$url = 'https://downloads.sourceforge.net/project/urlget/uget%20%28stable%29/2.2.2/uget-2.2.2-win32%2Bgtk3.7z?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Furlget%2Ffiles%2Fuget%2520%2528stable%2529%2F2.2.2%2Fuget-2.2.2-win32%252Bgtk3.7z%2Fdownload&ts=1566914697&use_mirror=svwh' 
+$checksum = '340F7C4622428704B76BB34F93BC4129313E89D66071D73FA0CCE2DC259F48A3'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
@@ -10,4 +10,4 @@ Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"					 
+                             -ChecksumType "$checksumType"
