41f8192223fb58d7f8ae5cea577e337473f12c74
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-08-31 01:10:14 +0900

    update kpcli to 3.3

--- kpcli/tools/chocolateyinstall.ps1
+++ kpcli/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName  = 'kpcli'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile  = join-path $toolsDir 'kpcli.exe'
-$url          = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.2.exe/download'
-$checksum     = '84732CE0F058B9E02DD72A28AD2F3E8A885B267B63F9D8C6AD83AF35531EDF17'
+$url          = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.3.exe/download'
+$checksum     = '7E80B47E5CE111B949849D900E4137AC29C3FF094956778609D0A6527365CC22'
 $checksumType = 'sha256'
 
 Get-ChocolateyWebFile $packageName "$installFile" "$url" -checksum $checksum -checksumType $checksumType
