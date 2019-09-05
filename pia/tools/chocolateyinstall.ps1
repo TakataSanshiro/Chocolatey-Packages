269cc56a2a1cbfee4795f4d13c2d4462a077edd8
ionred <ionred@users.noreply.github.com>
2019-09-04 23:02:41 -0500

    update pia to 140.0
    * Update pia.nuspec
    * Update chocolateyinstall.ps1

--- pia/tools/chocolateyinstall.ps1
+++ pia/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $packageName    = 'pia'
 $installerType  = 'exe'
-$url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-1.3.3-02880.exe'
-$checksum       = '034E93989E93EF6CC658ED5E9D9FACE7A5C312003181C56C9CDD53F6C05F10F7'
+$url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-1.4-03180.exe'
+$checksum       = 'B96EA420AAF92367D3C68B8E594B17D48E30F7912172E8EFA2CF46ED4D6FE300'
 $checksumType   = 'sha256'
-$url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-1.3.3-02880.exe'
-$checksum64     = 'E70A38C0B489CB6475943F88EC7C895DD6235A43BD31828C115AAB85E3C57B2B'
+$url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-1.4-03180.exe'
+$checksum64     = '2EC4152D5F400EC3D91AFBCC742EE2B4BE54E507F1AF4F2C1934DFA8F6921534'
 $checksumType64 = 'sha256'
 $toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs     = ''
