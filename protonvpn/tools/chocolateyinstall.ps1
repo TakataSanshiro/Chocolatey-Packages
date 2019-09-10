17a358147653df75d30068cc9ac48a91c33358d5
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-09-11 00:32:42 +0900

    update protonvpn to 1.10.1

--- protonvpn/tools/chocolateyinstall.ps1
+++ protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = 'protonvpn'
   fileType      = 'exe'
-  url           = 'https://protonvpn.com/download/ProtonVPN_win_v1.9.2.exe'
+  url           = 'https://protonvpn.com/download/ProtonVPN_win_v1.10.1.exe'
   silentArgs    = '/quiet'
   validExitCodes= @(0)
   softwareName  = 'ProtonVPN*'
-  checksum      = 'B3EED41D100C32700E48B7A365CBC22E7AEE78A0E62C53D58E5B02771BB8776B'
+  checksum      = 'C322AA76BB599F5C13EC94A680171CBFEEF9120C5FAD700FC7E1F011AAEBB0F3'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
