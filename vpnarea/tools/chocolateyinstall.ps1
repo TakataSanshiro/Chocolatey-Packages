94c61f9c75c5d41d6b1f620aa146f83d544e42cc
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-09-11 00:53:42 +0900

    update the vpnarea checksum

--- vpnarea/tools/chocolateyinstall.ps1
+++ vpnarea/tools/chocolateyinstall.ps1
@@ -1,22 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName    = 'vpnarea'
-$installerType  = 'exe'
-$url            = 'https://vpnarea.com/VPNArea.exe'
-$checksum       = '995FF02E2A4B0B706C4DBDDC3F9C874F748261A228C4B72CDCA0BD75D39B9CF7'
-$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$silentArgs     = '/S'
-$validExitCodes = @(0)
-
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = $installerType
-  url           = $url
-  silentArgs    = $silentArgs
-  validExitCodes= $validExitCodes
+  packageName   = 'vpnarea'
+  fileType      = 'exe'
+  url           = 'https://vpnarea.com/VPNArea.exe'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
   softwareName  = 'vpnarea*'
-  checksum      = $checksum
+  checksum      = 'A49F33C66D2737C3E47BC631C274F545B1EBFDFD0DBF6974582621C176F0EE21'
   checksumType  = 'sha256'
 }
-
 Install-ChocolateyPackage @packageArgs
