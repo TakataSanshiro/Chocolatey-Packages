935103a6809c346519b6b1cd685cbdc789a95217
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-09-24 02:02:19 +0900

    update quickviewer to 1.1.7

--- automatic/quickviewer/tools/chocolateyinstall.ps1
+++ automatic/quickviewer/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageArgs = @{
   packageName    = 'quickviewer'
-  url            = 'https://github.com/kanryu/quickviewer/releases/download/v1.1.6/QuickViewer-portable-1.1.6-x86.zip'
-  url64          = 'https://github.com/kanryu/quickviewer/releases/download/v1.1.6/QuickViewer-portable-1.1.6-x64.zip' 
-  checksum       = 'a415cf4d27e22a544a2ae8b4a781dfc50bd37aa6d8b6e769997a4f938d3775e9'
-  checksum64     = '2d8cd19f98e93c88f949041120f86f6973e20beb67322ca1554ba206af406234'
+  url            = 'https://github.com/kanryu/quickviewer/releases/download/v1.1.7/QuickViewer-portable-1.1.7-x86.zip'
+  url64          = 'https://github.com/kanryu/quickviewer/releases/download/v1.1.7/QuickViewer-portable-1.1.7-x64.zip' 
+  checksum       = 'f6b0ff66d86b0f5989b8a474314400797d6e65a995b5c28b12946cfc17295c33'
+  checksum64     = 'd5ac98d21a53e46b220f06c2cb4edcb35f04fedbca7c51fd1cff682d110b4eb5'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
