5d18fabfb31e551b52568a5503582c6618316442
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-10-03 01:27:14 +0900

    update onlyoffice to 5.4.1

--- automatic/onlyoffice/tools/chocolateyInstall.ps1
+++ automatic/onlyoffice/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = 'onlyoffice'
   fileType       = 'exe'
-  url            = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-5.3.5/DesktopEditors_x86.exe'
-  url64          = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-5.3.5/DesktopEditors_x64.exe'
+  url            = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-5.4.1-2/DesktopEditors_x86.exe'
+  url64          = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-5.4.1-2/DesktopEditors_x64.exe'
   softwareName   = 'Onlyoffice*'
-  checksum       = '546199e2bf5bb7880ff2762819525f591d0c82626a4fc1729cfd3ca746415565'
+  checksum       = 'bab0f8e6a6a466b701f1e047a92fe48440ac177426461d9269a5d11e8cefd5ad'
   checksumType   = 'sha256'
-  checksum64     = '356002deff774aa4bec1b382b3ac7495a34ee8087e02d29d7e1be73091051b96'
+  checksum64     = '0bf65e4c8b19115f1426e1e53b91eb50fcc253d80b89d8dd906a45777cc844d6'
   checksumType64 = 'sha256'
   silentArgs     = '/s /S /q /Q /quiet /silent /SILENT /VERYSILENT'
   validExitCodes = @(0)
