4453174046f6de5cc0161c2e57caf810938be3be
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-10-11 01:06:40 +0900

    update wingidepersonal to 7.1.2

--- wingidepersonal/tools/chocolateyInstall.ps1
+++ wingidepersonal/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'wingidepersonal' 
 $installerType  = 'exe' 
-$url            = 'https://wingware.com/pub/wing-personal/7.1.1.0/wing-personal-7.1.1.0.exe' 
+$url            = 'https://wingware.com/pub/wing-personal/7.1.2.0/wing-personal-7.1.2.0.exe' 
 $silentArgs     = '/VERYSILENT' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 8940B35807EA014270F5B9F772FEE1597011FD35821263A182A4928081D7CA6F -ChecksumType sha256
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 77F21076E90419A83A67FAFD76DC1E8BB3241F34D0EA17E5F60C132AF7E8A3F7 -ChecksumType sha256
