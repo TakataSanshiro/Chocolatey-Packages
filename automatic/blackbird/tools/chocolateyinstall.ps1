a3c8f4c3f113ed5cc3fe16b851f5323ed3a09014
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-10-19 01:34:27 +0900

    add AU to blackbird and update one to 1.0.79.2

---
+++ automatic/blackbird/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'blackbird'
+  url            = 'https://www.getblackbird.net/download/Blackbird_v1.0.79.2%5Fx32.zip' 
+  Url64          = 'https://www.getblackbird.net/download/Blackbird_v1.0.79.2%5Fx64.zip'
+  checksum       = 'd49951c28db58e53ab86f40b8abb44542db3d544d8864440daf07179c3e1712a'
+  Checksum64     = '85a5dd955c755f3b049955b06ef2d2a1bcc7955e5289ce48ffae0861388dbc87'
+  checksumType   = 'sha256'
+  ChecksumType64 = 'sha256'
+  UnzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Options        = @{
+    Headers = @{
+    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+    'Accept-Charset' = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3';
+    'Accept-Language' = 'en-US,en;q=0.5';
+    Referer = 'https://www.getblackbird.net/download/';
+    }
+  }
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/blackbird/update.ps1
@@ -0,0 +1,34 @@
+import-module au
+
+$releases = 'https://www.getblackbird.net/download/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.links.href -match 'Blackbird_v' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
+    $version = $version | % { $_ -split '_' | select -Last 2 }
+    $version = $version[0].Substring(1)
+
+    @{
+        Version = $version
+        URL32   = "https://www.getblackbird.net/download/Blackbird_v$version%5Fx32.zip"
+        URL64   = "https://www.getblackbird.net/download/Blackbird_v$version%5Fx64.zip"
+    }
+}
+
+try {
+    update
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
