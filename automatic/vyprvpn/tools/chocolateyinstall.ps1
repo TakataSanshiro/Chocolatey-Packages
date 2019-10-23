014008c50cc1a635bc74c113f08a001aaf55eaf0
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-10-24 01:11:49 +0900

    add an uncomplete AU to vyprvpn and update one to 3.0.0.9646
    The automatic checksum is broken.

---
+++ automatic/vyprvpn/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$ahkExe          = 'AutoHotKey'
+$ahkFile         = Join-Path $toolsDir "vyprvpnInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName    = 'vyprvpn'
+  fileType       = 'exe'
+  url            = 'https://downloads-goldenfrog.netdna-ssl.com/downloads/vyprvpn/desktop/windows/production/3.0.0.9646/VyprVPN-3.0.0.9646-installer.exe'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'vyprvpn*'
+  checksum       = 'D04198AF73EAB7CEE3D122ED38C5F2C9E72A7823A6891A0E25E6BB137A71A9FA'
+  checksumType   = 'sha256'
+  toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/vyprvpn/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'https://www.vyprvpn.com/vpn-apps/vpn-for-windows/download'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.links.href -match 'vyprvpn/desktop/windows/' | Select -First 1 | % { $_ -split '/' | select -Last 2 }
+    $version = $version[0]
+    
+    @{
+        Version = $version
+        URL32   = "https://downloads-goldenfrog.netdna-ssl.com/downloads/vyprvpn/desktop/windows/production/$version/VyprVPN-$version-installer.exe"
+    }
+}
+
+try {
+    update
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
