5a33ed1f3af381ff8cd43fa131bf9328a651f881
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-10-27 03:00:01 +0900

    update postbox to 7.0.7

--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.6-win32.exe'
-  checksum      = '596ebbb718eb267fff41b8481ba6cdc3fc7272218c0fb9b336166d57e71c78ea'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.7-win32.exe'
+  checksum      = '359fdcbc7e2c45a6945bfc507c1616274a6afb337a3e352ca6a477baf1351324'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
