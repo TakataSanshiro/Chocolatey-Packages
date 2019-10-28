0119ead1c7041b1d0cd71ef3c120e873a7f0f3bc
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-10-29 00:26:28 +0900

    add AU to tor and update one to 0.4.1.6

---
+++ automatic/tor/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'tor'
+  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/9.0/tor-win32-0.4.1.6.zip'
+  checksum       = '6a46065bf3becda2cc15cbe8d426d35a4d8d381c4b98360cb981b972094d582c'
+  checksumType   = 'sha256'
+  softwareName   = 'Tor'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/tor/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+$releases = 'https://www.torproject.org/download/tor/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.links.href -match 'tor-win' -notmatch 'alpha' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
+    $version = $version | % { $_ -split '-' | select -Last 1 }
+    $length  = $version.length
+    $version = $version.Substring(0, $length-4)
+
+    $torbrowserversion = $download_page.links.href -match 'tor-win' -notmatch 'alpha' | Select -First 1 | % { $_ -split '/' | select -Last 2 }
+    $torbrowserversion = $torbrowserversion[0]
+
+    @{
+        Version = $version
+        URL64   = "https://archive.torproject.org/tor-package-archive/torbrowser/$torbrowserversion/tor-win32-$version.zip"
+    }
+}
+
+try {
+    update
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
