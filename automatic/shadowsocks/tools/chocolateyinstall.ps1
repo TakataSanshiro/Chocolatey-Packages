9de137474703ddf9e3ca404bebf4d33c2ece5038
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-02 00:54:56 +0900

    update shadowsocks to 4.1.8.0

--- automatic/shadowsocks/tools/chocolateyinstall.ps1
+++ automatic/shadowsocks/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'shadowsocks'
-  url            = 'https://github.com/shadowsocks/shadowsocks-windows/releases/download/4.1.7.1/Shadowsocks-4.1.7.1.zip'
-  checksum       = 'e3584150cc2cc74f7582e84f91ae9c258e63b67e722b0219a6378212c03ee85a'
+  url            = 'https://github.com/shadowsocks/shadowsocks-windows/releases/download/4.1.8.0/Shadowsocks-4.1.8.0.zip'
+  checksum       = '14f0840dbabc554d43cf3021e04f7b11c7285bd85ee13dfb9d59c0a942bcd515'
   checksumType   = 'sha256'
   softwareName   = 'Shadowsocks'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
