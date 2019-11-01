51026e9e8d2cd5f4753f4509ce435550037cc876
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-02 00:52:20 +0900

    update wire to 3.11.3259

--- automatic/wire/tools/chocolateyinstall.ps1
+++ automatic/wire/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wire'
   installerType  = 'exe'
-  url            = 'https://github.com/wireapp/wire-desktop/releases/download/windows/3.10.3138/WireSetup.3.10.3138.exe'
-  checksum       = '8d09aebac24f54dcd1416aba0d18426b2fd4b477ac1b123d5728ac2e58373136'
+  url            = 'https://github.com/wireapp/wire-desktop/releases/download/windows/3.11.3259/WireSetup.3.11.3259.exe'
+  checksum       = '3a69dbdffd798b53eddb22d17cb52e10204bf550df8ffe10e97893297016f9d9'
   checksumType   = 'sha256'
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0)
