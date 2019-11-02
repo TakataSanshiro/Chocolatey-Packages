1d1168dcb87ee1eb82c06896e99dfe9be7ac77f2
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-03 01:04:19 +0900

    update blackbird to 1.0.79.3

--- automatic/blackbird/tools/chocolateyinstall.ps1
+++ automatic/blackbird/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageArgs = @{
   packageName    = 'blackbird'
-  url            = 'https://www.getblackbird.net/download/Blackbird_v1.0.79.2%5Fx32.zip' 
-  Url64          = 'https://www.getblackbird.net/download/Blackbird_v1.0.79.2%5Fx64.zip'
-  checksum       = 'd49951c28db58e53ab86f40b8abb44542db3d544d8864440daf07179c3e1712a'
-  Checksum64     = '85a5dd955c755f3b049955b06ef2d2a1bcc7955e5289ce48ffae0861388dbc87'
+  url            = 'https://www.getblackbird.net/download/Blackbird_v1.0.79.3%5Fx32.zip' 
+  Url64          = 'https://www.getblackbird.net/download/Blackbird_v1.0.79.3%5Fx64.zip'
+  checksum       = 'd66d16e341f3e6e3c9caf0d32e4080c76315ef50dfbb2576aef3c5e274f27b3c'
+  Checksum64     = '8c89913809b692dc8757ac80b406f333ef9fb0f2a1d5ef9323d9d0ad52df59ac'
   checksumType   = 'sha256'
   ChecksumType64 = 'sha256'
   UnzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
