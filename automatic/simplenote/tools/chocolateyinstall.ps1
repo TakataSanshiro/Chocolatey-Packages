ea7528345fb746743734b792888efb381cdaba3f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-08 01:31:22 +0900

    update simplenote to 1.10.0

--- automatic/simplenote/tools/chocolateyinstall.ps1
+++ automatic/simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'simplenote'
   installerType  = 'exe'
-  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.9.1/Simplenote-win-1.9.1.exe'
-  checksum       = 'e7a6d95c304e1c2d6375681c19d26fd871854db1986fc3cde79d7066614df334'
+  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v1.10.0/Simplenote-win-1.10.0.exe'
+  checksum       = '858b9ae8c0de57715af89d84d4e7854a52f3f7f6fcfcacab1ec78a612a538834'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
