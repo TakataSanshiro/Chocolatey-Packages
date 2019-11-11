fa61d9ceec8ceb7400fafd550e68fa31532b9247
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-12 02:40:43 +0900

    add AU to vpnarea and update one to 2.1.0.20191112

---
+++ automatic/vpnarea/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'vpnarea'
+  fileType       = 'exe'
+  url            = 'https://vpnarea.com/VPNArea.exe'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'VPNArea*'
+  checksum       = '32d6b4484b656c7130e074254bfaccc177ba76b8f7fe5c484bd4f7e5b039a8e9'
+  checksumType   = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/vpnarea/update.ps1
@@ -0,0 +1,43 @@
+import-module au
+
+$url = 'https://vpnarea.com/VPNArea.exe'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            #  "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum = Get-RemoteChecksum $Latest.URL
+}
+
+function global:au_GetLatest {
+    $current_checksum = (gi .\tools\chocolateyInstall.ps1 | sls '\bchecksum\b') -split "=|'" | Select -Last 1 -Skip 1
+
+    if ($current_checksum -ne $Latest.Checksum) {
+        Write-Host 'Remote checksum is different then the current one, forcing update'
+        
+        $global:au_old_force = $global:au_force
+        $global:au_force = $true
+
+        Write-Host $url
+ 
+        $temp_file = $env:TEMP + 'VPNArea.exe'
+        Invoke-WebRequest $url -OutFile $temp_file
+        Write-Host $temp_file
+        
+        $version = (Get-Command $temp_file).Version
+        Write-Host $version
+     }
+
+    @{
+        Version = $version
+        URL     = 'https://vpnarea.com/VPNArea.exe'
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl
