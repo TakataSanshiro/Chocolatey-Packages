00fc52fbd8848431c9239f13abe931880c684bf4
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-15 02:32:04 +0900

    update webrecorderplayer to 1.8.0

--- automatic/webrecorderplayer/tools/chocolateyinstall.ps1
+++ automatic/webrecorderplayer/tools/chocolateyinstall.ps1
@@ -2,18 +2,18 @@
 
 $is64bit = Get-ProcessorBits 64
 if ($is64bit) {
-  $path = "$toolsDir\webrecorder-player-win-x86_64-1.7.0.exe"
+  $path = "$toolsDir\webrecorder-player-win-x86_64-1.8.0.exe"
   }
 else{
-  $path = "$toolsDir\webrecorder-player-win-x86-1.7.0.exe"
+  $path = "$toolsDir\webrecorder-player-win-x86-1.8.0.exe"
 }
 
 $packageArgs = @{
   packageName      = 'webrecorderplayer'
-  url              = 'https://github.com/webrecorder/webrecorder-player/releases/download/v1.7.0/webrecorder-player-win-x86-1.7.0.exe'
-  url64            = 'https://github.com/webrecorder/webrecorder-player/releases/download/v1.7.0/webrecorder-player-win-x86_64-1.7.0.exe'
-  checksum         = '9fe858b7ae938bbaffce8e4de5898274f8f8543627875634d1d0ff61af2bbc7a'
-  checksum64       = '135b6a45ee936679cd5e18e6ee7e7570e22781b6338cafcd933f252a15d066b0'
+  url              = 'https://github.com/webrecorder/webrecorder-player/releases/download/v1.8.0/webrecorder-player-win-x86-1.8.0.exe'
+  url64            = 'https://github.com/webrecorder/webrecorder-player/releases/download/v1.8.0/webrecorder-player-win-x86_64-1.8.0.exe'
+  checksum         = '5de20aa1f535be786caaff02cc4443fde065d0dc6bdc003ec1900a223fae7cd9'
+  checksum64       = '32f7270ae5c02f2b301e0691012523e778fd52c5553d3b484124e2a74fa2a5c1'
   checksumType     = 'sha256'
   checksumType64   = 'sha256'
   toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
