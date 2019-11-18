b2dddcbcebb72f66616e7f51c2df76bdb90f5aac
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-19 01:10:27 +0900

    add AU to wingide and update one to 7.1.3

---
+++ automatic/wingide/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'wingide'
+  installerType  = 'exe'
+  url            = 'https://wingware.com/pub/wingpro/7.1.3.0/wingpro-7.1.3.0.exe'
+  checksum       = 'c0c02b23674dc6798ae7885a993555e618d4c6272c5825bde7fa5ac4749d49dd'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'WingIDE'
+  toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
--- wingide/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName    = 'wingide' 
-$installerType  = 'exe' 
-$url            = 'https://wingware.com/pub/wingpro/7.1.2.0/wingpro-7.1.2.0.exe' 
-$silentArgs     = '/VERYSILENT' 
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum C1F0148C952FA875AA90723941BF651801007177347A03EB18E8047404C80BA3 -ChecksumType sha256
