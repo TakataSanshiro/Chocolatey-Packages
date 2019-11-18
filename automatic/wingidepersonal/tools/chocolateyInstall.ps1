9771e06e61b0ddc7291e9520039c9598454add7c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-19 01:24:13 +0900

    add AU to wingidepersonal and update one to 7.1.3

---
+++ automatic/wingidepersonal/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'wingidepersonal'
+  installerType  = 'exe'
+  url            = 'https://wingware.com/pub/wing-personal/7.1.3.0/wing-personal-7.1.3.0.exe'
+  checksum       = '4f88b9fa365f424c2e5d46752a4a74a33fb0b3d953e832604e2fac36791f20ac'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'WingIDEpersonal'
+  toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
--- wingidepersonal/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName    = 'wingidepersonal' 
-$installerType  = 'exe' 
-$url            = 'https://wingware.com/pub/wing-personal/7.1.2.0/wing-personal-7.1.2.0.exe' 
-$silentArgs     = '/VERYSILENT' 
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 77F21076E90419A83A67FAFD76DC1E8BB3241F34D0EA17E5F60C132AF7E8A3F7 -ChecksumType sha256
