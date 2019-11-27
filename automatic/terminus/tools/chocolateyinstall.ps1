fa608cf95d88b9c9e91b03baee7d9f64f9266ca1
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-28 01:54:34 +0900

    update terminus to 1.0.97

--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.92/terminus-1.0.92-setup.exe'
-  checksum       = 'ad3aeeed2613d6b2ab7d9569e40f3b4ba8184964e4df9d399d2dfa237e811487'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.97/terminus-1.0.97-setup.exe'
+  checksum       = '0e173d60444ae73d0f5ecdecdf21ab9c60d27a85bb20b1ebb6cdc93834812321'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
