bdee63ece3dde79ebe496b258c41194d81f4e1d7
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-29 01:00:46 +0900

    add AU to jhead and update one to 3.04

---
+++ automatic/jhead/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = 'jhead'
+  url            = 'http://www.sentex.net/~mwandel/jhead/jhead.exe'
+  checksum       = '47d819ae4785dd89d8f6739a3de0f20448e148a12394c83c9abf78c07d279850'
+  checksumType   = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  fileFullPath   = "$toolsDir\jhead.exe"
+}
+Get-ChocolateyWebFile @packageArgs
---
+++ automatic/jhead/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'http://www.sentex.net/~mwandel/jhead/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            #  "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.links.href -match 'tar.gz' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
+    $version = $version -split "-" | select -last 1
+    $version = $version.Substring(0,$version.length-7)
+
+    @{
+        Version = $version
+        URL32   = "http://www.sentex.net/~mwandel/jhead/jhead.exe"
+    }
+}
+update -ChecksumFor 32 -NoCheckUrl
