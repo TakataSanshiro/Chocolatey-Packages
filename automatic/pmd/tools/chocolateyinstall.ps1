0588c68f2b93560ed07e3e47666496e805a9a3ed
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-12-01 01:32:32 +0900

    update pmd to 6.20.0

--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $package  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pmd_home = Join-path $package 'pmd-bin-6.19.0'
+$pmd_home = Join-path $package 'pmd-bin-6.20.0'
 $pmd_bin  = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.19.0/pmd-bin-6.19.0.zip'
-  checksum       = '9f3c1e28e2a005dd6ee195edb980e34cc25c342a559a798d99547a55fac9517b'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.20.0/pmd-bin-6.20.0.zip'
+  checksum       = '859d928b086434c19ba9204b8081f95474863bf0b57af056ed092eb5c8d1fdb9'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
