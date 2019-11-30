746dfd3ab9c004617bafba98a340f36dcf5beac5
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-12-01 01:27:40 +0900

    update insomnia-rest-api-client to 7.0.5

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/getinsomnia/insomnia/releases/download/v7.0.4/Insomnia.Setup.7.0.4.exe'
-  checksum      = '2d0f45952337bc98304366fdf4122d32291d655cecc6e71610329a57b7704f90'
+  url           = 'https://github.com/getinsomnia/insomnia/releases/download/v7.0.5/Insomnia.Setup.7.0.5.exe'
+  checksum      = 'bdfed06a3330989b30df9404fb1d81ce4e311ea1a2008615cd89697453413a68'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
