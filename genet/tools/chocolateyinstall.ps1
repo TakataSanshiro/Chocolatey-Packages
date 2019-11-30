4f5b7716d12ace742a6f11531ffe4dc24420b3d5
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-12-01 02:11:55 +0900

    remove the genet package. the package source url is 404

--- genet/tools/chocolateyinstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName = 'genet'
-$installerType = 'exe'
-$url = 'https://github.com/genet-app/genet/releases/download/v0.5.0/genet-setup-0.5.0.exe' 
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 6C8C6DE7031B764EDC22B662BB1FA3E1CF50CF1447202FFFCF085111E63153F6 -ChecksumType sha256
