0f0f8c079f0da8f1f67e2b8baf7b2f91753fb53b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-12-03 01:53:44 +0900

    update tagspaces to 3.3.2

--- automatic/tagspaces/tools/chocolateyinstall.ps1
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tagspaces'
-  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v3.3.0/tagspaces-win-x64-3.3.0.zip'
-  checksum       = '8eec9145d4be732b664ccbddb4a5ff9e43eddcdff8b572782d26f60f4c1cbacb'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v3.3.2/tagspaces-win-x64-3.3.2.zip'
+  checksum       = '275294efad3aea6fb8a512bac169314ac15b1bb39c462788af78d934cae6465e'
   checksumType   = 'sha256'
   softwareName   = 'Tagspaces'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
