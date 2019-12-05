ffaf5ccc57ba760eb9911aee2d3d9a487253d5b6
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-12-06 01:51:43 +0900

    add AU to grammarly and update one to 1.5.58.0

---
+++ automatic/grammarly/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'grammarly'
+  installerType  = 'exe'
+  url            = 'https://download-editor.grammarly.com/windows/GrammarlySetup.exe'
+  checksum       = 'c5c9c3dddc4fbe4897be3fbbfcadfb2140fe6e8cb4a8b6aa500b712e52f795e0'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Grammarly*'
+}
+Install-ChocolateyPackage @packageArgs
--- grammarly/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName    = 'grammarly'
-$installerType  = 'exe'
-$url            = 'https://download-editor.grammarly.com/windows/GrammarlySetup.exe'
-$silentArgs     = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 4FE1904D631F81D69706CCD20325D085C9EB92DC95390AFCCE227A8DFE331F6A -ChecksumType sha256
