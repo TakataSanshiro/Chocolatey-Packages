da36539e327f0d328dd5e3815f7d25368f0feccb
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-12-08 00:38:37 +0900

    update wickr to 5.42.15

--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wickr'
   installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.41.14.msi'
-  checksum       = '394a5b713eb674b408eb150f4dcdfd1e647b698ff695dd491d108a73562c1745'
+  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.42.15.msi'
+  checksum       = '001ff874b19344ce6baf3df04f8e206a6c7e9505ec9c562048fee8b9223f6abc'
   checksumType   = 'sha256'
   silentArgs     = '/qb'
   validExitCodes = @(0)
