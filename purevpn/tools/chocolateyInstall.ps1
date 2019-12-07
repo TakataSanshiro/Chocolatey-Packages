d8c2cb2ed487c3e55ce447ab1688bdf2f301f9e7
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-12-08 01:12:41 +0900

    remove no need packages

--- purevpn/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName = 'purevpn' 
-$installerType = 'exe' 
-$url = 'https://s3.amazonaws.com/purevpn-dialer-assets/windows/app/purevpn_setup.exe' 
-$silentArgs = '/VERYSILENT' 
-$validExitCodes = @(0 3010, 1641) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 5212777915F2402D13022EB010BAC0AFFC12E33E30C9C48125720F9B2BD5F526 -ChecksumType sha256
