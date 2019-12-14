30329ec403376c8cd2af5452dcb383b8318d6cf0
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-12-15 01:51:10 +0900

    add AU to twitch and update one

---
+++ automatic/twitch/tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop';
+
+if (-not $env:ChocolateyForce) {
+  try {
+    $TwitchExeHandler = Get-ItemPropertyValue -Path "Microsoft.PowerShell.Core\Registry::HKEY_CLASSES_ROOT\twitch\shell\open\command"-Name "(default)"
+    $TwitchExePath = [System.Management.Automation.PSParser]::Tokenize($TwitchExeHandler, [ref] $null) | Select-Object -First 1 -ExpandProperty "Content"
+    $InstalledVersion = (Get-Item -Path $TwitchExePath).VersionInfo.ProductVersion
+
+    if ([Version]::Parse($InstalledVersion) -ge [Version]::Parse($env:ChocolateyPackageVersion))
+    {
+      Write-Host "Skipping installation because version $InstalledVersion is already installed."
+      return
+    }
+  } catch {
+    # Installed version couldn't be checked, attempt installation
+  }
+}
+
+$packageArgs = @{
+  packageName    = 'twitch'
+  installerType  = 'exe'
+  url            = 'https://desktop.twitchsvc.net/installer/windows/TwitchSetup.exe'
+  checksum       = '2a9c8897249851dca47e95c23989ea7b36819a7db2adf09035a56d7903530dfc'
+  checksumType   = 'sha256'
+  silentArgs     = '/silent'
+  validExitCodes = @(0, 1638)
+  softwareName   = 'Twitch'
+}
+Install-ChocolateyPackage @packageArgs
