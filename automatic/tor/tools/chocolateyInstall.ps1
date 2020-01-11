765d0e638141812a2add162182e3732bc92ae4f2
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-01-12 00:48:24 +0900

    update firealpaca, tor, wickr

--- automatic/tor/tools/chocolateyInstall.ps1
+++ automatic/tor/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tor'
-  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/9.0/tor-win32-0.4.1.6.zip'
-  checksum       = '6a46065bf3becda2cc15cbe8d426d35a4d8d381c4b98360cb981b972094d582c'
+  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/9.0.4/tor-win32-0.4.2.5.zip'
+  checksum       = 'ce6bc27dd36e53f01e4ba6cf93bdb0600f5c6eafcf602f1aaae9262dcb1e3549'
   checksumType   = 'sha256'
   softwareName   = 'Tor'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wickr'
   installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.43.8.msi'
-  checksum       = 'd49eda9952a8640f712750458e8cf50d3cdc3fcbbed302a1d162780c0b5165e5'
+  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.45.8.msi'
+  checksum       = '4dbf7799beec248c89a991869a3fb397332ac5dc53cc4a1b37ed6249c6c31b79'
   checksumType   = 'sha256'
   silentArgs     = '/qb'
   validExitCodes = @(0)
