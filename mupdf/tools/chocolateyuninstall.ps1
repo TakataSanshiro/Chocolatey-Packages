b65b87460942ac8efc8b7026abb0045385210c49
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-01-17 01:36:40 +0900

    remove no need packages

--- mupdf/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "mupdf" `
-  -ZipFileName "mupdf-1.11-windows.zip" 
-
-Remove-item -path "$Home\Desktop\MuPDF.lnk" -Force -ErrorAction 'SilentlyContinue'
--- virtualrouter/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'virtualrouter' 
-$installerType = 'msi' 
-$url = 'https://virtualrouter.codeplex.com/downloads/get/621827' 
-$silentArgs = '/quiet' 
-$validExitCodes = @(0, 3010, 1641) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 73BC4652BC1B4FF67C990A0AB9A68DDE4EC03E90B0FC1A642CEA0FF08E25CBB2 -ChecksumType sha256
