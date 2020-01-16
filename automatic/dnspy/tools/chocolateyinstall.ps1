a79115af18f9d8807e873b40505f270e64142835
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-01-17 01:27:03 +0900

    update dnspy, terminus

--- automatic/dnspy/tools/chocolateyinstall.ps1
+++ automatic/dnspy/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'dnspy'
-  url            = 'https://github.com/0xd4d/dnSpy/releases/download/v6.1.2/dnSpy-net472.zip'
-  checksum       = 'bc85540d28770caf62457e099b3cf1331db84404a835b14a9e5780e55e9daaea'
+  url            = 'https://github.com/0xd4d/dnSpy/releases/download/v6.1.3/dnSpy-net472.zip'
+  checksum       = '93c4a639470218da4e46c30d1366752b82f2cf8e9110774551fc64d664c7cc33'
   checksumType   = 'sha256'
   softwareName   = 'dnSpy'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.99/terminus-1.0.99-setup.exe'
-  checksum       = '3d39e0e8d4314240100f827af5fdf0cc49ee51576cd0fde34042ff71e16f85b5'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.100/terminus-1.0.100-setup.exe'
+  checksum       = '94775c9084c9b141e0196a11f07d757d0089770b693fa4f269243fb733005913'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
