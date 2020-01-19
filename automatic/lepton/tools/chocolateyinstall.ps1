cfc386509d63212f047221e011fdb34d5e338397
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-01-20 00:37:22 +0900

    update lepton to 1.8.2

--- automatic/lepton/tools/chocolateyinstall.ps1
+++ automatic/lepton/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'lepton'
   installerType  = 'exe'
-  url            = 'https://github.com/hackjutsu/Lepton/releases/download/v1.8.1/Lepton-Setup-1.8.1.exe'
-  checksum       = 'ec10596f22523f15feed3ca0ab17bab7178cac98b9d0eb462592db755c40acd9'
+  url            = 'https://github.com/hackjutsu/Lepton/releases/download/v1.8.2/Lepton-Setup-1.8.2.exe'
+  checksum       = 'd424efc682cbf402597b395ec299c3c4e2eab0189fedf63f8c38ef44f21e23e1'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
