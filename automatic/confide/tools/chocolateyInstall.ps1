b82fdb43912c6267eb7302a75093c6d080e783e6
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-01-20 23:02:36 +0900

    add AU to confide and update one to 1.8.3.0

---
+++ automatic/confide/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'confide'
+  fileType       = 'exe'
+  url            = 'https://getconfide.com/download/windows/dl'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Confide*'
+  checksum       = '85cef39b3b02ff3dd45f7c811ea6467bef791e08e73d4c9af2d1b019bf9e6e3e'
+  checksumType   = 'sha256'
+  toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
--- confide/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName    = 'confide' 
-$installerType  = 'exe' 
-$url            = 'https://d3kedtkr0tjlm2.cloudfront.net/ConfideSetup.exe' 
-$silentArgs     = '/S' 
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum 8D9A33B02F7DFBCE83E9844F915F0C1B8E6591233F7F6ADE720225BAC8AE0F61 -ChecksumType sha256
