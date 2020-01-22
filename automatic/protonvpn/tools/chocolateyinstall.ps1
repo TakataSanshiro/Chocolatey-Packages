6a41c8cebbb315ed951d7c7c3f3ac55b297b832f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-01-23 01:50:28 +0900

    update protonvpn to 1.12.0

---
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'protonvpn'
+  fileType       = 'exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.12.0.exe'
+  silentArgs     = '/quiet'
+  validExitCodes = @(0)
+  softwareName   = 'ProtonVPN*'
+  checksum       = 'B254BF51675A8C615378556B217A30A8ADB449662BC2B944E939EB1573BF3F3A'
+  checksumType   = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
