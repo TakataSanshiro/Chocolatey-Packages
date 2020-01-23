1cfc337f41c86e8e0a47d658d790093bb1ac875c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-01-24 00:51:41 +0900

    update postbox, xnconvert, xnconvert.install, xnconvert.portable

--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.10-win32.exe'
-  checksum      = '4482a8095373300c183df2a6e2f921d9b0aa3efb3cc23533c38d5b442842e634'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.11-win32.exe'
+  checksum      = 'fc5e9c73804cdbdfa171574eb65380cdf72a0d621e4870e0f472fd4fe8c5fe18'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
