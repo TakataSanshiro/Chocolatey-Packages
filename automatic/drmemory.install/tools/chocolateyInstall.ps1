ca210b3ea9c0e5b14be7b1b2d1ea5c6b1cfaffd3
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-02-08 01:07:30 +0900

    update drmemory, drmemory.install, drmemory.portable, polar, textify

--- automatic/drmemory.install/tools/chocolateyInstall.ps1
+++ automatic/drmemory.install/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'drmemory.install'
   installerType  = 'msi'
-  url            = 'https://github.com/DynamoRIO/drmemory/releases/download/release_2.2.0/DrMemory-Windows-2.2.0-1.msi'
-  checksum       = '4847a0134e998ded9f826910c00b998e1f32f814e1cbd2873120a59017b433e0'
+  url            = 'https://github.com/DynamoRIO/drmemory/releases/download/release_2.3.0/DrMemory-Windows-2.3.0-1.msi'
+  checksum       = '9e16b52024df8dd59beabe82fa1771ff93a72b3a623751d81d7a4474c9d79729'
   checksumType   = 'sha256'
   silentArgs     = '/qn'
   validExitCodes = @(0, 3010, 1641)
--- automatic/drmemory.portable/tools/chocolateyInstall.ps1
+++ automatic/drmemory.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'drmemory.portable'
-  url            = 'https://github.com/DynamoRIO/drmemory/releases/download/release_2.2.0/DrMemory-Windows-2.2.0-1.zip'
-  checksum       = 'fb94511f05375b669f7ee90f374483e2eb199b82004404753fe10a895ca4d9c4'
+  url            = 'https://github.com/DynamoRIO/drmemory/releases/download/release_2.3.0/DrMemory-Windows-2.3.0-1.zip'
+  checksum       = '676186a24f7b0d3cbe58aaffadb68efc8b0eae7a743ac485363ef7c1a4665d5c'
   checksumType   = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   softwareName   = 'drmemory.portable*'
