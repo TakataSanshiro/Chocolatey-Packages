cd24422a7279743a1068b27f96c39db4858df050
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-02-08 01:45:33 +0900

    add AU to monosnap and update one to 4.1.0

---
+++ automatic/monosnap/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'monosnap'
+  fileType       = 'msi'
+  url            = 'https://static.monosnap.com/windows/Monosnap.4.1.0.msi?web'
+  silentArgs     = '/quiet'
+  validExitCodes = @(0)
+  softwareName   = 'monosnap*'
+  checksum       = 'ec66c11fb70a69823b56611dced480cd96e0ffa440bed2dfa49ccd6dd1305b94'
+  checksumType   = 'sha256'
+  toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
