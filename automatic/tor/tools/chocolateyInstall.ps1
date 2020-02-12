a9166a99c3f592b211358c2d35ad8a53ba249a66
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-02-13 01:36:37 +0900

    update tor to 0.4.2.6

--- automatic/tor/tools/chocolateyInstall.ps1
+++ automatic/tor/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tor'
-  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/9.0.4/tor-win32-0.4.2.5.zip'
-  checksum       = 'ce6bc27dd36e53f01e4ba6cf93bdb0600f5c6eafcf602f1aaae9262dcb1e3549'
+  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/9.0.5/tor-win32-0.4.2.6.zip'
+  checksum       = 'bc5675615660fa40614833a6baaaf608a8095fded0a2cd7e75c0d7e5bf2145ec'
   checksumType   = 'sha256'
   softwareName   = 'Tor'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
