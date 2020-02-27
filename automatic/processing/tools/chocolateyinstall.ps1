ff275ab34448329617c2835cd9c8d22d3fae3d6d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-02-28 01:34:50 +0900

    add AU to processing and update one to 3.5.4

---
+++ automatic/processing/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'processing'
+  url            = 'https://github.com/processing/processing/releases/download/processing-0270-3.5.4/processing-3.5.4-windows32.zip'
+  url64          = 'https://github.com/processing/processing/releases/download/processing-0270-3.5.4/processing-3.5.4-windows64.zip'
+  checksum       = 'e29dc633b708759dd376c3d2507f9dc75910543283098bc233f4e355e75090ca'
+  checksum64     = 'e7a3f924bc265e926e63e1ea771d1379448b58c2d0671b0f8ddadef1aa6eda0e'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  softwareName   = 'Processing*'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyZipPackage @packageArgs
