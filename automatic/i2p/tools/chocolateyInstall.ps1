519ada3e4419689b19a7cbae39acad119495f323
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-02-28 01:34:00 +0900

    update i2p, postbox

--- automatic/i2p/tools/chocolateyInstall.ps1
+++ automatic/i2p/tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'i2p'
   fileType       = 'exe'
-  url            = 'https://download.i2p2.de/releases/0.9.44/i2pinstall%5F0.9.44%5Fwindows.exe'
+  url            = 'https://download.i2p2.de/releases/0.9.45/i2pinstall%5F0.9.45%5Fwindows.exe'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'i2p*'
-  checksum       = '4d25819a846a461e6e87caf7f44dd03eb755d502e8efe990bfb0dc5af2ce9d5f'
+  checksum       = '8c18581a01411898994219554336be56467a738ec34a760a6e1382c2c779233e'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.11-win32.exe'
-  checksum      = 'fc5e9c73804cdbdfa171574eb65380cdf72a0d621e4870e0f472fd4fe8c5fe18'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.12-win32.exe'
+  checksum      = 'f3405068a6d763fea8774cfd85899b3cee355b9c6f543eee5727286200203368'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
