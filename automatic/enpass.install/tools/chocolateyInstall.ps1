d26d66ed1406524e8c96aca9f21e41785c4b2da1
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-03-04 02:19:25 +0900

    update enpass, riot-web

--- automatic/enpass.install/tools/chocolateyInstall.ps1
+++ automatic/enpass.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'Enpass'
-$url         = 'https://dl.enpass.io/stable/windows/setup/6.3.3.597/Enpass-setup.exe'
-$checksum    = '1fa5a731398e19397d113cb0d8fd2958db6b641121f020ae1bec41d75d35d9e2'
+$url         = 'https://dl.enpass.io/stable/windows/setup/6.4.0.631/Enpass-setup.exe'
+$checksum    = '74816acdb597f03e5fb83dd7a5a23ee5587d15d8ad02ecaa68ce75d309329214'
 
 $packageArgs = @{
   packageName            = $packageName
