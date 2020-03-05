e27a21d3f825569d5127a59979f3d86d9001447d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-03-06 01:42:46 +0900

    fix a typo

--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.103/terminus-1.0.104-setup.exe'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.104/terminus-1.0.104-setup.exe'
   checksum       = 'E9DE87CC396E2335C677D85B6C722D0B96FDC834F6018E629F518CBB16BF53EE'
   checksumType   = 'sha256'
   silentArgs     = '/S'
