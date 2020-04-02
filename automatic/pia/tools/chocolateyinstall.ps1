cdaaaeda0ab270914af7812c7d054a6ac6bb304e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-04-03 00:43:30 +0900

    update pia, tinywall

--- automatic/pia/tools/chocolateyinstall.ps1
+++ automatic/pia/tools/chocolateyinstall.ps1
@@ -9,14 +9,14 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'pia'
   fileType       = 'exe'
-  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-1.8-04193.exe'
-  checksum       = 'f1ecd28293618aee8e39728882090d9400218bd1078f8867a149af114dbdc10b'
+  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-2.0-04513.exe'
+  checksum       = '1bd8bad451b5be55cdc310da4f86e3bb2d57ba75ea8aa58d6c500907c8933a4b'
   checksumType   = 'sha256'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'pia*'
-  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-1.8-04193.exe'
-  checksum64     = 'd27a66e9585ee78e2f60d1c0b380643757f7ec7e8044cf23ebc522230ecf4d55'
+  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-2.0-04513.exe'
+  checksum64     = '7bb26755c18368035531115b329a292ca32fb86061bc8fefe913f96aef628424'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- automatic/tinywall/tools/chocolateyinstall.ps1
+++ automatic/tinywall/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = 'tinywall'
   installerType  = 'msi'
   url            = 'https://tinywall.pados.hu/ccount/click.php?id=4'
-  checksum       = '16486B76DEC2E0569AF1B9D8D57067DFDDC7439162F4598E327F4CBA9BBDE17E'
+  checksum       = '48A3AE91F00231D199628932E09697E591DB4C6D037D585A8A964A1FD4DD15E3'
   checksumType   = 'sha256'
   silentArgs     = '/qn /norestart'
   validExitCodes = @(0, 3010, 1641)
