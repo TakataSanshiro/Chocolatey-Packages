5dd2cc423ff54a59eeac9310a62e5d48edfb4523
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-04-13 02:03:28 +0900

    fix the wire AU script and update one to 3.16.3562

--- automatic/wire/tools/chocolateyinstall.ps1
+++ automatic/wire/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wire'
   installerType  = 'exe'
-  url            = 'https://github.com/wireapp/wire-desktop/releases/download/windows/3.12.3423/WireSetup.3.12.3423.exe'
-  checksum       = '1bc732f263dce4c6825ca5e0d53adf7574d94cc37f129bc19922067e39973799'
+  url            = 'https://github.com/wireapp/wire-desktop/releases/download/windows%2F3.16.3562/Wire-Setup.exe'
+  checksum       = '97d558d5615d264bdce3ac537260ed3c64cadb64dceca7ac98473f96e0ab9bc1'
   checksumType   = 'sha256'
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0)
