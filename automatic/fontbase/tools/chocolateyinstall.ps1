5c2bb6c724531653b7148fcf392d5000f69e0880
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-04-22 01:20:07 +0900

    update fontbase

--- automatic/fontbase/tools/chocolateyinstall.ps1
+++ automatic/fontbase/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'fontbase'
   installerType  = 'exe'
-  url            = 'https://releases.fontba.se/win/FontBase-2.11.2.exe'
-  checksum       = '638df293e7ee79b13fc18e699ebc5af4e4a9c19701b1782abb661ea86a33f1ef'
+  url            = 'https://releases.fontba.se/win/FontBase-2.11.3.exe'
+  checksum       = '3c5dcc9d66a7d56b39d476f8c2b46226ebd3669a75e878f4b9c6d6bc2c9415d6'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
