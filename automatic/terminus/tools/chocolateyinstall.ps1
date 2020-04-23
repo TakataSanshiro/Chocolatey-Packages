8ed2026cf33880c820248a37b8c7bdce802fc0da
Angelos Bousias <angelos@bousias.net>
2020-04-23 19:42:37 +0200

    Point to 1.0.108 setup

--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.106/terminus-1.0.106-setup.exe'
-  checksum       = 'ad4f3fb98620f864dc6c0dfcf5d4ea72d0a4e682c98a56a7a09539010a87df22'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.108/terminus-1.0.108-setup.exe'
+  checksum       = '425c8a03b6a0d79b5e76ca767010baae5c007ce29b2e87c0994aa9e2dbbdccea'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
