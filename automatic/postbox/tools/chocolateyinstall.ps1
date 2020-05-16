3ee5bbee5e170e0e9ec88967f0020c04214101e4
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-05-17 02:13:51 +0900

    update postbox, wickr

--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.15-win32.exe'
-  checksum      = 'b727f0b7e5a44de4844b5ea7d7212af9b28457b18e6fbfab903ea5a661454291'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.16-win32.exe'
+  checksum      = 'ef6684940e3e90e7714df2e95655127c203dde1f34d4e2c1fb595d5754458fae'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wickr'
   installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.53.6.msi'
-  checksum       = '2b1e18c33697ed3e4a5e7b6f9407696878a1fe8610d16b4ca4438af3e5fdc9a6'
+  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.54.11.msi'
+  checksum       = '8072dff96574b2f41b4b5cb767f47c785ebb97eeabecdfc64c24544588398f41'
   checksumType   = 'sha256'
   silentArgs     = '/qb'
   validExitCodes = @(0)
