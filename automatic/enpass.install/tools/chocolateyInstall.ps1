3ea4d121fde1c615b846bdf8da3c8b03d2eca864
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-05-21 01:21:40 +0900

    update enpass.install

--- automatic/enpass.install/tools/chocolateyInstall.ps1
+++ automatic/enpass.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'Enpass'
-$url         = 'https://dl.enpass.io/stable/windows/setup/6.4.1.643/Enpass-setup.exe'
-$checksum    = 'e59aa669477df37b4d0ff57424d9fb5589ebd357aceaf51bc2e3dffa0747a6e6'
+$url         = 'https://dl.enpass.io/stable/windows/setup/6.4.2.669/Enpass-setup.exe'
+$checksum    = '97c58db4e2cfd56548f0cc67960c1696903e72341c0dc6ce663c09be39bcc6e3'
 
 $packageArgs = @{
   packageName            = $packageName
