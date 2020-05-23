cd58770c8e8cdad4158246ca6891d323727657e1
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-05-24 02:12:28 +0900

    update quicklook

--- automatic/quicklook/tools/chocolateyinstall.ps1
+++ automatic/quicklook/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName   = 'quicklook'
   installerType = 'msi'
-  url           = 'https://github.com/QL-Win/QuickLook/releases/download/3.6.8/QuickLook-3.6.8.msi'
+  url           = 'https://github.com/QL-Win/QuickLook/releases/download/3.6.9/QuickLook-3.6.9.msi'
   silentArgs    = '/qn'
   softwareName  = 'quicklook*'
-  checksum      = '12c23308152f2128de62383c612cc606fce91fcec38eabe42763614fc9aa8c58'
+  checksum      = 'ff6fa22699b4159792f217357ea7d6a7a5ba3f58f404c74c97d47d200dafad05'
   checksumType  = 'sha256'
   validExitCodes = @(0, 3010, 1641)
 }
