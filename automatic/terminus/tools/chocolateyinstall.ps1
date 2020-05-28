36f6d1bc4814c61e4eace5cf7d1dfcfc83345237
Angelos Bousias <angelos@bousias.net>
2020-05-28 18:36:13 +0200

    Update chocolateyinstall.ps1

--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.110/terminus-1.0.110-setup.exe'
-  checksum       = 'e944ca3d0ff5d8e199adc386373b4cae9b9d53c6340e8b9784352ff832bf9532'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.112/terminus-1.0.112-setup.exe'
+  checksum       = '1f0817be4423ae945ce206d5884c2d8f531c78ff65ffffac9780da287abcde1e'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
