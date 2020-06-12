b4387001a52c0b8f05e760ddc4bdd2684e0d62a5
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-06-13 01:07:08 +0900

    add AU to magicavoxelviewer

---
+++ automatic/magicavoxelviewer/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'magicavoxelviewer'
+  url            = 'https://github.com/ephtracy/ephtracy%2Egithub%2Eio/releases/download/0.41/MagicaVoxel-Viewer.zip'
+  checksum       = '90C09FC708DA95E7BD57FAAB10C86B07FB160AFD25395208FC118B74D0A02BD5'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'MagicaVoxel Viewer'
+}
+Install-ChocolateyZipPackage @packageArgs
--- magicavoxelviewer/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "magicavoxelviewer" `
-  -ZipFileName "MagicaVoxel%20Viewer-win-mac.zip" 
-
-Remove-item -path "$Home\Desktop\MagicaVoxelViewer.lnk" -Force -ErrorAction 'SilentlyContinue'
