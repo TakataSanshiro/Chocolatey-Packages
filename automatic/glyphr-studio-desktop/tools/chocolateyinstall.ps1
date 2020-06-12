bd4111f72d19153dd537e40add44c0898ac6abd7
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-06-13 01:39:30 +0900

    update glyphr-studio-desktop

--- automatic/glyphr-studio-desktop/tools/chocolateyinstall.ps1
+++ automatic/glyphr-studio-desktop/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'glyphr-studio-desktop'
   installerType  = 'exe'
-  url            = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.5.4/Glyphr.Studio.Setup.0.5.4.exe'
-  checksum       = 'e76a9321b75767c7cf517809a59b58260483c8f917ab393600f2d37c41156726'
+  url            = 'https://github.com/glyphr-studio/Glyphr-Studio-Desktop/releases/download/v0.5.5/Glyphr.Studio.Setup.0.5.5.exe'
+  checksum       = '914cf0bfa5e58dfb8dfef320ed85e1abb146db9c5151c6c6fa7156e9e3921855'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
