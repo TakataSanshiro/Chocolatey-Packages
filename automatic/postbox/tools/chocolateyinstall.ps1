646c0d420123828b50b5fac3a50cb1a79dfa1d4a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-07-07 01:32:45 +0900

    update firealpaca, postbox

--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.21-win32.exe'
-  checksum      = '65fd9e288535b55d8db933bd83e037d4feeadab389e8ba5bd1042f5d3220ff02'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.22-win32.exe'
+  checksum      = '88d7e792bad19d13e8f3af097f3a5364c23f6f490e545eb236fa07d250ccb57f'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
