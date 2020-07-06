3552f2036aec412c78896e7ea3c7d6c42d55a738
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-07-07 01:12:40 +0900

    add silentArgs /nolaunch to no auto starts after installing one

--- automatic/boostnote/tools/chocolateyInstall.ps1
+++ automatic/boostnote/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   url            = 'https://github.com/BoostIO/boost-releases/releases/download/v0.15.3/BoostnoteSetup.exe'
   checksum       = '778d1ab887825a3e4b3e9958a3ab8e6e302a40c14bd1e158e2a1fb3309ffed99'
   checksumType   = 'sha256'
-  silentArgs     = '/VERYSILENT'
+  silentArgs     = '/VERYSILENT /nolaunch'
   validExitCodes = @(0)
   softwareName   = 'Boostnote'
 }
--- automatic/github-desktop/tools/chocolateyinstall.ps1
+++ automatic/github-desktop/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   url            = 'https://desktop.githubusercontent.com/releases/2.5.3-f26bc4fa/GitHubDesktopSetup.exe'
   checksum       = 'feeb24002fca661383ece230aa12ff7dc810e10a3c9743700b74e347d45d1219'
   checksumType   = 'sha256'
-  silentArgs     = '/S'
+  silentArgs     = '/S /nolaunch'
   validExitCodes = @(0)
   softwareName   = 'GitHub Desktop'
 }
