4c42aa8df37f5ffc711e4331a2389d4e8a24105f
lpokoradi <laszlo.pokoradi@gmail.com>
2020-07-20 21:32:25 +0200

    Fixed url for version v1.0.113

--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.112/terminus-1.0.113-setup.exe'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.113/terminus-1.0.113-setup.exe'
   checksum       = '27b89c227babb6c54e14a9e87e5b4627895a1d8ea36f1d9fd11659ae58a350e0'
   checksumType   = 'sha256'
   silentArgs     = '/S'
