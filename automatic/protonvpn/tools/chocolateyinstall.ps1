b0d74ca548237a2dc95da9f668b24694870291be
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-07-29 02:15:34 +0900

    update firealpaca, protonvpn, regcool.portable, terminus

--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.16.1.exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.16.3.exe'
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = 'a4d2cfca3209f21e50a02387439e90cb0dd595235560867059b178eee835d9e9'
+  checksum       = '343403394c655dc1b58f5d8bf5b7dd33d0df9af2f844ffb1e5ef8d346a1dc087'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.113/terminus-1.0.113-setup.exe'
-  checksum       = '27b89c227babb6c54e14a9e87e5b4627895a1d8ea36f1d9fd11659ae58a350e0'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.114/terminus-1.0.114-setup.exe'
+  checksum       = '8b73bbc095ee369ab551a2e46ed64a6137c89e417a77b53bb10dc83837ad9977'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
