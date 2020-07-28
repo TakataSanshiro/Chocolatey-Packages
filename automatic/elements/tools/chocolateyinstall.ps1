d1515103451d119af19351a908fa852fa59f5456
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-07-29 01:38:47 +0900

    add AU to elements and update one to 1.2.1

---
+++ automatic/elements/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'elements'
+  installerType  = 'exe'
+  url            = 'https://github.com/FlorianFe/Elements/releases/download/v1.2.1/Elements.exe'
+  checksum       = '64ac745aa65b96491e70ca0d71e6ef16eb1a0f3708568f09d2be7d507d83540a'
+  checksumType   = 'sha256'
+  silentArgs     = '/S /nolaunch'
+  validExitCodes = @(0)
+  softwareName   = 'Elements'
+}
+Install-ChocolateyPackage @packageArgs
