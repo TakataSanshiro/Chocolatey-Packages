dd0e58df3871a82763f9bf4202058802b318bc92
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-07-31 01:35:15 +0900

    update grammarly, onlyoffice, tor

--- automatic/onlyoffice/tools/chocolateyInstall.ps1
+++ automatic/onlyoffice/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = 'onlyoffice'
   fileType       = 'exe'
-  url            = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-5.5.1/DesktopEditors_x86.exe'
-  url64          = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-5.5.1/DesktopEditors_x64.exe'
+  url            = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-5.6.0/DesktopEditors_x86.exe'
+  url64          = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-5.6.0/DesktopEditors_x64.exe'
   softwareName   = 'Onlyoffice*'
-  checksum       = 'ae6d22ce466013f45c8869a133cc31a73ec867b18b636828ab8a2281df62a68e'
+  checksum       = 'f94dbb1420ead786f168f9201d5fa89889548a01431baa3d1aa5cb59a922e896'
   checksumType   = 'sha256'
-  checksum64     = 'ec664165a5a9183f76bc5f0da2a3c28797fee25f3cda0f636395312320767fac'
+  checksum64     = 'e0a6703983a353ce7b5205205302b87a4d5fcc982f4b8a708ce143cc4a96823b'
   checksumType64 = 'sha256'
   silentArgs     = '/s /S /q /Q /quiet /silent /SILENT /VERYSILENT'
   validExitCodes = @(0)
--- automatic/tor/tools/chocolateyInstall.ps1
+++ automatic/tor/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tor'
-  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/9.5/tor-win32-0.4.3.5.zip'
-  checksum       = '9c4c320224d81a043983d8dd04c8074ef8b35be8f769e6024f6d4f0ecbe23ea3'
+  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/9.5.3/tor-win32-0.4.3.6.zip'
+  checksum       = 'd87b78789751b75379132c2cc21d48e4ca2c16192708667d373b830c1477ea9e'
   checksumType   = 'sha256'
   softwareName   = 'Tor'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
