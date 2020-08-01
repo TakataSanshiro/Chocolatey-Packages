22aded37706f7739673cd122272d83593c331343
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-08-02 01:58:02 +0900

    update element-desktop, postbox, terminus

--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.24-win32.exe'
-  checksum      = '5404f590e70e806e9dfc86aff331486281388192b972a9cd4d2c8a330abc91d3'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.25-win32.exe'
+  checksum      = '56e656b1e06481f31677b24a8671a1fdf33c7d978d296e4e97db13f79aadee29'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.114/terminus-1.0.114-setup.exe'
-  checksum       = '8b73bbc095ee369ab551a2e46ed64a6137c89e417a77b53bb10dc83837ad9977'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.115/terminus-1.0.115-setup.exe'
+  checksum       = '7642604afce5762e446aa2eda402040d588ee2ab18ac4871fc06498c4bf6a6b4'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
