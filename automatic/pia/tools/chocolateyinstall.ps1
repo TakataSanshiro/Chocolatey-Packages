6d3dcda494b68202fe8efbdbf6a5c45bf00b6aff
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-09-09 01:04:26 +0900

    update firealpaca, grammarly, pia, protonvpn

--- automatic/pia/tools/chocolateyinstall.ps1
+++ automatic/pia/tools/chocolateyinstall.ps1
@@ -9,14 +9,14 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'pia'
   fileType       = 'exe'
-  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-2.3.1-05354.exe'
-  checksum       = 'd6df99d0c4a70fc4e22470ac9076e722e4e20b28b0acc6c6f518af0bf4e6a34b'
+  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-2.4-05574.exe'
+  checksum       = '2ba5ad0a4fef360d4cf725ca09d4fedd59349c3c3dae969b4d900e7b370a763e'
   checksumType   = 'sha256'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'pia*'
-  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-2.3.1-05354.exe'
-  checksum64     = 'e31a3c5903787908e14805ae731ea5fb9ec84ae7272160a9f1053e516c25ecde'
+  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-2.4-05574.exe'
+  checksum64     = '035a3b7019deb97da7abefa50d7d40324c5d63056a79a50926628066de45d00e'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.16.3.exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.17.1.exe'
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = '343403394c655dc1b58f5d8bf5b7dd33d0df9af2f844ffb1e5ef8d346a1dc087'
+  checksum       = '774da46dd76567dca64a201bb3f1750b73804d39211a33ecf3a2d070ccc9c5ba'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
