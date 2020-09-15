82a53eef71ccd77a625673fde20565b4e00f2a04
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-09-16 02:18:31 +0900

    update element-desktop, enpass.install, postbox

--- automatic/enpass.install/tools/chocolateyInstall.ps1
+++ automatic/enpass.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'Enpass'
-$url         = 'https://dl.enpass.io/stable/windows/setup/6.4.2.669/Enpass-setup.exe'
-$checksum    = '97c58db4e2cfd56548f0cc67960c1696903e72341c0dc6ce663c09be39bcc6e3'
+$url         = 'https://dl.enpass.io/stable/windows/setup/6.5.0.707/Enpass-setup.exe'
+$checksum    = '9386871d2b0cce8d9e604eb219e0020a2bc21a0af3022035f5b072c324c1ae62'
 
 $packageArgs = @{
   packageName            = $packageName
--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.28-win32.exe'
-  checksum      = '8153ad3e06f7380ba3467bcc5368e42fad1903dd7f4a636fd7eef1337051f3e0'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.29-win32.exe'
+  checksum      = '9347db4aaa5d583f23ee8148d907e55f9743b21450f55865f91d4a38edebf1a9'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
