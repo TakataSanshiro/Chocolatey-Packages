d20a5e8992f5da29beaeb907e82ec206502f57da
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-09-21 01:58:36 +0900

    add AU to kpcli and update one to 3.4

---
+++ automatic/kpcli/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$installFile  = join-path $toolsDir 'kpcli.exe'
+
+$packageArgs = @{
+  packageName    = 'kpcli'
+  installerType  = 'exe'
+  url            = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.4.exe/download'
+  checksum       = '317C16E5ADA68B5FF017874B446D29D7751C565655B487D0330763F228059DC6'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'kpcli'
+  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+}
+
+Get-ChocolateyWebFile $packageName "$installFile" "$url" -checksum $checksum -checksumType $checksumType
---
+++ automatic/kpcli/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'https://sourceforge.net/projects/kpcli/files/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.links.href -match 'kpcli-' -notmatch '.pl' | Select -First 1 | % { $_ -split '/' | select -Last 2 }
+    $version = $version[0]
+    $version = $version.split("-") | select -Last 1
+    $version = $version.Substring(0,$version.length-4)
+
+    @{
+        Version = $version
+        URL64   = "https://sourceforge.net/projects/kpcli/files/kpcli-$version.exe/download"
+    }
+}
+
+try {
+    update
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
