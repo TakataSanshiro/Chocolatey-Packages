e8a84594743a4c3b012cc03240bdf98429a810bb
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-10-22 02:15:37 +0900

    fix the AU script and update insomnia-rest-api-client to 2020.4.1

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/getinsomnia/insomnia/releases/download/v7.1.1/Insomnia.Setup.7.1.1.exe'
-  checksum      = 'fbc755662e4b7e416f6d4827281e3df6510b9c66a08531b3aa10d666019dd93f'
+  url           = 'https://github.com/Kong/insomnia/releases/download/core%402020.4.1/Insomnia.Core-2020.4.1.exe'
+  checksum      = '2b36f3756c7c08ac62dbe1834431c57607dcefcb9652bbb85410ca924131de0a'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
--- automatic/insomnia-rest-api-client/update.ps1
+++ automatic/insomnia-rest-api-client/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/getinsomnia/insomnia/tags'
+$releases = 'https://github.com/Kong/insomnia/releases/latest'
 
 function global:au_SearchReplace {
    @{
@@ -14,12 +14,12 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $version = $download_page.links.href -match 'tag/v' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
-    $version = $version.Substring(1)
+    $version = $download_page.links.href -match 'exe' | Select -First 1 | % { $_ -split '/' | select -Last 2 }
+    $version = $version[0] | % { $_ -split '%40' | select -Last 1 }
 
     @{
         Version = $version
-        URL64   = "https://github.com/getinsomnia/insomnia/releases/download/v$version/Insomnia.Setup.$version.exe"
+        URL64   = "https://github.com/Kong/insomnia/releases/download/core%40$version/Insomnia.Core-$version.exe"
     }
 }
 
