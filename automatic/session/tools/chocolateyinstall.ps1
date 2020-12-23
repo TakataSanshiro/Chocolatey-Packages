c8a3f0f5e41e25025762f3791c6a41a6d23362f8
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-12-24 02:43:22 +0900

    fix the AU script and update session

--- automatic/session/tools/chocolateyinstall.ps1
+++ automatic/session/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'session'
   installerType  = 'exe'
-  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.4.2/session-messenger-desktop-win-1.4.2.exe'
-  checksum       = 'b6c00504f49daad90db579557275cf2590cc2bfe50cf38828bf1b75035e1ef57'
+  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.4.4/session-desktop-win-1.4.4.exe'
+  checksum       = 'b9e7ca8c05e98dd69761fd6a0a485408274db89202fc3a5fb7ada76e6b0e30cd'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
