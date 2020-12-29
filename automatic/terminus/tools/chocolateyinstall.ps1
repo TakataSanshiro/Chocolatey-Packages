7882d821aebe8d8d584d6803698660ac12819f18
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-12-30 01:31:03 +0900

    update terminus

--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.122/terminus-1.0.122-setup.exe'
-  checksum       = '86c1c12f4fc99e1b689e782ae191632a9f6f51921de700ddac2a227868ee17b7'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.123/terminus-1.0.123-setup.exe'
+  checksum       = '9987bf1c7285a425c877c2e0a5a44536d31f14025bf7a9533895bd1e44e607f0'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
