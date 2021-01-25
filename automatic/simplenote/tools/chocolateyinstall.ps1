d65853cf5c0bc2dbc6c3cf62e963b259d6106433
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-01-26 02:21:12 +0900

    update grammarly, polar, simplenote

--- automatic/simplenote/tools/chocolateyinstall.ps1
+++ automatic/simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'simplenote'
   installerType  = 'exe'
-  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.4.0/Simplenote-win-2.4.0.exe'
-  checksum       = '4aca504dd51acd28bd885b012ee4c23abadbe17fad943feaa715cf31e941e852'
+  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.5.0/Simplenote-win-2.5.0.exe'
+  checksum       = '58efde99358a8a25dd6b3acbd6cee2f1fa1ea7cd32d546fef78e6da29e4c9468'
   checksumType   = 'sha256'
   silentArgs     = '/S /nolaunch'
   validExitCodes = @(0)
