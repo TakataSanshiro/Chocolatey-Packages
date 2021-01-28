d8b2ffac4394cd17e0742ee266396a3fab02acf2
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-01-29 02:36:47 +0900

    update element-desktop, terminus

--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.127/terminus-1.0.127-setup.exe'
-  checksum       = '27ad3897fc0a0fad96f47d1d53c8f3a127bdd026d0946aecbf642c06bd63d1b4'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.129/terminus-1.0.129-setup.exe'
+  checksum       = 'eaeb43037cb33ed9cace3b27928926efb8a74005a78abd81c77eaf5539c4d130'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
