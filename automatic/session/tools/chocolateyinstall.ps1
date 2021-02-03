af5bb7c104863da91f4505080abe78a133f04dc1
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-02-04 02:41:33 +0900

    update element-desktop, session

--- automatic/session/tools/chocolateyinstall.ps1
+++ automatic/session/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'session'
   installerType  = 'exe'
-  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.4.7/session-desktop-win-1.4.7.exe'
-  checksum       = 'B00D884A73E5F68ADA31BB29C2BE4C5B1BD542DC10A86CB00DAF7D20EDBFD0C7'
+  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.4.8/session-desktop-win-1.4.8.exe'
+  checksum       = 'd5bed365cadd924f5bf46da137b4ebfe8e9a501d4aeffacd44bb6e26699acbb6'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
