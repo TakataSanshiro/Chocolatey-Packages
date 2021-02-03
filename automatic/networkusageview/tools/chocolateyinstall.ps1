78ddfd4918f9d77befff9eddf829bfa80fb4a6c9
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-02-04 03:21:18 +0900

    add AU to networkusageview and update one to 1.25

---
+++ automatic/networkusageview/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'networkusageview'
+  url            = 'https://www.nirsoft.net/utils/networkusageview.zip'
+  url64          = 'https://www.nirsoft.net/utils/networkusageview-x64.zip'
+  checksum       = 'a43d9488fc39f39d1fe43b4c79b35dd25371b33f3111b102593bc90a92af66ba'
+  checksum64     = '03db3df18cd8455f9f6c6034e0653fd16413c9553c6f1a6d26c71a2b4b843187'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'NetworkUsageView*'
+}
+Install-ChocolateyZipPackage @packageArgs
