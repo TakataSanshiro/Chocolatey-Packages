ba8f8a601beaba2e5c27b83fb20d603ed2086923
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-02-17 01:23:11 +0900

    update element-desktop, session, terminus

--- automatic/session/tools/chocolateyinstall.ps1
+++ automatic/session/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'session'
   installerType  = 'exe'
-  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.4.8/session-desktop-win-1.4.8.exe'
-  checksum       = 'd5bed365cadd924f5bf46da137b4ebfe8e9a501d4aeffacd44bb6e26699acbb6'
+  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.4.9/session-desktop-win-1.4.9.exe'
+  checksum       = 'fc3b34a6c64ce55f419918a0516c527b8dc9bf356fd655f13f7fef0d4321ec6c'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.132/terminus-1.0.132-setup.exe'
-  checksum       = 'dff28d162bc0e2086f13f203c04929dba7c1365794c644a0639c8a1af067459e'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.134/terminus-1.0.134-setup.exe'
+  checksum       = 'bb2d4afeb5d998e9f76290178906b233cf1d971818ff4db3c3920f26550038aa'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
