e07152d65ecf4aa0c75011a152fcd8d3adaf180d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-02-21 00:55:43 +0900

    update i2p, wickr

--- automatic/i2p/tools/chocolateyInstall.ps1
+++ automatic/i2p/tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'i2p'
   fileType       = 'exe'
-  url            = 'https://download.i2p2.de/releases/0.9.48/i2pinstall%5F0.9.48%5Fwindows.exe'
+  url            = 'https://download.i2p2.de/releases/0.9.49/i2pinstall%5F0.9.49%5Fwindows.exe'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'i2p*'
-  checksum       = '817d52ea7662ef22a6158d62431aee44b8effe26d3883d979bbc72dac02a80d6'
+  checksum       = 'e48939761c8d0303903b16b1480152de8b4f08ca918d5df0b1ae5c417f15f23a'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wickr'
   installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.72.9.msi'
-  checksum       = '152d414da616071c90604dc3773a1a3156f8f13ba16a21471b126323258d4e03'
+  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.73.13.msi'
+  checksum       = '0e3c9f1ea47b1ad8660559e00a4e8797b57ff753a9c96431681506c2a18a2f1a'
   checksumType   = 'sha256'
   silentArgs     = '/qb'
   validExitCodes = @(0)
