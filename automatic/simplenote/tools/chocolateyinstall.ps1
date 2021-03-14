ff2adca8201e1a9c9865b3fb5f5a9f39b3c7dc2d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-03-15 00:13:54 +0900

    update simplenote, terminus, uninstallview

--- automatic/simplenote/tools/chocolateyinstall.ps1
+++ automatic/simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'simplenote'
   installerType  = 'exe'
-  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.7.0/Simplenote-win-2.7.0.exe'
-  checksum       = 'a8cc36906c6f9876cb9b4429108303ee0e393dc529ec0adc875025372586053f'
+  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.7.1/Simplenote-win-2.7.1.exe'
+  checksum       = 'fda5f7cb1d4f1f74ca7e7b0d4e0413598e5f0ff392b745541847a6fc5fd2556d'
   checksumType   = 'sha256'
   silentArgs     = '/S /nolaunch'
   validExitCodes = @(0)
--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.134/terminus-1.0.134-setup.exe'
-  checksum       = 'bb2d4afeb5d998e9f76290178906b233cf1d971818ff4db3c3920f26550038aa'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.135/terminus-1.0.135-setup.exe'
+  checksum       = 'be39ce3322349ac685fc15da12dc083a2d53e6560a0e71118051330e4cd926ec'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
