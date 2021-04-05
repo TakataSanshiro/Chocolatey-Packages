f416292c0cbfad12db2d13bad8b374d5875ce46d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-04-06 01:51:42 +0900

    update firealpaca, pia

--- automatic/pia/tools/chocolateyinstall.ps1
+++ automatic/pia/tools/chocolateyinstall.ps1
@@ -9,14 +9,14 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'pia'
   fileType       = 'exe'
-  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-2.8-06331.exe'
-  checksum       = '488b23ad652d880a85464fb96d3f9912bb93470bcfa69b40cadb7d7ec9ee99cc'
+  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-2.8.1-06335.exe'
+  checksum       = '98a5c8e829bb13ea8d78491d50c0ac7755b8008f26dd8740a34dab594a36e6fc'
   checksumType   = 'sha256'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'pia*'
-  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-2.8-06331.exe'
-  checksum64     = '6ad37b60e52400db0652d29b1e572a95105f34fb7df5f5f3bc4c678383aa4a49'
+  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-2.8.1-06335.exe'
+  checksum64     = '4e84b07c6346db1a7e9310290e949559691a066959c18497c61df740dc3b23bd'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
