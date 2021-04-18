3f504b4a89dd1c6521e2fce30bdc960ed5d3c100
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-04-19 02:01:19 +0900

    update tagspaces, terminus

--- automatic/tagspaces/tools/chocolateyinstall.ps1
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tagspaces'
-  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v3.9.5/tagspaces-win-x64-3.9.5.zip'
-  checksum       = 'c10a343a772de28e54ad2b98bca857dff1883a3b92ee3192d76b9df5d43b910b'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v3.9.8/tagspaces-win-x64-3.9.8.zip'
+  checksum       = 'cea149d499a6496932d170eaa69d7a8596eb15368361c88c0fde03f1377ce397'
   checksumType   = 'sha256'
   softwareName   = 'Tagspaces'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.136/terminus-1.0.136-setup.exe'
-  checksum       = '60db638164b0f9edf82e2ec0a76953881c847ed84d67ebc6e2c50197b8bc7e5a'
+  url            = 'https://github.com/Eugeny/terminus/releases/download/v1.0.137/terminus-1.0.137-setup.exe'
+  checksum       = 'fe20f6605e03a53b6099545f80504b93bc983d08d6fa8d2c0d6a918f6cfc5395'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
