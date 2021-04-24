f6e1440671d2ca7f57b03017995d484bff579bd5
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-04-25 00:34:05 +0900

    update pmd, wickr

--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $package  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pmd_home = Join-path $package 'pmd-bin-6.33.0'
+$pmd_home = Join-path $package 'pmd-bin-6.34.0'
 $pmd_bin  = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.33.0/pmd-bin-6.33.0.zip'
-  checksum       = 'f7cf4d5bdc9e8634d3aabf05558024fa58d0792c40256473db793b3180ff9d02'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.34.0/pmd-bin-6.34.0.zip'
+  checksum       = 'fb2f85fd36a243116340b6aafcbbb74eac982c27f8c22cd0860f1e08aef63a4c'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wickr'
   installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.76.13.msi'
-  checksum       = '5c443e05d3c5f032d8fbf1565457f04fbc330ec778cf7f282c32cafeb38f3bb0'
+  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.77.12.msi'
+  checksum       = 'f8cd1df4583463bafb777b9f59cd2f1dffaaad61dd03ae087b664ed59a1bff99'
   checksumType   = 'sha256'
   silentArgs     = '/qb'
   validExitCodes = @(0)
