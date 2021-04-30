a7509ca65fc9207e7c9e1543ebff5f5f7009501e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-05-01 01:33:09 +0900

    update github-desktop, insomnia-rest-api-client

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/Kong/insomnia/releases/download/core%402021.2.2/Insomnia.Core-2021.2.2.exe'
-  checksum      = '87b3138a4519713cdc7007c167f55b3d38421d66434eab1d92200c64c7f507c7'
+  url           = 'https://github.com/Kong/insomnia/releases/download/core%402021.3.0/Insomnia.Core-2021.3.0.exe'
+  checksum      = '32229e2336c4f3b6eda58dbb5ad8cfc0409471b844ca017600a1421c339cbbf6'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
