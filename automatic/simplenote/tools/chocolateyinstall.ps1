8f87e071a806a03b469672e387e0e6eee8eb7f7b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-05-19 01:04:10 +0900

    update element-desktop, simplenote

--- automatic/simplenote/tools/chocolateyinstall.ps1
+++ automatic/simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'simplenote'
   installerType  = 'exe'
-  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.10.0/Simplenote-win-2.10.0.exe'
-  checksum       = '81d2c6a74b4a2b5a7ea9958b2bcddf7c9c0fd0fefe7260801bc67ab0062b21bf'
+  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.11.0/Simplenote-win-2.11.0.exe'
+  checksum       = '1ba03ba2c664720f4c96ca38c29a7e6ae332a26ee5899ec72a721f665be753f5'
   checksumType   = 'sha256'
   silentArgs     = '/S /nolaunch'
   validExitCodes = @(0)
