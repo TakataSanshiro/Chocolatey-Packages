abdb5ed94582731122b024ae615bf5b615634e92
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-05-23 02:13:48 +0900

    add the logging option to msi packages

--- automatic/tinywall/tools/chocolateyinstall.ps1
+++ automatic/tinywall/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   url            = 'https://tinywall.pados.hu/ccount/click.php?id=4'
   checksum       = '863f74bfaf5fe60015b16558365948967c30389454f33e85e53d30ed2a4544e1'
   checksumType   = 'sha256'
-  silentArgs     = '/qn /norestart'
+  silentArgs     = '/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`"'
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'TinyWall*'
 }
