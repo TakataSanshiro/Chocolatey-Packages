49ae1b7a36ec601696554dfbf36327b13fcdb87c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-06-01 01:31:17 +0900

    update firealpaca, pmd

--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $package  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pmd_home = Join-path $package 'pmd-bin-6.34.0'
+$pmd_home = Join-path $package 'pmd-bin-6.35.0'
 $pmd_bin  = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.34.0/pmd-bin-6.34.0.zip'
-  checksum       = 'fb2f85fd36a243116340b6aafcbbb74eac982c27f8c22cd0860f1e08aef63a4c'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.35.0/pmd-bin-6.35.0.zip'
+  checksum       = '2beee89f0c6cb7044962d825b788ce0f856f6a5beba66b0d6f2ab0e6d36e5412'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
