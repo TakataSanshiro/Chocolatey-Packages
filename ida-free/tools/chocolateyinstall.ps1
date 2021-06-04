c9698d02defeff05c5f6ec594024d6c6cbff8a27
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-06-05 02:45:32 +0900

    update ida-free

--- ida-free/tools/chocolateyinstall.ps1
+++ ida-free/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName    = 'ida-free'
   fileType       = 'exe'
-  url            = 'https://out7.hex-rays.com/files/idafree70_windows.exe'
+  url            = 'https://out7.hex-rays.com/files/idafree76_windows.exe'
   silentArgs     = "--unattendedmodeui minimal --mode unattended --installpassword freeware"
   softwareName   = '*idafree*'
-  checksum       = '64C4073CA57DC612963413C337FD7F802BF812E01E655D0F4F33827342F7A772'
+  checksum       = '2ECC5B2F5329C4E7A4243634801180BE38A397C31A330324C8ABC605F5DFFB9E'
   checksumType   = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
