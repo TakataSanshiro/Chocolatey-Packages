68684d34d48f0b3304dd98eb0dc34dc3d944aa2b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-07-18 02:41:52 +0900

    fix the AU script and update pia

--- automatic/pia/tools/chocolateyinstall.ps1
+++ automatic/pia/tools/chocolateyinstall.ps1
@@ -9,14 +9,14 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'pia'
   fileType       = 'exe'
-  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-2.9-06393.exe'
-  checksum       = '598ce14c424bd7903678e2ffc5fd4586bd12cbe0b92868c0312ab1a11ca3d115'
+  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-2.10-06571.exe'
+  checksum       = '49b3813cd310ee9bd5f22315051b284c3446650f5bbfeb015a564c146c2e81c9'
   checksumType   = 'sha256'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'pia*'
-  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-2.9-06393.exe'
-  checksum64     = '307f2bae66ddbe66b321f5e2c9beaa60ef82e83862db481963311f83c0e18e8a'
+  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-2.10-06571.exe'
+  checksum64     = '6ddb8e210e4f2d7a7ad494045bff6c8d935f89256b3d5e520898a0a44c2855bf'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- automatic/pia/update.ps1
+++ automatic/pia/update.ps1
@@ -1,39 +1,28 @@
 import-module au
 
-$releases = 'https://www.privateinternetaccess.com/pages/download'
+$releases = 'https://www.privateinternetaccess.com/download/windows-vpn#download-windows'
 
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    # -UseBasicParsing
-    $urlVersion = $download_page.ParsedHtml.querySelector(".checksum_os > p").innerText
-    $urlVersion = $urlVersion.Substring(16)
-    $urlVersion = $urlVersion.Substring(0,$urlVersion.length-4)
-    $version = $urlVersion -split "-" | select -First 1
-    $version = $version.Replace('.','')
-    if ($version.Length -gt 2)
-    {
-      $version = $version + '.0'
-    } 
-    else
-    {
-      $version = $version + '0.0'
-    }
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $version = $download_page.links.href -match 'pia-windows' | Select -First 1 | % { $_ -split '-' | select -Last 2 }
+    $secondVersion = $version[1].Substring(0,$version[1].length-4)
+    $version = $version[0]
 
     @{
         Version   = $version
-        URL32     = "https://installers.privateinternetaccess.com/download/pia-windows-x86-$urlVersion.exe"
-        URL64     = "https://installers.privateinternetaccess.com/download/pia-windows-x64-$urlVersion.exe"
+        URL32     = "https://installers.privateinternetaccess.com/download/pia-windows-x86-$version-$secondVersion.exe"
+        URL64     = "https://installers.privateinternetaccess.com/download/pia-windows-x64-$version-$secondVersion.exe"
     }
 }
 
