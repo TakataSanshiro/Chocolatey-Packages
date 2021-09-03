95f5d76379517d39a1af9d90b96db077363c0028
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-09-04 02:06:26 +0900

    update drmemory, drmemory.install, drmemory.portable

--- automatic/drmemory.portable/tools/chocolateyInstall.ps1
+++ automatic/drmemory.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'drmemory.portable'
-  url            = 'https://github.com/DynamoRIO/drmemory/releases/download/release%5F2.3.18609-10/DrMemory-Windows-2.3.18609-10.zip'
-  checksum       = '8915fd5e516186d01080cbf80212153cfd7a65e65da9471633dc5d4fd648db0e'
+  url            = 'https://github.com/DynamoRIO/drmemory/releases/download/release%5F2.4.0/DrMemory-Windows-2.4.0-2.zip'
+  checksum       = 'ecf13d7acf6b936b85498d1dc9d1a77efe481de3b779afe9b6dd64f0ceb63955'
   checksumType   = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   softwareName   = 'drmemory.portable*'
