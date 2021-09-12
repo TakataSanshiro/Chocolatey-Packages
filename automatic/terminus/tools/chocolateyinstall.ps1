0ea4de3350a09e5fb502fd90efcc3d6447f01c5a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-09-13 01:59:02 +0900

    update terminus

--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/tabby/releases/download/v1.0.156/tabby-1.0.156-setup.exe'
-  checksum       = 'b0845a8092494b673c65d4d4b74fa846da0b9d2a6327444d5f8023865e42001e'
+  url            = 'https://github.com/Eugeny/tabby/releases/download/v1.0.157/tabby-1.0.157-setup.exe'
+  checksum       = '109b68776b05c511b967b255e33654c9e68b747536fc395002d6f20635020cfe'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
