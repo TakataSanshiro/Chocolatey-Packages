c26f5745766557fa3ba2e2c31d9d06c3fa7a3b28
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-09-27 02:09:23 +0900

    create the tabby choco package

---
+++ automatic/tabby/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'tabby'
+  installerType  = 'exe'
+  url            = 'https://github.com/Eugeny/tabby/releases/download/v1.0.158/tabby-1.0.158-setup.exe'
+  checksum       = '455c87839aef0dc761de500c10208585a044f9c994da5febffbdb5d74912ab1f'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Tabby*'
+}
+Install-ChocolateyPackage @packageArgs
