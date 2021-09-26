50c7d697d1f210420466ecea0ede2992d26e5353
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-09-27 01:16:53 +0900

    update firealpaca, pmd, terminus, textify

--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $package  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pmd_home = Join-path $package 'pmd-bin-6.38.0'
+$pmd_home = Join-path $package 'pmd-bin-6.39.0'
 $pmd_bin  = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.38.0/pmd-bin-6.38.0.zip'
-  checksum       = 'bc316327421367fd33a9fe3655d10aea1486d01e744a0f9096e3bf7a869664e8'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.39.0/pmd-bin-6.39.0.zip'
+  checksum       = '3831a51241c38285e4c0e375e844a2195c0dc5216d7247ffe12f2ab3fced64fd'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/terminus/tools/chocolateyinstall.ps1
+++ automatic/terminus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'terminus'
   installerType  = 'exe'
-  url            = 'https://github.com/Eugeny/tabby/releases/download/v1.0.157/tabby-1.0.157-setup.exe'
-  checksum       = '109b68776b05c511b967b255e33654c9e68b747536fc395002d6f20635020cfe'
+  url            = 'https://github.com/Eugeny/tabby/releases/download/v1.0.158/tabby-1.0.158-setup.exe'
+  checksum       = '455c87839aef0dc761de500c10208585a044f9c994da5febffbdb5d74912ab1f'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
