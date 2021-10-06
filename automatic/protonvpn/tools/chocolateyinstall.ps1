6e1bfdaccfcc941b68c38275ae3b496ec48f0945
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-10-07 00:40:47 +0900

    update github-desktop, protonvpn, simplenote, xnconvert, xnconvert.install, xnconvert.portable

--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.22.2.exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.23.4.exe'
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = '0c2a0c43bdef628d767a33b4d90895203288e34f46e201b667b7f5a5310369d0'
+  checksum       = 'c4eaddc22ebc6244771c53098c734978ca59ba1a2552cdaf033be9ca8bc6204c'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- automatic/simplenote/tools/chocolateyinstall.ps1
+++ automatic/simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'simplenote'
   installerType  = 'exe'
-  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.19.1/Simplenote-win-2.19.1.exe'
-  checksum       = 'dcf26cfd0e3103dd022c974d95151f20bce8152921a127459604a9f5b38d1438'
+  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.20.0/Simplenote-win-2.20.0.exe'
+  checksum       = '3e8fa9d632b48fdaccd6ce639888876fbe13790dc9d2b1f3c4b35538f1d2213d'
   checksumType   = 'sha256'
   silentArgs     = '/S /nolaunch'
   validExitCodes = @(0)
