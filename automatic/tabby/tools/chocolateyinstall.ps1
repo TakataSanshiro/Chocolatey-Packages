6503a973217f3d4038a578b9c02fb2084e53cf58
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-10-10 03:34:32 +0900

    add the PackageParameters to install allusers

--- automatic/tabby/tools/chocolateyinstall.ps1
+++ automatic/tabby/tools/chocolateyinstall.ps1
@@ -1,12 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$args = '/S'
+$pp = Get-PackageParameters
+if ($pp['allusers']) { $args = $args + " /allusers" }
+
 $packageArgs = @{
   packageName    = 'tabby'
   installerType  = 'exe'
   url            = 'https://github.com/Eugeny/tabby/releases/download/v1.0.159/tabby-1.0.159-setup.exe'
   checksum       = 'd308a0d3b72ad72d5f945e88ae4c3b4f96c290ac1275230cc6a28cd6cb44d05a'
   checksumType   = 'sha256'
-  silentArgs     = '/S'
+  silentArgs     = $args
   validExitCodes = @(0)
   softwareName   = 'Tabby*'
 }
