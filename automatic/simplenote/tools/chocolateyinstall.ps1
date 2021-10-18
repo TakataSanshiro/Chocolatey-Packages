7bf0a1b9a6be7ac92521efff66ca508bad0e27b6
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-10-19 00:54:37 +0900

    update appaudioconfig, simplenote

--- automatic/simplenote/tools/chocolateyinstall.ps1
+++ automatic/simplenote/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'simplenote'
   installerType  = 'exe'
-  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.20.0/Simplenote-win-2.20.0.exe'
-  checksum       = '3e8fa9d632b48fdaccd6ce639888876fbe13790dc9d2b1f3c4b35538f1d2213d'
+  url            = 'https://github.com/Automattic/simplenote-electron/releases/download/v2.21.0/Simplenote-win-2.21.0.exe'
+  checksum       = 'a3f26f4d0d196b152da5cfcafb72ac2f3e82e5041009a20f6958c43f9f478cd8'
   checksumType   = 'sha256'
   silentArgs     = '/S /nolaunch'
   validExitCodes = @(0)
