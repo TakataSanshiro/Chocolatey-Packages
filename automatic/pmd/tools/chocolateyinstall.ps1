7f041e6b103cddfaaa760d5d7095331548280f7f
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-11-01 00:09:43 +0900

    update pmd

--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $package  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pmd_home = Join-path $package 'pmd-bin-6.39.0'
+$pmd_home = Join-path $package 'pmd-bin-6.40.0'
 $pmd_bin  = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.39.0/pmd-bin-6.39.0.zip'
-  checksum       = '3831a51241c38285e4c0e375e844a2195c0dc5216d7247ffe12f2ab3fced64fd'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.40.0/pmd-bin-6.40.0.zip'
+  checksum       = '31b2009e2b52a35a0e9c28c4ae25c6eea21611b960232c6a0240fecf77995955'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/pmd/tools/chocolateyuninstall.ps1
+++ automatic/pmd/tools/chocolateyuninstall.ps1
@@ -3,7 +3,7 @@
 $id       = "pmd"
 $tools    = Split-Path $MyInvocation.MyCommand.Definition
 $content  = Split-Path $tools
-$pmd_home = Join-Path $content "pmd-bin-6.39.0"
+$pmd_home = Join-Path $content "pmd-bin-6.40.0"
 $pmd_bin  = Join-Path $pmd_home "bin/pmd.bat"
 $cpd_bin  = Join-Path $pmd_home "bin/cpd.bat"
 
