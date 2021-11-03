26908c31a2d173d8fba5801b69179afdb2b9d99c
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-11-03 23:58:51 +0900

    update mamp, tagspaces

--- automatic/mamp/tools/chocolateyinstall.ps1
+++ automatic/mamp/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'mamp'
   fileType       = 'exe'
-  url            = 'https://downloads.mamp.info/MAMP-PRO-WINDOWS/releases/5.0.2/MAMP_MAMP_PRO_5.0.2.exe'
+  url            = 'https://downloads.mamp.info/MAMP-PRO-WINDOWS/releases/5.0.3/MAMP_MAMP_PRO_5.0.3.exe'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
   softwareName   = 'MAMP*'
-  checksum       = '64edd7e225ea9188e24404447132293d30b51f807b0c960b3304498ec399308a'
+  checksum       = 'ceb97858e2752775e3fe5e7c01f2727939f39dafb539990c6966c93f73ad9250'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
--- automatic/tagspaces/tools/chocolateyinstall.ps1
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tagspaces'
-  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v3.11.9/tagspaces-win-x64-3.11.9.zip'
-  checksum       = '9c4d0e2a8232dd112284f383398f1a266c3c9904239068435bd545bb4f9cf4d0'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v4.0.4/tagspaces-win-x64-4.0.4.zip'
+  checksum       = 'd9a6621eca6588a580c2eec1063f1d7e150ed5ef40c05d28854064f09b5f68b6'
   checksumType   = 'sha256'
   softwareName   = 'Tagspaces'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
