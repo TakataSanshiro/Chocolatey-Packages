bf4c04cdaba4449bf2b9cf7a3af285ea18d526f3
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-11-13 02:08:04 +0900

    update magicavoxel

--- automatic/magicavoxel/tools/chocolateyInstall.ps1
+++ automatic/magicavoxel/tools/chocolateyInstall.ps1
@@ -2,11 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'magicavoxel'
-  url            = 'https://github.com/ephtracy/ephtracy.github.io/releases/download/0.99.6/MagicaVoxel-0.99.6.2-win32.zip'
-  url64          = 'https://github.com/ephtracy/ephtracy.github.io/releases/download/0.99.6/MagicaVoxel-0.99.6.2-win64.zip'
-  checksum       = 'AB6CA0D2A6DEC93FB362BD5184FDE1CE23287B695ECED3021809BD302566630F'
-  checksum64     = '169DCDDBEA7DAD30C0713D9CA211D445525501ACEB9E12C8A6B51E7AFAD11420'
-  checksumType   = 'sha256'
+  url64          = 'https://github.com/ephtracy/ephtracy.github.io/releases/download/0.99.6/MagicaVoxel-0.99.6.4-win64.zip'
+  checksum64     = 'FD2B64141CC5E33A01CE807008A3739BF3C86C30E78FB1B1F8D855EBFF5AF8BD'
   checksumType64 = 'sha256'
   softwareName   = 'MagicaVoxel'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
