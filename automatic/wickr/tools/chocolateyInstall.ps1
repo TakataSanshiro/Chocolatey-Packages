d32ba76c22bec1a4a3fe16a91aff278550e14bbe
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-11-13 00:29:37 +0900

    update wickr

--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wickr'
   installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.90.3.msi'
-  checksum       = '2c389866cfa1c6624a7273c8d1650b42f983bb48e6b39fdf291d7db4715c5b5f'
+  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.90.6.msi'
+  checksum       = '211503d7740398c3da863909d28e19d61125fcb95fa9332b66664c3818ae7689'
   checksumType   = 'sha256'
   silentArgs     = "/qb /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0)
