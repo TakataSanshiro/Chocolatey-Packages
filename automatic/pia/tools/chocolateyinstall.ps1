a2fa04c986e147e12b670f3c80742f75ba50e77a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-11-29 01:11:37 +0900

    update pia, pmd, regcool.portable

--- automatic/pia/tools/chocolateyinstall.ps1
+++ automatic/pia/tools/chocolateyinstall.ps1
@@ -9,14 +9,14 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'pia'
   fileType       = 'exe'
-  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-3.1.1-06763.exe'
-  checksum       = 'f72d8bf907f8c686d142f2b8ad7b83f84c702e462c5f4dc63298081b3e62a07d'
+  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-3.1.2-06767.exe'
+  checksum       = 'ffd1c180a6bc1529f388bf60627d85daa2500daf33f5c4ae266795500fbab06d'
   checksumType   = 'sha256'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'pia*'
-  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-3.1.1-06763.exe'
-  checksum64     = 'fbb6471cc721f2f2d957ed99e393230f337b8a383bbb36b69d96c2745e8b4887'
+  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-3.1.2-06767.exe'
+  checksum64     = 'a19c534721b6603ebafafd8f6fdd9d6de428a023232c3bb60de5b95779faf6c4'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $package  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pmd_home = Join-path $package 'pmd-bin-6.40.0'
+$pmd_home = Join-path $package 'pmd-bin-6.41.0'
 $pmd_bin  = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.40.0/pmd-bin-6.40.0.zip'
-  checksum       = '31b2009e2b52a35a0e9c28c4ae25c6eea21611b960232c6a0240fecf77995955'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.41.0/pmd-bin-6.41.0.zip'
+  checksum       = '917c94ba4a8b14875a0693bea229b2af39869ba6cdc2841a44322832d66ae399'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
