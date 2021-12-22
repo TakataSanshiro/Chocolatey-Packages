141a4bff9db8be487a87948d0f3bc0e69663970b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-12-23 00:23:54 +0900

    takeover the exifdataview package

---
+++ automatic/exifdataview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'exifdataview'
+  url            = 'https://www.nirsoft.net/utils/exifdataview.zip'
+  checksum       = 'f244a806e8ef8e1a85df67dfa96e40bbf4cf9196905ebcf18d4a81c91aa69261'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'ExifDataView'
+}
+Install-ChocolateyZipPackage @packageArgs
