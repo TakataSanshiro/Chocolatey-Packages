bb926776259f0113c6556f7697934b19429f262b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2021-12-30 00:11:23 +0900

    update tabby, tinywall, wifiguard

--- automatic/tinywall/tools/chocolateyinstall.ps1
+++ automatic/tinywall/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = 'tinywall'
   installerType  = 'msi'
   url            = 'https://tinywall.pados.hu/ccount/click.php?id=4'
-  checksum       = 'f8dab19132b2e60f864ffe5ac643277db657929b4236492b63087db12596ba57'
+  checksum       = '9623bf296ed19a2bb63006e4c5a0665847d98a31f9ee43ac73718cb3cd296b86'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
