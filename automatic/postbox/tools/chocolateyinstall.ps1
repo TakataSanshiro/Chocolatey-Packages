705c733be52fd00dc60805d9dc49546ced6b0fec
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-01-06 01:08:51 +0900

    update postbox, tagspaces

--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.53-win32.exe'
-  checksum      = 'd082b93fb94db293ca8ba43e86dfe95522996cbdc3040d86a1431b08dd066f8b'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.54-win32.exe'
+  checksum      = 'b6390eed15e17d31b8852be115c6c98f6d5915c3c4e83c5e1f815ff9f3fb7bdc'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/tagspaces/tools/chocolateyinstall.ps1
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tagspaces'
-  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v4.1.2/tagspaces-win-x64-4.1.2.zip'
-  checksum       = 'ee66a390be15463b2b4f869e1dc6fb1196473b00835c6b8c6e0efe39fd0286c3'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v4.1.3/tagspaces-win-x64-4.1.3.zip'
+  checksum       = '597a43a27f7dc997f5305f6df8748b5dc87f1a9433e16ae5ee262dcb5b528b0b'
   checksumType   = 'sha256'
   softwareName   = 'Tagspaces'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
