d033b7fd2376005f073a4489dc69206a8894767e
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-01-15 00:43:34 +0900

    update puredata, regcool.portable

--- automatic/puredata/tools/chocolateyinstall.ps1
+++ automatic/puredata/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'puredata'
   installerType  = 'exe'
-  url            = 'http://msp.ucsd.edu/Software/pd-0.51-4.windows-installer.exe'
-  checksum       = '9cd027f9a105097e25ee940df65b755750071198f514fddbb463f5bd622bdb12'
+  url            = 'http://msp.ucsd.edu/Software/pd-0.52-1.windows-installer.exe'
+  checksum       = '369b0a9b0c4d2f005ba4c829854d2ab2f6300a3e44484311d7a0039426c3968b'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0, 3010, 1641)
