41a3284832bc3b9c09dc19a3a5267082fb23028d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-01-18 01:36:38 +0900

    update beeftext, beeftext.install, beeftext.portable, element-desktop, firealpaca, tabby

--- automatic/beeftext.install/tools/chocolateyInstall.ps1
+++ automatic/beeftext.install/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'beeftext.install'
   installerType  = 'exe'
-  url            = 'https://github.com/xmichelo/Beeftext/releases/download/v12.0/Beeftext-12.0-Installer.exe'
-  checksum       = '85b114a16f50dbc55d940535206269fcef307a68becea4465d7dcbb97692525f'
+  url            = 'https://github.com/xmichelo/Beeftext/releases/download/v13.0/Beeftext-13.0-Installer.exe'
+  checksum       = '0242ddb0cef9d9cf23ce392080c7f17d0e0cfbe7e58918d41b91563c13f546f2'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/beeftext.portable/tools/chocolateyInstall.ps1
+++ automatic/beeftext.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'beeftext.portable'
-  url            = 'https://github.com/xmichelo/Beeftext/releases/download/v12.0/Beeftext-12.0-PortableEdition.zip'
-  checksum       = 'bc57405531799f18f58781b0a547c0fa82296762f5b43b01ef1358ffdb3fccb2'
+  url            = 'https://github.com/xmichelo/Beeftext/releases/download/v13.0/Beeftext-13.0-PortableEdition.zip'
+  checksum       = 'c7357d63841485b308c096ba9732ead58880cdede996e52e149d4117d2795c85'
   checksumType   = 'sha256'
   softwareName   = 'Beeftext'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
