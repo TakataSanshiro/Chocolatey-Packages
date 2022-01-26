0f03e5f3a406c981137a1150e68680a2604e96d4
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-01-27 02:25:25 +0900

    create the grammarly-for-windows choco package

---
+++ automatic/grammarly-for-windows/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'grammarly-for-windows'
+  installerType  = 'exe'
+  url            = 'https://download-windows.grammarly.com/GrammarlyInstaller.exe'
+  checksum       = 'DEFC5C4BB6C2B6A5B8333BDF35F2A9DAFAB4F3398EBDF5DD2A9F0102BE170640'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Grammarly-for-windows*'
+}
+Install-ChocolateyPackage @packageArgs
