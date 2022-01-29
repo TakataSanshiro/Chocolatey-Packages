334161d704731521724ae67975a4c7bdef16cf14
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-01-30 00:46:16 +0900

    update grammarly-for-windows, pmd, vyprvpn

--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $package  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pmd_home = Join-path $package 'pmd-bin-6.41.0'
+$pmd_home = Join-path $package 'pmd-bin-6.42.0'
 $pmd_bin  = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.41.0/pmd-bin-6.41.0.zip'
-  checksum       = '917c94ba4a8b14875a0693bea229b2af39869ba6cdc2841a44322832d66ae399'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.42.0/pmd-bin-6.42.0.zip'
+  checksum       = 'ad5a873218ae14b553cff27d4c69c0ff8da6f46686391b1ff82ad452ab1d51fb'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/vyprvpn/tools/chocolateyinstall.ps1
+++ automatic/vyprvpn/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'vyprvpn'
   fileType       = 'exe'
-  url            = 'https://downloads-goldenfrog.netdna-ssl.com/downloads/vyprvpn/desktop/windows/production/4.3.0.10746/VyprVPN-4.3.0.10746-installer.exe'
+  url            = 'https://downloads-goldenfrog.netdna-ssl.com/downloads/vyprvpn/desktop/windows/production/4.3.1.10763/VyprVPN-4.3.1.10763-installer.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'vyprvpn*'
-  checksum       = '1234a946357925403221fb5a3ff82ddb40227313834e95105908ee37f7fd05b5'
+  checksum       = '6e9c8eb31b66541ce3f9bc1a4576d95c6f85d3ceca4d75e6c3372b93e9d05050'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
