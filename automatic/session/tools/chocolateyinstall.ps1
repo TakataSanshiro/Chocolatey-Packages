ff5a2dacda9c2ab8374a4937aed9d1eb5ed3009d
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-02-09 00:58:14 +0900

    update session

--- automatic/session/tools/chocolateyinstall.ps1
+++ automatic/session/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'session'
   installerType  = 'exe'
-  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.8.0/session-desktop-win-1.8.0.exe'
-  checksum       = 'bf29a941a5f9dccc8f49d567f3aee9e2e80c680b435df22a8521db52cecc67d7'
+  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.8.1/session-desktop-win-1.8.1.exe'
+  checksum       = 'c5ac7500d8e9c98689fdd04dd06ce0ab2bc4df521f296b38a27252a8afbf5166'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
