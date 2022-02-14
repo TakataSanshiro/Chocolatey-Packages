8a2abf710882c88136fae35e5203b7ca73d1d503
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-02-15 00:31:17 +0900

    update protonvpn, regcool.portable, tabby

--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.25.0.exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.25.2.exe'
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = 'be8500bb0c29417d4dbc543aabdfa1817363176e3fdcb651e916015d11d91ea3'
+  checksum       = 'a3a770dbf2c7b56f45d30c2bb934c78c5fa0f2017bf4e51673232dd4eb7e73c2'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
