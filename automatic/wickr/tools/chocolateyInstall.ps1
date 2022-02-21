38632a088479aa8cff0816e044b343c050ac434b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-02-22 00:16:25 +0900

    update glasswire, wickr

--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wickr'
   installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.97.4.msi'
-  checksum       = '26dbfcd1130ececc7cad99dc12daaa77e94a67db3ae1f3890245e1458e453265'
+  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.98.4.msi'
+  checksum       = '4d671a6578e135c760155f81cebae704792b894d5d23495129008ee4654ecbd1'
   checksumType   = 'sha256'
   silentArgs     = "/qb /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0)
