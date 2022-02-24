5398a26c3dd9ccc1384db7ad33b73dbe04210ffc
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-02-25 01:19:43 +0900

    update github-desktop, i2p, pia, tabby

--- automatic/i2p/tools/chocolateyInstall.ps1
+++ automatic/i2p/tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'i2p'
   fileType       = 'exe'
-  url            = 'https://files.i2p-projekt.de/1.6.1/i2pinstall%5F1.6.1%5Fwindows.exe'
+  url            = 'https://files.i2p-projekt.de/1.7.0/i2pinstall%5F1.7.0%5Fwindows.exe'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'i2p*'
-  checksum       = 'c5d5757d0cc1f4ce0cf2a663c35ca173d0cfe4ef0f38ac50b1be116787b03c87'
+  checksum       = 'a2206e2578e88aa08294b68df2c9dad4f9301a94dc3d00bfccfe4517555e226c'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
--- automatic/pia/tools/chocolateyinstall.ps1
+++ automatic/pia/tools/chocolateyinstall.ps1
@@ -9,14 +9,14 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'pia'
   fileType       = 'exe'
-  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-3.2-06857.exe'
-  checksum       = 'a3200d2e99cb3833a25439c5b666e8dba02bf3a5f8d44d4dcdd5bc9512aaccc5'
+  url            = 'https://installers.privateinternetaccess.com/download/pia-windows-x86-3.3-06906.exe'
+  checksum       = '774049b786ae4cab7a56d304e65f2e6e17e4aad12200f1a631a11c3f3b65637b'
   checksumType   = 'sha256'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'pia*'
-  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-3.2-06857.exe'
-  checksum64     = '243aa77a41d984dce4058f8eb6a96ef7f609b892f32652ac4c2566398065fe7d'
+  url64          = 'https://installers.privateinternetaccess.com/download/pia-windows-x64-3.3-06906.exe'
+  checksum64     = '1fdc61f2283d117bc707b55ce3cca172826e7e924020f85b6ffc512d3d525872'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
