6088bdc7fbb6cb9b46bdac061a477a49ec5a19ec
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-02-28 02:09:22 +0900

    update pmd and use the $PATH variable to Join-Path

--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $package  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pmd_home = Join-path $package 'pmd-bin-6.42.0'
+$pmd_home = Join-path $package "pmd-bin-$env:ChocolateyPackageVersion"
 $pmd_bin  = Join-Path $pmd_home 'bin/pmd.bat'
 $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.42.0/pmd-bin-6.42.0.zip'
-  checksum       = 'ad5a873218ae14b553cff27d4c69c0ff8da6f46686391b1ff82ad452ab1d51fb'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.43.0/pmd-bin-6.43.0.zip'
+  checksum       = 'f9e24237cf74be6e1605c319d95086392f1653221c90bf837d054d6944a8bc61'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
