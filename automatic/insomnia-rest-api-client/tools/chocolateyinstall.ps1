66d05df34a3ecfb351405de8316052ea89e920ea
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-03-02 23:57:58 +0900

    update element-desktop, firealpaca, grammarly-for-windows, insomnia-rest-api-client, wickr

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/Kong/insomnia/releases/download/core%402021.7.2/Insomnia.Core-2021.7.2.exe'
-  checksum      = 'cf2b39abd9d87f825facc1594dcccc40e0f740f3fc743b103bcdeabb16f78bea'
+  url           = 'https://github.com/Kong/insomnia/releases/download/core%402022.1.0/Insomnia.Core-2022.1.0.exe'
+  checksum      = '97426529c9bebbaf3d9c57a12cc429e9fe1cea75b9307599276b93977f6fb88f'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'wickr'
   installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.98.4.msi'
-  checksum       = '4d671a6578e135c760155f81cebae704792b894d5d23495129008ee4654ecbd1'
+  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.98.5.msi'
+  checksum       = '1c77c1c5b627237ade48df4c2252e080d57168cbc387cacafdbad4998d6e3149'
   checksumType   = 'sha256'
   silentArgs     = "/qb /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0)
