3a3d6cceb5293bc0f1e953dfb07c29ea15ed942a
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-03-07 00:33:03 +0900

    update gitg

--- gitg/tools/chocolateyInstall.ps1
+++ gitg/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName    = 'gitg'
   installerType  = 'msi'
-  url            = 'https://ftp.gnome.org/pub/GNOME/binaries/win64/gitg/3.32/gitg-x86_64-3.32.1.msi'
-  checksum       = 'E11A254BB3CDE97E50B0304699851F3B0CA2AD83C123D678C2578DF5A998A132'
+  url            = 'https://download.gnome.org/binaries/win64/gitg/41/gitg-x86_64-41.0.msi'
+  checksum       = '96C4E323607FDA46BDB02698C92B991AAEC882E050DE1DF959E7ACF3993F117C'
   checksumType   = 'sha256'
-  silentArgs     = '/quiet'
+  silentArgs     = "/quiet /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0)
   softwareName   = 'Gitg*'
 }
