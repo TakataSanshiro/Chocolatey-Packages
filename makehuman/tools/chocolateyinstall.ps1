15e5ba4af45412dd32797c06ec9ce2b8d4ba649d
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-03-15 08:33:18 -0700

    use https for the makehuman download url

--- makehuman/tools/chocolateyinstall.ps1
+++ makehuman/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName  = 'makehuman'
-$url          = 'http://download.tuxfamily.org/makehuman/releases/makehuman-community-1.2.0-windows.zip' 
+$url          = 'https://download.tuxfamily.org/makehuman/releases/makehuman-community-1.2.0-windows.zip'
 $checksum     = '7F801801F4307A89C8DC91E5713553000A37D1225F25CB1C32AFAD20F3B0F4EE'
 $checksumType = 'sha256'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
