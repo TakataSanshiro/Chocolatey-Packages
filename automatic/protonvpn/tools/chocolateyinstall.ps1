b0269f32774bd44f1b31cf2d80e3f54edeaac21a
Sanshiro <ncolskdslv02r9f2929kfj@ctemplar.com>
2022-04-20 08:28:46 -0700

    update github-desktop, grammarly-for-windows, protonvpn, regcool.portable

--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.26.0.exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v1.27.2.exe'
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = '42b44bf0c43aea27b04cc72cbe7f0e103fb96a586a191a651933c7bf35f2946c'
+  checksum       = '5685b17417c6faf5a653c57fbbb375f1e858aa64132c54d545a1ea3c9103b345'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
