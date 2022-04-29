6c16f055fe9a3ea4aed8e809438a72a90b3d2797
Sanshiro <ncolskdslv02r9f2929kfj@ctemplar.com>
2022-04-29 09:25:26 -0700

    update insomnia-rest-api-client

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/Kong/insomnia/releases/download/core%402022.2.1/Insomnia.Core-2022.2.1.exe'
-  checksum      = '103150bffa6eb8d52988e215fa6fa360b74700b22581385ba6f197bc7ae572a9'
+  url           = 'https://github.com/Kong/insomnia/releases/download/core%402022.3.0/Insomnia.Core-2022.3.0.exe'
+  checksum      = '69d3badecd0d15e8b60ea8642fbf810ce7d13fe4beab1678de480e8e08872f78'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
