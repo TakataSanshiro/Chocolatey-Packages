f603c668df8db8f1062bfc5047692ad93ca37c26
Sanshiro <ncolskdslv02r9f2929kfj@ctemplar.com>
2022-05-05 07:57:54 -0700

    update enpass.install, regcool.install, regcool.portable, uninstallview

--- automatic/enpass.install/tools/chocolateyInstall.ps1
+++ automatic/enpass.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'Enpass'
-$url         = 'https://dl.enpass.io/stable/windows/setup/6.7.4.933/Enpass-setup.exe'
-$checksum    = '81fafb8823987a01c611beacf710b573283593cba173a088ebe8ee62cfb061f1'
+$url         = 'https://dl.enpass.io/stable/windows/setup/6.8.0.1059/Enpass-setup.exe'
+$checksum    = 'fc3c6042fb23f2ece03e2ea178aafd0cbcfdb880ee97af73297b966c13ff7480'
 
 $packageArgs = @{
   packageName            = $packageName
