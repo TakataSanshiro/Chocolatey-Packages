3ead474d104bdbd1c18b24a204c69ae7bc8429ed
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-06-17 09:35:58 -0700

    update tad

--- tad/tools/chocolateyinstall.ps1
+++ tad/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://github.com/antonycourtney/tad/releases/download/v0.10.0/Tad.Setup.0.10.0.exe'
+  url            = 'https://github.com/antonycourtney/tad/releases/download/v0.10.1/Tad-Setup-0.10.1.exe'
   silentArgs     = "/S"
-  checksum       = '35B29728A484C909721D7F9B5DF3EC080FC29FF2AEA7B010A9F38987A820D9A7'
+  checksum       = '08C4E3E979DD1DC98FB162D70CF504D966556704E0151C026F9056E7B41E73F8'
   checksumType   = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'tad*'
