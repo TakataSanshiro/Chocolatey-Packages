dd1824b2604aba697896215d28e8f7e428724ba7
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-06-20 09:51:17 -0700

    takeover the falkon pkg

---
+++ falkon/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    file           = "$toolsDir\Falkon.Installer.3.1.0.exe"
+    file64         = "$toolsDir\Falkon.Installer.3.1.0.x64.exe"
+    installerType  = 'exe'
+    silentArgs     = '/S'
+    validExitCodes = @(0)
+    softwareName   = 'Falkon'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
