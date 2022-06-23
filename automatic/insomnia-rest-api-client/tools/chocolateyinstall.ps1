b71f630f9d2042ea8606b115431fc12f901832a2
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-06-23 07:36:18 -0700

    update insomnia-rest-api-client, protonvpn

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/Kong/insomnia/releases/download/core%402022.4.0/Insomnia.Core-2022.4.0.exe'
-  checksum      = '8f2e3fc87eeb06522e3b1fe07408fc47d57e2a501d63c118066d31c659ec8adf'
+  url           = 'https://github.com/Kong/insomnia/releases/download/core%402022.4.2/Insomnia.Core-2022.4.2.exe'
+  checksum      = '79d0b024b2a6726893abb0bfd4179658a7b95476317287da353fa18ea93545c8'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://protonvpn.com/download/ProtonVPN_win_v2.0.0.exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v2.0.1.exe'
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = '7f9e88e584aa540ac00e7b94083500a7488f6815b48dcc117e5a208c5f395f52'
+  checksum       = 'b6db90130e99b34b6b28cd48c244b5c897baaa6f5cb200f83feb74d32614a3e3'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
