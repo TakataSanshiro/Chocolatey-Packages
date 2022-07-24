83dc1360fe5304335df39ecac336f9ca1c21863d
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-07-24 08:24:30 -0700

    takeover the folderchangesview pkg

---
+++ automatic/folderchangesview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/folderchangesview.zip'
+  checksum       = '6f73e8670634223dfd6f4655c72a31a08e5d0af8f348decc8eabbfa2249c9d5c'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'FolderChangesView*'
+}
+Install-ChocolateyZipPackage @packageArgs
