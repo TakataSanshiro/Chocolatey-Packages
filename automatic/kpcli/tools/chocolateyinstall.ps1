5da3248d3120354761139e83e0ab771f70c8ddbd
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-07-24 07:51:08 -0700

    update kpcli, networkusageview

--- automatic/kpcli/tools/chocolateyinstall.ps1
+++ automatic/kpcli/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $installFile  = join-path $toolsDir 'kpcli.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.7.exe/download'
-  url64          = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.7.exe/download'
-  checksum       = '2BDCFA0C4EA789697544C7F20D226C854ABC49FC3E1EE99C4D84BB013C3C719D'
-  checksum64     = '2BDCFA0C4EA789697544C7F20D226C854ABC49FC3E1EE99C4D84BB013C3C719D'
+  url            = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.8.1.exe/download'
+  url64          = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.8.1.exe/download'
+  checksum       = 'BADDD92143319E2C3A17541FAE2AA1F6050E95D0CC7308012D00364582042DFD'
+  checksum64     = 'BADDD92143319E2C3A17541FAE2AA1F6050E95D0CC7308012D00364582042DFD'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
