ed73728eafaa9a46161302623385e0f2d713b181
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-07-27 08:13:12 -0700

    update itch

--- itch/tools/chocolateyInstall.ps1
+++ itch/tools/chocolateyInstall.ps1
@@ -1,22 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName    = 'itch' 
-$installerType  = 'exe' 
-$url            = 'https://itch.io/app/download?platform=windows' 
-$silentArgs     = '' 
-$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$validExitCodes = @(0) 
-$options = @{
-  Headers = @{
-    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
-    'Accept-Charset' = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3';
-    'Accept-Language' = 'en-US,en;q=0.5';
-    Referer = 'https://itch.io/app';
-  }
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  installerType  = 'exe'
+  url            = 'https://itch.io/app/download?platform=windows'
+  checksum       = '55E97E558F22F6EE39243EB47C6E3A5DEE12D1039EA73B9A860562AD12AF0FEB'
+  checksumType   = 'sha256'
+  silentArgs     = '--silent'
+  validExitCodes = @(0)
+  softwareName   = 'itch*'
+  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 }
-$ahkExe         = 'AutoHotKey'
-$ahkFile        = Join-Path $toolsDir "itchInstall.ahk"
-
-Start-Process $ahkExe $ahkFile
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Options $options
+Install-ChocolateyPackage @packageArgs
