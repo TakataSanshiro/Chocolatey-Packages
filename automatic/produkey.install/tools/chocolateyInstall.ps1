fd1dfdd25a1263b3d70b1b59c0120aa42a6d4424
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-08-21 08:57:26 -0700

    takeover the produkey, produkey.install, produkey.portable pkgs

---
+++ automatic/produkey.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  installerType  = 'exe'
+  url            = 'https://www.nirsoft.net/utils/produkey_setup.exe'
+  checksum       = 'f46cd7bb5f4ddf320b8618e5d567e2242c5f4bb6db0f219b44113b288758420d'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'produkey*'
+  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/produkey.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/produkey.zip'
+  url64          = 'https://www.nirsoft.net/utils/produkey-x64.zip'
+  checksum       = '1293aa22a718a8940fe6a0b0472a8806b1e00894d4d2a54485c1d9931f29cc8e'
+  checksum64     = 'e4604e0ee680370448c6a832856dff4a0255ca763672825a7a67a2a4662b9909'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'produkey.portable'
+}
+Install-ChocolateyZipPackage @packageArgs
