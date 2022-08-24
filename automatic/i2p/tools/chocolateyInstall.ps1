30312d05b9acfeeab70e8c2ee02b34c2a1e68c2f
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-08-24 07:40:40 -0700

    update github-desktop, i2p

--- automatic/i2p/tools/chocolateyInstall.ps1
+++ automatic/i2p/tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'i2p'
   fileType       = 'exe'
-  url            = 'https://files.i2p-projekt.de/1.8.0/i2pinstall%5F1.8.0%5Fwindows.exe'
+  url            = 'https://files.i2p-projekt.de/1.9.0/i2pinstall%5F1.9.0%5Fwindows.exe'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'i2p*'
-  checksum       = '738b7608d7f2c6433dcde8a1cbd7ea025d281e90b45c8695385004625a4c88d1'
+  checksum       = 'fdb2e471fadfda33589697536180df966ec165ab59a0d9c8a623491cc2c8eae3'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
