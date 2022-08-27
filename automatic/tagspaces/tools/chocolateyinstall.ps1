56190db1dabdcf5d7371aec569b23347bd79b94f
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-08-27 08:04:05 -0700

    update grammarly-for-windows, tagspaces

--- automatic/tagspaces/tools/chocolateyinstall.ps1
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tagspaces'
-  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v4.4.3/tagspaces-win-x64-4.4.3.zip'
-  checksum       = 'b106282df362b672ad653de95281318376ae2d1b0d32fbd89097a8d653eefdf1'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v4.5.2/tagspaces-win-x64-4.5.2.zip'
+  checksum       = '5d4763bb9249d236baf9dc22c0c719bd24aee57aadc5cd610cad2f2d0f47733a'
   checksumType   = 'sha256'
   softwareName   = 'Tagspaces'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
