a8bbf34c8fbd61608ffd5d39b0504059c4cfe2ef
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-08-27 08:33:50 -0700

    takeover the filetypesman pkg

---
+++ automatic/filetypesman/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/filetypesman.zip'
+  url64          = 'https://www.nirsoft.net/utils/filetypesman-x64.zip'
+  checksum       = 'dfe33e557f2a11aa81e5f9e590447bccc4f505fd8f84307d73cc53ff7e403281'
+  checksum64     = '5feec2a7c0ca1d46ff8bd27a0eadd068f3d8c6dc3fee6f7354973319aeecefb3'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'FileTypesMan'
+}
+Install-ChocolateyZipPackage @packageArgs
