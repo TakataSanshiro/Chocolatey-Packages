d60caa1840d59386608df2d77133ba2da9f3726a
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-08-30 07:33:30 -0700

    update github-desktop, tor

--- automatic/tor/tools/chocolateyInstall.ps1
+++ automatic/tor/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $packageArgs = @{
   packageName    = 'tor'
-  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/11.0.15/tor-win32-0.4.7.8.zip'
-  url64          = 'https://archive.torproject.org/tor-package-archive/torbrowser/11.0.15/tor-win64-0.4.7.8.zip'
-  checksum       = '70b023e5647aefcf5a76a611b608d1288ca496d790a6ec6dc06a22a4a52c2962'
-  checksum64     = '14115567eb9674c79481509ac97d4a7b76c80a36966dd9864989a96670440757'
+  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/11.5.2/tor-win32-0.4.7.10.zip'
+  url64          = 'https://archive.torproject.org/tor-package-archive/torbrowser/11.5.2/tor-win64-0.4.7.10.zip'
+  checksum       = '114d5a4b2915d940bdc913287a2e54ed8aef79bce092c370b1c849842045369c'
+  checksum64     = 'e19c99b52eb152e45daf62b86d8b11462cb44bf2fc2485f2636f8d193b284ee6'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   softwareName   = 'Tor'
