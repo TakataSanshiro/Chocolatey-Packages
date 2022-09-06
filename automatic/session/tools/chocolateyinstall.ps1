d42202fa377f4aa4822c5db570ad389a035aeed2
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-09-06 08:08:04 -0700

    update firealpaca, glasswire, session, uninstallview

--- automatic/session/tools/chocolateyinstall.ps1
+++ automatic/session/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'session'
   installerType  = 'exe'
-  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.9.1/session-desktop-win-1.9.1.exe'
-  checksum       = '7a381bebf3b451e9909e2185138b94df330ee48922e5f94805b6fd77172fa4fa'
+  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.10.0/session-desktop-win-1.10.0.exe'
+  checksum       = '2128dba20b5bd2d819ef6a2744cd7c5a7d44ecc4d03acf678f33af3e5ebbc326'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
