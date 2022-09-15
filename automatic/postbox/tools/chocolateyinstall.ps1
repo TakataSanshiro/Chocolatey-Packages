79d96463127880bb3f316d401c64d7b41f2b6409
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-09-15 08:13:32 -0700

    update element-desktop, grammarly-for-windows, postbox

--- automatic/postbox/tools/chocolateyinstall.ps1
+++ automatic/postbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'postbox'
   installerType = 'exe'
-  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.56-win32.exe'
-  checksum      = '76ece8b86674c0e56203a7a73a19c5a341f8b8a7635a8e45c8a2dc99b1cf753d'
+  url           = 'https://d3nx85trn0lqsg.cloudfront.net/win/postbox-7.0.57-win32.exe'
+  checksum      = '26959b8db3d30923c3fa8baab9a21e652530c1f684bd3f97f7c25ae0232db590'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
