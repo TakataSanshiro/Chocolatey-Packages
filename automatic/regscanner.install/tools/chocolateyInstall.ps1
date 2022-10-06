c1f512d323de71e41798cb4db0dc63d2f0337d23
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-10-06 09:39:28 -0700

    takeover the regscanner, regscanner.install, regscanner.portable pkgs

---
+++ automatic/regscanner.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  installerType  = 'exe'
+  url            = 'https://www.nirsoft.net/utils/regscanner_setup.exe'
+  checksum       = '558508195d78f0b5e4a663ced20907a8024df40d06d448f84d37402502bd2f1a'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'regscanner*'
+  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
