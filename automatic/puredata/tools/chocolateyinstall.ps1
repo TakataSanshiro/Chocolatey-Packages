1a451737a6ef1b66e5c0e95e30cf39ebb848417c
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-10-28 07:49:45 -0700

    update grammarly-for-windows, puredata, tabby, tagspaces

--- automatic/puredata/tools/chocolateyinstall.ps1
+++ automatic/puredata/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'puredata'
   installerType  = 'exe'
-  url            = 'http://msp.ucsd.edu/Software/pd-0.52-2.windows-installer.exe'
-  checksum       = 'cfc2a91decf0fd195b0ed7d6aa93adb9b356e07f2f0aa51d7efba06bdacd0630'
+  url            = 'http://msp.ucsd.edu/Software/pd-0.53-0.windows-installer.exe'
+  checksum       = '4d48a48cad583d7f57d2457d83a953aa73e6fd1a1c08bf9d6f4d42175c26193f'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0, 3010, 1641)
--- automatic/tagspaces/tools/chocolateyinstall.ps1
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tagspaces'
-  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v5.0.4/tagspaces-win-x64-5.0.4.zip'
-  checksum       = '2860b2d212a7e4571a649e783d8fe992ccc98b71d421526299c53dc1af0fd1dc'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v5.0.5/tagspaces-win-x64-5.0.5.zip'
+  checksum       = 'ae804c5aabbb6c05e14e31a180e3eea7b8b0b8310d1d2353dd425a2b7851dd8d'
   checksumType   = 'sha256'
   softwareName   = 'Tagspaces'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
