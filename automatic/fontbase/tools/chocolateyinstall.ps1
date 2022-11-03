3e088f4e0ff55318064a86e3db82af4926f9893e
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-11-03 09:16:43 -0700

    update element-desktop, fontbase, grammarly-for-windows, wifiguard

--- automatic/fontbase/tools/chocolateyinstall.ps1
+++ automatic/fontbase/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'fontbase'
   installerType  = 'exe'
-  url            = 'https://releases.fontba.se/win/FontBase-2.17.5.exe'
-  checksum       = 'c565f9e2d56e1aae78b4f83c740535c613486b29b82b151f1853d9d4ef4995a7'
+  url            = 'https://releases.fontba.se/win/FontBase-2.17.16.exe'
+  checksum       = 'ecc5a2b54cdd58a0b68e99affd299024f511181054a30ccceccbf545bcc3fb23'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
