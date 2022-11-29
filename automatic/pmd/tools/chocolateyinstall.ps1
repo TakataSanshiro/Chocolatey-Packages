31a3db0e7be581d6247b8adacf2bccb265f1627f
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-11-29 07:21:16 -0800

    update pmd, protonvpn, ungoogled-chromium

--- automatic/pmd/tools/chocolateyinstall.ps1
+++ automatic/pmd/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $cpd_bin  = Join-Path $pmd_home 'bin/cpd.bat'
 
 $packageArgs = @{
   packageName    = 'pmd'
-  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases/6.51.0/pmd-bin-6.51.0.zip'
-  checksum       = '0577ae83e6f3cc82ea9e7fd0304043e1b81d001d24313c41cf2d8a9095d95891'
+  url            = 'https://github.com/pmd/pmd/releases/download/pmd_releases/6.52.0/pmd-bin-6.52.0.zip'
+  checksum       = 'fa757430ee45bb79ce3d10bb5706b71aa7bd2f0df40c135b1ae2ffd2ae91810e'
   checksumType   = 'sha256'
   softwareName   = 'PMD'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://protonvpn.com/download/ProtonVPN_win_v2.2.0.exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v2.2.1.exe'
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = '6e56a88530bacab438705410194bc3241eff32baa87d42217280ac60e3f810f0'
+  checksum       = '78f313c6f45e23738ba0af184cfad17d4593261935f4650f9f3d36421075e39a'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
