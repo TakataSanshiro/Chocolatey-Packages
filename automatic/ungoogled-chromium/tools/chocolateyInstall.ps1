a9b417b08c6ca0effa207978df28d49424e5a81c
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-12-11 07:53:43 -0800

    update ungoogled-chromium

--- automatic/ungoogled-chromium/tools/chocolateyInstall.ps1
+++ automatic/ungoogled-chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\ungoogled-chromium_108.0.5359.95-1.1_windows_x64_x64.zip"
+  file64      = "$toolsDir\ungoogled-chromium_108.0.5359.99-1.1_windows_x64_x64.zip"
   destination = $toolsDir
 }
 
