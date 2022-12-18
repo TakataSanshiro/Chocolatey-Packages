d82b3b86e2f3f06a6f798f3d6ca78531536e6859
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2022-12-18 08:26:36 -0800

    update dnsquerysniffer, firealpaca, glasswire, grammarly-for-windows, tagspaces, ungoogled-chromium, vpnarea, xnconvert, xnconvert.install, xnconvert.portable

--- automatic/tagspaces/tools/chocolateyinstall.ps1
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tagspaces'
-  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v5.1.2/tagspaces-win-x64-5.1.2.zip'
-  checksum       = '337436051a4a1a8c2e84560db4eb79c40c7ff96daea428311936fc5ffa2fa220'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v5.1.3/tagspaces-win-x64-5.1.3.zip'
+  checksum       = '40a98d4245e96ad2b6984d67867b0c9cfe3ef27cad9b69ee40fc99e1ceb5a07b'
   checksumType   = 'sha256'
   softwareName   = 'Tagspaces'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/vpnarea/tools/chocolateyinstall.ps1
+++ automatic/vpnarea/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'VPNArea*'
-  checksum       = '087ED25D267D6C7D13AA623B862D15A635BD326CA0BDCE28DC36E8BA3A166B9B'
+  checksum       = '422edbc9d760fc7e7e5232c07282b56e5cbcde8387b2df88292a0fdeb8af5ebe'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
