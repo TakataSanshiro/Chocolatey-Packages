1847f20fe29a86f11f160996dfaf56d8f2142ce9
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-03-04 07:32:30 -0800

    update element-desktop, github-desktop, grammarly-for-windows, ungoogled-chromium

--- automatic/ungoogled-chromium/tools/chocolateyInstall.ps1
+++ automatic/ungoogled-chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\ungoogled-chromium_110.0.5481.78-1.1_windows_x64_x64.zip"
+  file64      = "$toolsDir\ungoogled-chromium_110.0.5481.177-1.1_windows_x64_x64.zip"
   destination = $toolsDir
 }
 
