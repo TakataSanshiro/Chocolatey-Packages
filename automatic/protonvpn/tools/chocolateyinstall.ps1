7e45fc5766825053a22dae5448d45bc35b759745
Miloly <113513722+Miloliness@users.noreply.github.com>
2023-03-31 17:52:22 +0000

    Update chocolateyinstall.ps1 to Version 2.4.1

--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://protonvpn.com/download/ProtonVPN_win_v2.3.2.exe'
+  url            = 'https://protonvpn.com/download/ProtonVPN_win_v2.4.1.exe'
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = '3fd2a8f09887fef57cd6c99e31ad71804281142c4f731f0685dfea8c362411b6'
+  checksum       = 'c95cf2af65dd0b1556c02cd17952462f02314cf532eec06ebca08328549790ae'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
