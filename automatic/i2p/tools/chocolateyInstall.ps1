d6c744a8e28c6cafd4a949ff0212ceb2e90d2543
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-04-15 08:39:36 -0700

    update element-desktop, glasswire, grammarly-for-windows, i2p, tagspaces

--- automatic/i2p/tools/chocolateyInstall.ps1
+++ automatic/i2p/tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'i2p'
   fileType       = 'exe'
-  url            = 'https://files.i2p-projekt.de/2.2.0/i2pinstall%5F2.2.0%5Fwindows.exe'
+  url            = 'https://files.i2p-projekt.de/2.2.1/i2pinstall%5F2.2.1%5Fwindows.exe'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'i2p*'
-  checksum       = 'ab7efbcc5288d6df9161347277f5d5bad8a5601f70fa12c77ab518e0c0314537'
+  checksum       = '9e25abed16c783ed964f504f72069d07da148850471b8250ecf2f4ea5da710bd'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
--- automatic/tagspaces/tools/chocolateyinstall.ps1
+++ automatic/tagspaces/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'tagspaces'
-  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v5.3.2/tagspaces-win-x64-5.3.2.zip'
-  checksum       = '31f98818d06df30857518c6e370bc3d1b961e34d82a0907b1fe501b3a1b1e44d'
+  url            = 'https://github.com/tagspaces/tagspaces/releases/download/v5.3.3/tagspaces-win-x64-5.3.3.zip'
+  checksum       = '712a863cf0f48dd5a882638bb7d0085ecc6bd47d0b95436c1352c70ae2460a7c'
   checksumType   = 'sha256'
   softwareName   = 'Tagspaces'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
