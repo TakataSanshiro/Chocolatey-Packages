8f3c13b6139fabfc8992def443ac53a5891ee609
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-05-21 09:01:21 -0700

    update tor and the AU script

--- automatic/tor/tools/chocolateyInstall.ps1
+++ automatic/tor/tools/chocolateyInstall.ps1
@@ -1,14 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+
 $packageArgs = @{
   packageName    = 'tor'
-  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/11.5.8/tor-win32-0.4.7.11.zip'
-  url64          = 'https://archive.torproject.org/tor-package-archive/torbrowser/11.5.8/tor-win64-0.4.7.11.zip'
-  checksum       = 'c9494749f02493bf3d53c849f9672fda5426f90b1c34d81237a0bc24c0c6993a'
-  checksum64     = 'f86b07027cc46f352eeae4666a5190a54ffc4e90ca2aa29e1c8231c2afe8ed34'
+  url            = 'https://archive.torproject.org/tor-package-archive/torbrowser/12.0.6/tor-expert-bundle-12.0.6-windows-i686.tar.gz'
+  url64          = 'https://archive.torproject.org/tor-package-archive/torbrowser/12.0.6/tor-expert-bundle-12.0.6-windows-x86_64.tar.gz'
+  checksum       = '450507aef3a2a7217f14edccc3b596a3f15057e197418218ea15d2023f478fd5'
+  checksum64     = '6a6125859291e26e0203473ea8aba0fed011ea96f2978d3bf4c62552e205e53a'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   softwareName   = 'Tor'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
 Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip -FileFullPath "$toolsDir\*tar" -Destination $toolsDir
+Remove-Item $toolsDir\*.tar -ea 0
--- automatic/tor/update.ps1
+++ automatic/tor/update.ps1
@@ -16,18 +16,18 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $version = $download_page.links.href -match 'tor-win' -notmatch 'alpha' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
+    $version = $download_page.links.href -match 'ChangeLog' -notmatch 'alpha' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
     $version = $version | % { $_ -split '-' | select -Last 1 }
-    $length  = $version.length
-    $version = $version.Substring(0, $length-4)
+    #$length  = $version.length
+    #$version = $version.Substring(0, $length-4)
 
-    $torbrowserversion = $download_page.links.href -match 'tor-win' -notmatch 'alpha' | Select -First 1 | % { $_ -split '/' | select -Last 2 }
-    $torbrowserversion = $torbrowserversion[0]
+    $torbrowserversion = $download_page.links.href -match 'torbrowser' -notmatch 'alpha' | Select -First 1 | % { $_ -split '/' | select -First 6 }
+    $torbrowserversion = $torbrowserversion[5]
 
     @{
         Version = $version
-        URL32   = "https://archive.torproject.org/tor-package-archive/torbrowser/$torbrowserversion/tor-win32-$version.zip"
-        URL64   = "https://archive.torproject.org/tor-package-archive/torbrowser/$torbrowserversion/tor-win64-$version.zip"
+        URL32   = "https://archive.torproject.org/tor-package-archive/torbrowser/$torbrowserversion/tor-expert-bundle-$torbrowserversion-windows-i686.tar.gz"
+        URL64   = "https://archive.torproject.org/tor-package-archive/torbrowser/$torbrowserversion/tor-expert-bundle-$torbrowserversion-windows-x86_64.tar.gz"
     }
 }
 
