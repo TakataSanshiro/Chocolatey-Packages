d10169faf5afd5074e67cfb9b379e28f833eaee6
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-06-17 08:33:17 -0700

    update grammarly-for-windows, onlyoffice, vyprvpn

--- automatic/onlyoffice/tools/chocolateyInstall.ps1
+++ automatic/onlyoffice/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = 'onlyoffice'
   fileType       = 'exe'
-  url            = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v7.3.3/DesktopEditors_x86.exe'
-  url64          = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v7.3.3/DesktopEditors_x64.exe'
+  url            = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v7.4.0/DesktopEditors_x86.exe'
+  url64          = 'https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v7.4.0/DesktopEditors_x64.exe'
   softwareName   = 'Onlyoffice*'
-  checksum       = '20c71cf8099e89f0f013b9c062fbb7bcd94a9a95ee9761acbd7dafb729896d5c'
+  checksum       = '2d5b50e378405acc1691257c0db2cc1bef39034fbcd4f3fe8d75ffed1f611d29'
   checksumType   = 'sha256'
-  checksum64     = '141bcfd86fd7802813aec3ba7a76296a5b40ff5aa8c4af99005a956a4f725d03'
+  checksum64     = 'dc36f5b4f66d11802661e77b80b292e34dc04074873b92a826f2e5e654259398'
   checksumType64 = 'sha256'
   silentArgs     = '/s /S /q /Q /quiet /silent /SILENT /VERYSILENT'
   validExitCodes = @(0)
--- automatic/vyprvpn/tools/chocolateyinstall.ps1
+++ automatic/vyprvpn/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'vyprvpn'
   fileType       = 'exe'
-  url            = 'https://www.vyprvpn.com/downloads/vyprvpn/desktop/windows/production/5.0.0.0/VyprVPN-5.0.0.0-installer.exe'
+  url            = 'https://www.vyprvpn.com/downloads/vyprvpn/desktop/windows/production/5.0.1.0/VyprVPN-5.0.1.0-installer.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'vyprvpn*'
-  checksum       = '5BA5821E3CFDB80466925A7ABA145B50AA1D2CE660F0385CBC3EAD2E68C931F8'
+  checksum       = 'e0e015b2083de1cb3f0f91fdca9f9b103d4c13226cc140afa0509e740e850f92'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
