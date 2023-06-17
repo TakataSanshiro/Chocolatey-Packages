21719be9492c7c1bf2892d428e51254396d01f27
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-06-17 10:20:02 -0700

    add the privatezilla pkg

---
+++ automatic/privatezilla/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://github.com/builtbybel/privatezilla/releases/download/0.60.0/privatezilla.zip'
+  checksum       = '541139164555109babee6d7349034c4722684fd4a7cd9050d82709a351a4f432'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyZipPackage @packageArgs
