55d4df86c3cfe17f115e0ad842b0bb8ebd6032de
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-07-19 09:13:01 -0700

    improve element-desktop

---
+++ automatic/element-desktop/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$UninstallArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    FileType    = 'exe'
+    File        = "$env:LocalAppData\element-desktop\Update.exe"
+    SilentArgs  = '--uninstall -s'
+}
+
+Uninstall-ChocolateyPackage @UninstallArgs
