bad638ec9a2880004c14e86a32efee899601282e
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-07-22 07:34:46 -0700

    improve itch

--- itch/tools/chocolateyInstall.ps1
+++ itch/tools/chocolateyInstall.ps1
@@ -1,15 +1,25 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  installerType  = 'exe'
-  url            = 'https://itch.io/app/download?platform=windows'
-  checksum       = '55E97E558F22F6EE39243EB47C6E3A5DEE12D1039EA73B9A860562AD12AF0FEB'
-  checksumType   = 'sha256'
-  silentArgs     = '--silent'
-  validExitCodes = @(0)
-  softwareName   = 'itch*'
-  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-}
-Install-ChocolateyPackage @packageArgs
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+#$packageArgs = @{
+#  packageName    = $env:ChocolateyPackageName
+#  installerType  = 'exe'
+#  url            = 'https://itch.io/app/download?platform=windows'
+#  checksum       = '55E97E558F22F6EE39243EB47C6E3A5DEE12D1039EA73B9A860562AD12AF0FEB'
+#  checksumType   = 'sha256'
+#  silentArgs     = '--silent'
+#  validExitCodes = @(0)
+#  softwareName   = 'itch*'
+#  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+#}
+#This cmdlet displays huge error messages unless itch has been installed successfully.
+#Install-ChocolateyPackage @packageArgs
+#Call the installer directly to suppress huge error messages.
+Get-ChocolateyWebFile -PackageName "$env:ChocolateyPackageName" -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\thefile.exe" -Url 'https://itch.io/app/download?platform=windows' -Checksum '55E97E558F22F6EE39243EB47C6E3A5DEE12D1039EA73B9A860562AD12AF0FEB' -ChecksumType 'SHA256' -GetOriginalFileName
+
+Start-Process -FilePath "$toolsDir\itch-setup.exe" -ArgumentList "--silent"
+Start-Sleep -Seconds 120
+
 Start-WaitandStop "itch"
+Remove-Item -ea 0 -Force -Path $toolsDir\*.exe
