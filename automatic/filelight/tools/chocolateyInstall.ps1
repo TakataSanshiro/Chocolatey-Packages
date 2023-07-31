6a4abf557bc820edc1d4bc24a9776744c942058d
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-07-31 08:52:22 -0700

    add the filelight pkg

---
+++ automatic/filelight/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  file64         = Join-Path $toolsDir 'filelight-23.04.0-1330-windows-cl-msvc2019-x86_64_x64.exe'
+  packageName    = $env:ChocolateyPackageName
+  installerType  = 'exe'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Filelight'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem -Path $toolsDir\*.exe | % { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
---
+++ automatic/filelight/update.ps1
@@ -0,0 +1,39 @@
+﻿import-module au
+
+$artifacts64 = 'https://download.kde.org/stable/release-service/23.04.0/windows/'
+
+function global:au_SearchReplace {
+   @{
+		".\legal\VERIFICATION.txt" = @{
+		  "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+		  "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType64)"
+		  "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+		}
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*File64\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    #$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    #$url           = $download_page.links | ? href -match '.zip$' | select -First 1 -expand href
+    $version = ((Invoke-WebRequest $artifacts64 -UseBasicParsing).links | ? href -match '.exe$' | select -First 1 -expand href) -split '-' | select -First 1 -Skip 1
+    #$build64 = ((Invoke-WebRequest $artifacts64 -UseBasicParsing).links | ? href -match '.exe$' | select -First 1 -expand href) -split '-' | select -First 1 -Skip 2
+
+    @{
+        Version        = $version
+        URL64          = "https://download.kde.org/stable/release-service/$version/windows/filelight-$version-1330-windows-cl-msvc2019-x86_64.exe"
+    }
+}
+
+function global:au_BeforeUpdate() {
+	Get-RemoteFiles -Purge
+}
+
+
+try {
+    update -checksumfor none
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
