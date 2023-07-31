989fcbdfc02d8b48de75cbdb41396f77301c4c65
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-07-31 07:59:55 -0700

    improve exiftoolgui

--- exiftoolgui/tools/chocolateyInstall.ps1
+++ exiftoolgui/tools/chocolateyInstall.ps1
@@ -1,10 +1,6 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-
-$pp = Get-PackageParameters
-if ($pp['installPath']) { $toolsDir = $pp["installPath"] }
-
 $packageName  = $env:ChocolateyPackageName
 $url          = 'https://exiftool.org/gui/exiftoolgui516.zip'
 $checksum     = '8A55DAAE8CBF342152A0CCE53792C5EEEC2F54C64F29CB94FD841674940F344F'
@@ -17,3 +13,10 @@ Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -ChecksumType "$checksumType"
 
 Install-BinFile -Path "$toolsDir\exiftoolgui\ExifToolGUI.exe" -Name 'exiftoolgui'
+
+# set NTFS modify file permissions to $toolsDir\exiftoolgui for user account that installed the package
+$WhoAmI = whoami
+$Acl = Get-Acl "$toolsDir\exiftoolgui"
+$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($WhoAmI, "Modify", 'ContainerInherit,ObjectInherit', 'None', "Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl "$toolsDir\exiftoolgui" $Acl
