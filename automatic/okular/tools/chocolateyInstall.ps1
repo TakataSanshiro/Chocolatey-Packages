f3148882b61142c91d7be5904cb7baf2af460556
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-08-06 06:35:11 -0700

    update okular to the build number 1476 for security reasons
    dependency ghostscript version vulnerable.
    CVE-2023-36664
    CVE-2023-28879

--- automatic/okular/tools/chocolateyInstall.ps1
+++ automatic/okular/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  file64         = "$toolsDir\okular-23.04.3-1453-windows-cl-msvc2019-x86_64.exe"
+  file64         = "$toolsDir\okular-23.04.3-1476-windows-cl-msvc2019-x86_64.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
   softwareName   = 'Okular'
