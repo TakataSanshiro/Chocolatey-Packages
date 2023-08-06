3174cc2b875ef5d345fdd730ee2b4438aa0e8211
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-08-06 07:10:51 -0700

    improve perfect-privacy

---
+++ automatic/perfect-privacy/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+#$UninstallArgs = @{
+#    PackageName = $env:ChocolateyPackageName
+#    FileType    = 'exe'
+#    File        = Join-Path ${env:ProgramFiles(x86)} "Perfect Privacy VPN Manager\uninst.exe"
+#    SilentArgs  = '/S'
+#}
+#Uninstall-ChocolateyPackage @UninstallArgs
+
+$File = Join-Path ${env:ProgramFiles(x86)} "Perfect Privacy VPN Manager\uninst.exe"
+Start-Process -FilePath $File -ArgumentList "/S"
+Start-Sleep -Seconds 30
