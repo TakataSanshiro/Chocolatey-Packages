9130efb0af773d6144ed29532960be7a31b3abd4
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-08-20 07:50:59 -0700

    improve graphicsmagick

--- graphicsmagick/tools/chocolateyInstall.ps1
+++ graphicsmagick/tools/chocolateyInstall.ps1
@@ -27,14 +27,7 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChocolateyWebFile -PackageName "$env:ChocolateyPackageName" -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\thefile.exe" -Url 'https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick-binaries/1.3.40/GraphicsMagick-1.3.40-Q8-win32-dll.exe' -Checksum '560BFCDEBD49E8594DC56CB5261EF4C95A8EE06E10D4D635944A2B391CE8D69B' -ChecksumType 'SHA256' -Url64 'https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick-binaries/1.3.40/GraphicsMagick-1.3.40-Q8-win64-dll.exe' -Checksum64 '8AF562FE1B6A80EC172D2D19707B99016D9195C37911371412667ACE3E3FB0E6' -ChecksumType64 'sha256' -GetOriginalFileName
 
-if (Get-OSArchitectureWidth -Compare 32) {
-  $urlbits = 32
-} else {
-  $urlbits = 64
-}
-$version = "$env:ChocolateyPackageVersion" -split "(\.)" | select -First 5
-$version = -join $version
-Start-Process -FilePath "$toolsDir\GraphicsMagick-$version-Q8-win$urlbits-dll.exe" -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+Start-Process -FilePath (Get-Childitem -Path $toolsDir -Filter "*.exe").fullname -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 Start-Sleep -Seconds 20
 
 Remove-Item -ea 0 -Force -Path $toolsDir\*.exe
