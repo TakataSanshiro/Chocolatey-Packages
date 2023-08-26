1f0d624f26dc016140baad6d297eecc13a279d2a
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-08-26 08:23:46 -0700

    improve the AU script

--- automatic/grammarly-for-windows/update.ps1
+++ automatic/grammarly-for-windows/update.ps1
@@ -1,27 +1,64 @@
 import-module au
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
 $url = 'https://download-windows.grammarly.com/GrammarlyInstaller.exe'
 
 function global:au_SearchReplace {
-    @{
-        'tools\ChocolateyInstall.ps1' = @{
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+    return @{
+      ".\tools\chocolateyInstall.ps1" = @{
+        "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+        "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
         }
      }
 }
 
-function global:au_GetLatest {
-    Write-Host $url
-
-    $temp_file = $env:TEMP + '\GrammarlySetup.exe'
-    Invoke-WebRequest $url -OutFile $temp_file
-    Write-Host $temp_file
-
-    $version = (Get-Command $temp_file).Version
-    Write-Host $version
-
-    $Latest = @{ URL = $url; Version = $version }
-    return $Latest
+function global:au_AfterUpdate {
+  "$($Latest.ETAG)|$($Latest.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
 }
 
-update -NoCheckUrl -ChecksumFor 32
+function GetResultInformation([string]$url) {
+  $dest = "$env:TEMP\GrammarlyInstaller.exe"
+  Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $dest
+  $version = (Get-Command $dest).Version
+
+  $result = @{
+    URL32          = $url
+    Version        = $version
+    Checksum32     = Get-FileHash $dest -Algorithm sha256 | % Hash
+    ChecksumType32 = 'sha256'
+  }
+  Remove-Item -Force $dest
+  return $result
+}
+
+function GetETagIfChanged() {
+  param([string]$url)
+  if (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
+    $existingETag = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -first 1 | % { $_ -split '\|' } | select -First 1
+  }
+  else { $existingETag = $null }
+
+  $etag = Invoke-WebRequest -Method Head -Uri $url -UseBasicParsing
+  $etag = $etag | % { $_.Headers.ETag }
+  if ($etag -eq $existingETag) { return $null }
+
+  return $etag
+}
+
+function global:au_GetLatest {
+  $etag = GetETagIfChanged $url
+  if ($etag) {
+    $result = GetResultInformation $url
+    $result["ETAG"] = $etag
+  }
+  else {
+    $result = @{
+      URL32   = $url
+      Version = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -Last 1
+    }
+  }
+
+  return $result
+}
+
+update -NoCheckUrl
