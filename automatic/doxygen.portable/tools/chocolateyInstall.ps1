3305d5cb9867c21d1d562c8e5eff7579afb8e92b
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-08-26 06:40:27 -0700

    update doxygen.portable, firealpaca, grammarly-for-windows, insomnia-rest-api-client

--- automatic/doxygen.portable/tools/chocolateyInstall.ps1
+++ automatic/doxygen.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://www.doxygen.nl/files/doxygen-1.9.7.windows.x64.bin.zip'
-  checksum       = 'd0c0c4c2b707582d57a9ed51253a638069c91427b0e569dedd963fdd01bf1136'
+  url            = 'https://www.doxygen.nl/files/doxygen-1.9.8.windows.x64.bin.zip'
+  checksum       = '03f98f585acee18df0575262feffccc8a93a5aeacd4ee8c21872aeef12532244'
   checksumType   = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   softwareName   = 'doxygen.portable'
--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/Kong/insomnia/releases/download/core%402023.5.5/Insomnia.Core-2023.5.5.exe'
-  checksum      = '26e1d327f325e367ccb9f84a4039c1e34fe52d67c7a6db5f8b51964323c49dfa'
+  url           = 'https://github.com/Kong/insomnia/releases/download/core%402023.5.6/Insomnia.Core-2023.5.6.exe'
+  checksum      = '33933ad650a139620e5e5d55925e656335cfd4bfa0b8cbba8ac489cda5f89cd0'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
