984c47393a5c48209d489d4970b70cde32f7d9da
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-09-10 00:06:09 +0100

    update kpcli

--- automatic/kpcli/tools/chocolateyinstall.ps1
+++ automatic/kpcli/tools/chocolateyinstall.ps1
@@ -5,15 +5,11 @@ $installFile  = join-path $toolsDir 'kpcli.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.8.1.exe/download'
-  url64          = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.8.1.exe/download'
-  checksum       = 'BADDD92143319E2C3A17541FAE2AA1F6050E95D0CC7308012D00364582042DFD'
-  checksum64     = 'BADDD92143319E2C3A17541FAE2AA1F6050E95D0CC7308012D00364582042DFD'
+  url            = 'https://sourceforge.net/projects/kpcli/files/kpcli-4.0.exe/download'
+  checksum       = 'A5452012AECD82FB6B38B89D69456BEE26092601E01DD5BE0D73DCC68A3379B4'
   checksumType   = 'sha256'
-  checksumType64 = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   softwareName   = 'kpcli'
   fileFullPath   = "$installFile"
 }
 Get-ChocolateyWebFile @packageArgs
-# not specified 64 then this error - CPU is 64 bit. ERROR: This package does not support architecture.
