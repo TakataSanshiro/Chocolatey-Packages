5fa22182c9c4bce05608be08f49e4e47fca9bcf0
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-10-16 04:40:56 +0300

    takeover the appcrashview pkg

---
+++ automatic/appcrashview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/appcrashview.zip'
+  checksum       = '76eca5db923e938504d525b8b48a64ab4ad375ced896665dbc543960b07c6054'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'AppCrashView'
+}
+Install-ChocolateyZipPackage @packageArgs
