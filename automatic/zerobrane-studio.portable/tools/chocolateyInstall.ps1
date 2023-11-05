0d35c90c28071b14430144c75e641d52ded6faa7
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-05 03:28:42 +0200

    update zerobrane-studio.portable graphicsmagick

--- automatic/zerobrane-studio.portable/tools/chocolateyInstall.ps1
+++ automatic/zerobrane-studio.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'zerobrane-studio.portable'
-  url            = 'https://download.zerobrane.com/ZeroBraneStudioEduPack-2.00-win32.zip'
-  checksum       = '43b85b166d1c234be040728b5ef8ee70a67a1e78bae9f7ac0b46747802028881'
+  url            = 'https://download.zerobrane.com/ZeroBraneStudioEduPack-2.01-win32.zip'
+  checksum       = '3e200bb0c3ef6201d50cd34e5a7c9712a63600168dabe1ea6003ef01032affa4'
   checksumType   = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   softwareName   = 'zerobrane-studio.portable'
--- graphicsmagick/tools/chocolateyInstall.ps1
+++ graphicsmagick/tools/chocolateyInstall.ps1
@@ -25,7 +25,7 @@ $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyWebFile -PackageName "$env:ChocolateyPackageName" -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\thefile.exe" -Url 'https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick-binaries/1.3.40/GraphicsMagick-1.3.40-Q8-win32-dll.exe' -Checksum '560BFCDEBD49E8594DC56CB5261EF4C95A8EE06E10D4D635944A2B391CE8D69B' -ChecksumType 'SHA256' -Url64 'https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick-binaries/1.3.40/GraphicsMagick-1.3.40-Q8-win64-dll.exe' -Checksum64 '8AF562FE1B6A80EC172D2D19707B99016D9195C37911371412667ACE3E3FB0E6' -ChecksumType64 'sha256' -GetOriginalFileName
+Get-ChocolateyWebFile -PackageName "$env:ChocolateyPackageName" -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\thefile.exe" -Url 'https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick-binaries/1.3.42/GraphicsMagick-1.3.42-Q8-win32-dll.exe' -Checksum '92AE2A0E0464E73142E4FDDC2FD19471EFDF65C5E823AF188B6E4B1DCCDA0C2A' -ChecksumType 'SHA256' -Url64 'https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick-binaries/1.3.42/GraphicsMagick-1.3.42-Q8-win64-dll.exe' -Checksum64 'D010C61A29283FE5EF4CED33F09797D33D6B014CCDD1219B6028BC678AAC83AD' -ChecksumType64 'sha256' -GetOriginalFileName
 
 Start-Process -FilePath (Get-Childitem -Path $toolsDir -Filter "*.exe").fullname -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 Start-Sleep -Seconds 20
