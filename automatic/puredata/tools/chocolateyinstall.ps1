26c87039574f68346e5861072141b2348c5e3cf7
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-05 03:04:39 +0200

    update firealpaca github-desktop grammarly-for-windows puredata ungoogled-chromium

--- automatic/puredata/tools/chocolateyinstall.ps1
+++ automatic/puredata/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'puredata'
   installerType  = 'exe'
-  url            = 'http://msp.ucsd.edu/Software/pd-0.54-0.windows-installer.exe'
-  checksum       = '09eb8ea32df8ab39f99f676462860f0ddc1d56656a944495c543e316322c8ee3'
+  url            = 'http://msp.ucsd.edu/Software/pd-0.54-1.windows-installer.exe'
+  checksum       = '7804155d7ab3283e9b34e1b19e398dd39dd3dbceae936b487723fafeff39faf2'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0, 3010, 1641)
--- automatic/ungoogled-chromium/tools/chocolateyInstall.ps1
+++ automatic/ungoogled-chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\ungoogled-chromium_118.0.5993.88-1.1_windows_x64_x64.zip"
+  file64      = "$toolsDir\ungoogled-chromium_118.0.5993.117-1.1_windows_x64_x64.zip"
   destination = $toolsDir
 }
 
