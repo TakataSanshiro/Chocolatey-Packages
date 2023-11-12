4938e8d243687ae2b9ffd5a77b2bb96c3de24ef1
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-12 05:40:48 +0200

    add the protondrive pkg

---
+++ automatic/protondrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'protondrive'
+  fileType       = 'exe'
+  url            = 'https://proton.me/download/drive/windows/Proton%20Drive%20Setup%201.4.1.exe'
+  silentArgs     = '/exenoui /qn /norestart'
+  validExitCodes = @(0)
+  softwareName   = 'ProtonDrive*'
+  checksum       = 'A6D0010282B78476265B57C2E8BF6218751D6F431AE823C3157D13CFBC64859E'
+  checksumType   = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
