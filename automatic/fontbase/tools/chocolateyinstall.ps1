bf5f351234862ed3bfcb121d2886207db47396b6
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-12 02:45:59 +0200

    update element-desktop elisa fontbase glasswire grammarly-for-windows

--- automatic/fontbase/tools/chocolateyinstall.ps1
+++ automatic/fontbase/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'fontbase'
   installerType  = 'exe'
-  url            = 'https://releases.fontba.se/win/FontBase-2.18.1.exe'
-  checksum       = '8eaed3f53c8af60ec0a13522a857c9bc27c69a8e554ad9399547298b31fd1373'
+  url            = 'https://releases.fontba.se/win/FontBase-2.19.4.exe'
+  checksum       = '1ed6eba61a50fedfc121b4a98eba2425a01572ac3278090d212af6bd6da3f396'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
