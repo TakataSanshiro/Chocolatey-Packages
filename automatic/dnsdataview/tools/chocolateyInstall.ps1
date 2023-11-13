895013714d23458869bc53ce822c45b67d878751
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-13 04:20:35 +0200

    takeover the dnsdataview pkg

---
+++ automatic/dnsdataview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/dnsdataview.zip'
+  checksum       = 'c6e8607093f3100c2a0d45a168e665d38ccae0ade36ac4032ef80142f6a1feb8'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'DNSDataView'
+}
+Install-ChocolateyZipPackage @packageArgs
