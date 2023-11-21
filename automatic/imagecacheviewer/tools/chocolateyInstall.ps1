c2bb1a3dd7cad941d39237cb222e40cc549582e0
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-21 02:30:05 +0200

    takeover the imagecacheviewer pkg

---
+++ automatic/imagecacheviewer/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/imagecacheviewer.zip'
+  checksum       = '7af88048f1a24306fd5d0a9004ddff921da92a2f889c8b9b2e788703cca65516'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'ImageCacheViewer'
+}
+Install-ChocolateyZipPackage @packageArgs
