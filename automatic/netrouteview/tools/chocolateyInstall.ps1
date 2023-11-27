e7140bf11a07c3fc3611e1608ed0adb32a0024f0
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-27 03:28:53 +0200

    takeover the netrouteview pkg

---
+++ automatic/netrouteview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/netrouteview.zip'
+  checksum       = '266d4972ef251f950d63f3c5491da99dffceb3c505f7d8118fc9eccdb476101a'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'NetRouteView'
+}
+Install-ChocolateyZipPackage @packageArgs
