29e2c07284df340a7cfb72ba79b7e2cf51edcd6f
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-27 03:16:14 +0200

    takeover the macaddressview pkg

---
+++ automatic/macaddressview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/macaddressview.zip'
+  checksum       = 'ab0439c04e469f1eb28d6e26c20520a77e16d0188e3b6bf2a547f3716ac785f6'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'MACAddressView'
+}
+Install-ChocolateyZipPackage @packageArgs
