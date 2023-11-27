7d0c849f9ec00ccc1a340920a2f98f57560b003b
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-11-27 02:29:18 +0200

    update glasswire grammarly-for-windows insomnia-rest-api-client okular

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/Kong/insomnia/releases/download/core%408.4.2/Insomnia.Core-8.4.2.exe'
-  checksum      = 'bcf142a80a85235f2349bd6d19d02df807c4370df3d69c65636bf2e901f2343a'
+  url           = 'https://github.com/Kong/insomnia/releases/download/core%408.4.5/Insomnia.Core-8.4.5.exe'
+  checksum      = '71f39570512da5614436bdff0536deadfabbbfd7d0ad8b92ed85d588d231a01a'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
--- automatic/okular/tools/chocolateyInstall.ps1
+++ automatic/okular/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  file64         = "$toolsDir\okular-23.08.2-1548-windows-cl-msvc2019-x86_64.exe"
+  file64         = "$toolsDir\okular-23.08.3-1591-windows-cl-msvc2019-x86_64.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
   softwareName   = 'Okular'
