4022b9757c54320c35fa15f76c1ad56dd2bd225c
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-12-04 02:58:09 +0200

    takeover the diskcountersview pkg

---
+++ automatic/diskcountersview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/diskcountersview.zip'
+  checksum       = '916de66eaee1fed9ce1b337683dc94d3929cf12c53dd4a6d33c4db8470770c0e'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'DiskCountersView'
+}
+Install-ChocolateyZipPackage @packageArgs
