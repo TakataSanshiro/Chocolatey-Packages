7ce3f539fc9d4fd8f173c39ea2412d9f8591a7b5
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-12-11 02:58:46 +0200

    takeover the turnedontimesview pkg

---
+++ automatic/turnedontimesview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/turnedontimesview.zip'
+  checksum       = '1a1c1e6ed4583acb7b0a8960b34f37797fed81fe65dab1c33d1353e3923a9c4e'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'TurnedOnTimesView'
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/turnedontimesview/update.ps1
@@ -0,0 +1,36 @@
+import-module au
+
+$releases = 'https://www.nirsoft.net/utils/computer_turned_on_times.html'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            # "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            # "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            #"(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+    # -UseBasicParsing
+    $version = $download_page.ParsedHtml.querySelector(".utilcaption").innerText
+    $version = $version.split("`r`n") | select -first 1
+    $version = $version | % { $_ -split ' ' | select -first 3 }
+    $version = $version[2]
+    $version = $version.Substring(1)
+    $version = $version.Trim(":")
+
+    @{
+        Version = $version
+        URL32   = "https://www.nirsoft.net/utils/turnedontimesview.zip"
+    }
+}
+
+try {
+    update
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
