a1b79e857d06aa5b4a1d092cea792cd0aa46b37f
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-12-11 02:45:13 +0200

    takeover the foldertimeupdate pkg

---
+++ automatic/foldertimeupdate/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/foldertimeupdate.zip'
+  url64          = 'https://www.nirsoft.net/utils/foldertimeupdate-x64.zip'
+  checksum       = '788ee2865d22c45f65c0d12f23441d0c5469e84d90b7aebf0c860818702c96d3'
+  checksum64     = 'b49e8bc044893f978e9a8e60714e29f32fa6ba3f929c4aaa58b74b536e160e3e'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'FolderTimeUpdate'
+}
+Install-ChocolateyZipPackage @packageArgs
