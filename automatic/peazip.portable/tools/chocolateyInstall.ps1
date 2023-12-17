8af760f8ace41e57ceb80fa0762469b8e0443519
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-12-17 04:12:27 +0200

    update peazip.portable

--- automatic/peazip.portable/tools/chocolateyInstall.ps1
+++ automatic/peazip.portable/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://github.com/peazip/PeaZip/releases/download/9.5.0/peazip_portable-9.5.0.WINDOWS.zip'
-  url64          = 'https://github.com/peazip/PeaZip/releases/download/9.5.0/peazip_portable-9.5.0.WIN64.zip'
-  checksum       = '39d054ca0e301adbe5f526924be12926d6003c0f247a157d014a417bb40e677f'
-  checksum64     = 'a0cac53a6f140acdca314e898e758c029f41d37deea89b925b85a0a4cdfe7e6a'
+  url            = 'https://github.com/peazip/PeaZip/releases/download/9.6.0/peazip_portable-9.6.0.WINDOWS.zip'
+  url64          = 'https://github.com/peazip/PeaZip/releases/download/9.6.0/peazip_portable-9.6.0.WIN64.zip'
+  checksum       = '0fbd16348e26372fb6eabfa7af307463e32912b114c9e986a3eb34ac8817322f'
+  checksum64     = '654d0098c790d00b1ab1d7b4d6c59a854e4ccb30eeeb868906c98823fc44bef3'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- automatic/peazip.portable/update.ps1
+++ automatic/peazip.portable/update.ps1
@@ -5,9 +5,9 @@ $releases = 'https://github.com/peazip/PeaZip/tags'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            # "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            # "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
         }
     }
