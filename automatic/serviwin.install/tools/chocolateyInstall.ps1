ad20645194d31e2736ddab8a615225285458b44f
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-12-31 04:42:49 +0200

    takeover the serviwin.install pkg

---
+++ automatic/serviwin.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  installerType  = 'exe'
+  url            = 'https://www.nirsoft.net/utils/serviwin_setup.exe'
+  checksum       = '3ed456833f319536bf262c956f4780c66ce8111ca2b3a5c3fe29aa88e7f753fe'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'ServiWin*'
+  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
