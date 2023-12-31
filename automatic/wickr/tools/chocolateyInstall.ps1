667a79db6d122f4cf366fc0689fa8ad4df6b1b43
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2023-12-31 02:30:24 +0200

    retire the wickr pkg

--- automatic/wickr/tools/chocolateyInstall.ps1
+++ automatic/wickr/tools/chocolateyInstall.ps1
@@ -1,14 +1,1 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageArgs = @{
-  packageName    = 'wickr'
-  installerType  = 'msi'
-  url            = 'https://s3.amazonaws.com/static.wickr.com/downloads/win/me/WickrMe-5.102.9.msi'
-  checksum       = '385fd690f179ec8bf96708c7772036c83b42b859c0ece7c05bffc913793e3df6'
-  checksumType   = 'sha256'
-  silentArgs     = "/qb /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes = @(0)
-  softwareName   = 'Wickr'
-  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-}
-Install-ChocolateyPackage @packageArgs
+﻿Write-Host ""  ** This package has been retired."" -Foreground Magenta
