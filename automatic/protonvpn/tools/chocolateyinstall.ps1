8ca71ef3423b317544a36717925ab071609333a0
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-01-07 02:35:00 +0200

    update element-desktop grammarly-for-windows protonvpn

--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://github.com/ProtonVPN/win-app/releases/download/3.2.8/ProtonVPN_v3.2.8.exe'
+  url            = 'https://github.com/ProtonVPN/win-app/releases/download/3.2.9/ProtonVPN_v3.2.9.exe'
   silentArgs     = '/silent'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = 'd69305dda26e4e4cf9a84593570be29cdbafb9f9ff0de17e78dfabf6e0fbcf50'
+  checksum       = '8532b3ee4dfd64d634caf7856cb4beb241eaf016ffe7889ac0c5a8542a7ba4f7'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
