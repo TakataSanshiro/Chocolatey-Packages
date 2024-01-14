633b2eac65d647a42dbcbadfe6ff3a920a2a8e54
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-01-14 03:11:09 +0200

    takeover the ipinfooffline pkg

---
+++ automatic/ipinfooffline/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/ipinfooffline.zip'
+  checksum       = 'a9a19a6a0230df524481cb28dbb44d9668ae847d4de38bbd1487a12fa20dceca'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'IPInfoOffline'
+}
+Install-ChocolateyZipPackage @packageArgs
