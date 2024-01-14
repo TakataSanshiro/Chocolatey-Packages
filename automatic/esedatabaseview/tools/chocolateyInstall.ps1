911981fe7720c8d1430c1af4b98ed3169118bbb3
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-01-14 02:59:46 +0200

    takeover the esedatabaseview pkg

---
+++ automatic/esedatabaseview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/esedatabaseview.zip'
+  checksum       = '1ee30f78b41ac3b8486a798f87d43c4f27dd294713899873b150a432056b234f'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'ESEDatabaseView'
+}
+Install-ChocolateyZipPackage @packageArgs
