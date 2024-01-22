ea90e2344e167a8111df6d58792c7d19e709cb99
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-01-22 03:28:34 +0200

    takeover the chromecookiesview pkg

---
+++ automatic/chromecookiesview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/chromecookiesview.zip'
+  checksum       = '30e1694c33c599984b46319c276e1bd5438f9898cdb6211e88a35c8129f6aa57'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'ChromeCookiesView'
+}
+Install-ChocolateyZipPackage @packageArgs
