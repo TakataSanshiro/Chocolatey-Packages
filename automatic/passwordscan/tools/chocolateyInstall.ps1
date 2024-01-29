7162a12300f8d9e249969c43ef4eea3fba27bf9b
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-01-29 03:11:25 +0200

    takeover the passwordscan pkg

---
+++ automatic/passwordscan/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/passwordscan.zip'
+  url64          = 'https://www.nirsoft.net/utils/passwordscan-x64.zip'
+  checksum       = 'a8844c647c53266cc263662d45003b2c22c15a78f3bca11f0a6c324eeb19b7c4'
+  checksum64     = 'a87651cf3ffd7550018294c1b6a5c987f7f8bf29e205b82b31a76f518050b3fd'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'Password Security Scanner'
+}
+Install-ChocolateyZipPackage @packageArgs
