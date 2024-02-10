6c9c27408ce0e815ebfe4cd2e6ececb8c4e78a3b
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-02-11 01:38:34 +0200

    update glasswire grammarly-for-windows insomnia-rest-api-client ungoogled-chromium

--- automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
+++ automatic/insomnia-rest-api-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'insomnia-rest-api-client'
   installerType = 'exe'
-  url           = 'https://github.com/Kong/insomnia/releases/download/core%408.6.0/Insomnia.Core-8.6.0.exe'
-  checksum      = 'C597C5EE12F64907C08459DA52D93D56C10E0C6F78CDB890DF78652229346218'
+  url           = 'https://github.com/Kong/insomnia/releases/download/core%408.6.1/Insomnia.Core-8.6.1.exe'
+  checksum      = '1140e6ce712525887f21225e89590a0dbeb2d30a2a44d449513c0da07803d8b1'
   checksumType  = 'sha256'
   silentArgs    = '--silent'
   validExitCodes= @(0)
