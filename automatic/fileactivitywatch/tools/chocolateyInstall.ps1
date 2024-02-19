9c4569ef31ada59ed2e3780944784f54f05a7a3b
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-02-19 02:52:27 +0200

    add the fileactivitywatch pkg

---
+++ automatic/fileactivitywatch/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/fileactivitywatch.zip'
+  url64          = 'https://www.nirsoft.net/utils/fileactivitywatch-x64.zip'
+  checksum       = 'a1932da51808ec5748b8ce33d55f14faa358c51ff66b12f92bfab6a3b0161f49'
+  checksum64     = '325a472199d0fbcfaf558349c1a3fd18942109ad989bd7dbab654ff93e05b723'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'FileActivityWatch'
+}
+Install-ChocolateyZipPackage @packageArgs
