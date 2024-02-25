37c45e141b6f3f6a463f2ed8252bd917e24065fb
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-02-25 03:42:44 +0200

    takeover the webcacheimageinfo pkg

---
+++ automatic/webcacheimageinfo/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/webcacheimageinfo.zip'
+  checksum       = '079757d81aafa7ef09a3a7bc98ee1fb6a3666a0580ce3a9fe8195c37e3837919'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'WebCacheImageInfo'
+}
+Install-ChocolateyZipPackage @packageArgs
