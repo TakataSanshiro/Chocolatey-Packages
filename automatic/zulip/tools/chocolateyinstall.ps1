0cc2ec948e852fa47c84607d0783d5e7834358b3
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-03-17 03:59:38 +0200

    takeover the zulip pkg

---
+++ automatic/zulip/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = 'https://github.com/zulip/zulip-desktop/releases/download/v5.10.5/Zulip-Web-Setup-5.10.5.exe'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  checksum       = 'fbefaa39de016430c49e86bfa8df73d4710a6bc3299fbb646c8dbd710d9eb02a'
+  checksumType   = 'sha256'
+  softwareName   = 'Zulip*'
+  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
+
+Start-Sleep -Seconds 60
