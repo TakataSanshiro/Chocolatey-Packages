a9d8d20da4fcd97fa6f6b2bfb61bdaaac82944fb
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-03-19 02:59:14 +0200

    add the dnslookupview pkg

---
+++ automatic/dnslookupview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/dnslookupview.zip'
+  checksum       = 'bd9c559b6d5ee28f6ca5183aba78d6a84e6ca8ddbc86f8f6f590edc4921c296d'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'DNSLookupView'
+}
+Install-ChocolateyZipPackage @packageArgs
