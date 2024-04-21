9c7e6730de2dd085834f1af34f77b302643c33f0
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-04-21 04:24:40 +0300

    takeover the quicksetdns pkg

---
+++ automatic/quicksetdns/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/quicksetdns.zip'
+  checksum       = '36e44a30c7bf9158f358604186b2ab4be46c4c2e0da3f97af8df4c06a20461f3'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'QuickSetDNS'
+}
+Install-ChocolateyZipPackage @packageArgs
