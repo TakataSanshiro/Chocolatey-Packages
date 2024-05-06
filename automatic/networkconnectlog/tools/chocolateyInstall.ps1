7a04736aa66279984ccfc317d0bf54cdb1fabc00
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-05-06 03:09:48 +0300

    takeover the networkconnectlog pkg

---
+++ automatic/networkconnectlog/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/networkconnectlog.zip'
+  checksum       = 'b48e97201145f9d9b15256729e26aceebf28f9a8d8633b9519f5ae9dd9eeeedd'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'NetworkConnectLog'
+}
+Install-ChocolateyZipPackage @packageArgs
