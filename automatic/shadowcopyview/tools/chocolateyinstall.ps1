b6c0f823eb42d509082b5928e4f11d5701af3aa9
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-05-13 02:57:12 +0300

    takeover the shadowcopyview pkg

---
+++ automatic/shadowcopyview/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/shadowcopyview.zip'
+  url64          = 'https://www.nirsoft.net/utils/shadowcopyview-x64.zip'
+  checksum       = '177ad1b42c4392b38225427b3015054bd61a1d7f2e651943b03f653f02c32c74'
+  checksum64     = '55c72769e4e13b9a712cf8a7a7ea74eb25014af2ef6598513bc1429ea0746f3d'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'ShadowCopyView'
+}
+Install-ChocolateyZipPackage @packageArgs
