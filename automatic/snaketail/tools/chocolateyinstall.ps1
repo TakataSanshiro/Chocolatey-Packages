b4cd051f3134d30bbb1d34deb02da4582ec114f7
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-05-13 03:33:12 +0300

    takeover the snaketail pkg

---
+++ automatic/snaketail/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿#Install-VirtualPackage 'snaketail.portable' 'snaketail.install'
---
+++ automatic/snaketail/update.ps1
@@ -0,0 +1,11 @@
+. $PSScriptRoot\..\snaketail.install\update.ps1
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+        }
+    }
+}
+
+update -ChecksumFor none
