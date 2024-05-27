64eb62a5d16479b37d75225f43a68c6b6dba3f0b
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-05-27 04:13:38 +0300

    takeover the kdiff3 pkg

---
+++ automatic/kdiff3/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'kdiff3'
+  file64         = (Get-Childitem -Path $toolsDir -Filter "*.exe").fullname
+  installerType  = 'exe'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'KDiff3'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+#------additional setup ----------------
+#add it to the path
+$programPath = "$env:SystemDrive\Program Files\KDiff3\bin"
+if (![System.IO.Directory]::Exists($programPath)) {
+$programPath = "$env:SystemDrive\Program Files (x86)\KDiff3\bin";
+}
+Install-ChocolateyPath $programPath
---
+++ automatic/kdiff3/update.ps1
@@ -0,0 +1,39 @@
+﻿import-module au
+
+$releases = 'https://download.kde.org/stable/kdiff3/'
+
+function global:au_SearchReplace {
+   @{
+		".\legal\VERIFICATION.txt" = @{
+		  "(?i)(x86_64:).*"        = "`${1} $($Latest.URL64)"
+		  "(?i)(checksum type:).*" = "`${1} $($Latest.ChecksumType64)"
+		  "(?i)(checksum64:).*"    = "`${1} $($Latest.Checksum64)"
+		}
+        #".\tools\chocolateyinstall.ps1" = @{
+        #    "(?i)(^\s*File64\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName64)'"
+        #}
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.links.href -match '-windows-x86_64' -notmatch 'sideload|slientfixed|1.11.1|1.11.0' | Select -Last 1 | % { $_ -split '-' | select -First 2 }
+    $version = $version[1]
+
+    @{
+        Version        = $version
+        URL64          = "https://download.kde.org/stable/kdiff3/kdiff3-$version-windows-x86_64.exe"
+    }
+}
+
+function global:au_BeforeUpdate() {
+	Get-RemoteFiles -Purge
+}
+
+
+try {
+    update -checksumfor none
+} catch  {
+    if ($_ -match '404') { Write-Host "$_"; return 'ignore' }
+}
