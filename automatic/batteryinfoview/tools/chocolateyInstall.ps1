3bca63fe18efd922b7cc61ace3c6f52dd330c12a
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-06-29 16:56:01 +0300

    takeover the batteryinfoview pkg

---
+++ automatic/batteryinfoview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/batteryinfoview.zip'
+  checksum       = 'ecbf6ecb79d19c821f04c39f5c622ed85c38f5a6deeedc442366f175a2d016e1'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'BatteryInfoView'
+}
+Install-ChocolateyZipPackage @packageArgs
