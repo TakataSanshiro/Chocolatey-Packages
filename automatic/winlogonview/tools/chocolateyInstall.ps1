a11b529027bf4619dad7eb2ff7f08a477b63201f
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-06-29 16:48:16 +0300

    takeover the winlogonview pkg

---
+++ automatic/winlogonview/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/winlogonview.zip'
+  checksum       = 'e680236347a5c63b25a0ea6f745e0bcb5529fd9939753c3c099ccb9d5b549c8d'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'WinLogOnView'
+}
+Install-ChocolateyZipPackage @packageArgs
