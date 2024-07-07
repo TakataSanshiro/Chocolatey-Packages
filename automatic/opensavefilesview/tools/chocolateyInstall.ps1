b00f19b39666a884f4ac646f0e234b9e5b52b32e
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-07-07 16:24:18 +0300

    takeover the opensavefilesview pkg

---
+++ automatic/opensavefilesview/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/opensavefilesview.zip'
+  url64          = 'https://www.nirsoft.net/utils/opensavefilesview-x64.zip'
+  checksum       = '7aa6322cd9538ff6471424396b9f9f866d8970985a960b876849a0d34452aacb'
+  checksum64     = '8060ca16dddac85f204699cfa9656488943a16553200679e67cd3428d09f06bf'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'OpenSaveFilesView'
+}
+Install-ChocolateyZipPackage @packageArgs
