93fac1082bf2cc4ea0de772587a1607e503e3d6c
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-07-21 15:36:26 +0300

    update browseraddonsview i2p ungoogled-chromium

--- automatic/i2p/tools/chocolateyInstall.ps1
+++ automatic/i2p/tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Start-Process $ahkExe $ahkFile
 $packageArgs = @{
   packageName    = 'i2p'
   fileType       = 'exe'
-  url            = 'https://files.i2p-projekt.de/2.5.2/i2pinstall%5F2.5.2%5Fwindows.exe'
+  url            = 'https://files.i2p-projekt.de/2.6.0/i2pinstall%5F2.6.0%5Fwindows.exe'
   silentArgs     = ''
   validExitCodes = @(0)
   softwareName   = 'i2p*'
-  checksum       = '1aa1ac29620886a7d744424318287c67dc9ead488e6ab434848597ee9db7ce18'
+  checksum       = '13f2e6b3cc9716c89c4d4d3bc9918171fbad5f450171e1c32b1237b5421bc065'
   checksumType   = 'sha256'
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
