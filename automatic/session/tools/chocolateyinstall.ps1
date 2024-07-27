e9ea655d69a8b6212ffb224a05a139f466d051a2
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-07-27 15:59:11 +0300

    update grammarly-for-windows postbox session

--- automatic/session/tools/chocolateyinstall.ps1
+++ automatic/session/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'session'
   installerType  = 'exe'
-  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.12.4/session-desktop-win-x64-1.12.4.exe'
-  checksum       = '3fcd3b658ae97fca1f6358f45c382670ba1c299c95b88922e32e8d35b65ccd5b'
+  url            = 'https://github.com/loki-project/session-desktop/releases/download/v1.12.5/session-desktop-win-x64-1.12.5.exe'
+  checksum       = '26a19e3b117a07281dc3d89ff40a83ca8866809620d5d90bf6babf81e0b5034e'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
