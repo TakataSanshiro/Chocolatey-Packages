02c9f8a2c8c819179a5ed1d79b64eb6200e06679
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-07-27 17:06:16 +0300

    takeover the insideclipboard pkg

---
+++ automatic/insideclipboard/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/insideclipboard.zip'
+  checksum       = '54f7c48f9c270bbddfa4e8b5c055d862d481622480f1c9395965d5e0468006df'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'InsideClipboard'
+}
+Install-ChocolateyZipPackage @packageArgs
