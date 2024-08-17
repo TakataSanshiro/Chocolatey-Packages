fc05b9bfe1a30960679d3fa9c8e1989d18483ad1
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-08-17 16:50:00 +0300

    takeover the netconnectchoose pkg

---
+++ automatic/netconnectchoose/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/netconnectchoose.zip'
+  checksum       = 'a2c9735688b3d763508d5b5012309604ffd365d09f3f2a99e72bc4aff906b132'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'NetConnectChoose'
+}
+Install-ChocolateyZipPackage @packageArgs
