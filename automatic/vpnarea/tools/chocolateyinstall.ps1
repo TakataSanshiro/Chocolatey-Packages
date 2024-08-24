5f426fa153e1cba7430f27d467aa09caad1ee764
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-08-24 16:10:51 +0300

    retire the vpnarea pkg

--- automatic/vpnarea/tools/chocolateyinstall.ps1
+++ automatic/vpnarea/tools/chocolateyinstall.ps1
@@ -1,13 +1,1 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageArgs = @{
-  packageName    = 'vpnarea'
-  fileType       = 'exe'
-  url            = 'https://vpnarea.com/VPNArea.exe'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
-  softwareName   = 'VPNArea*'
-  checksum       = '422edbc9d760fc7e7e5232c07282b56e5cbcde8387b2df88292a0fdeb8af5ebe'
-  checksumType   = 'sha256'
-}
-Install-ChocolateyPackage @packageArgs
+﻿Write-Host ""  ** This package has been retired."" -Foreground Magenta
