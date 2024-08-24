c36d816e65882e41cef3c7f4d18a76e0f07fcd20
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-08-24 16:33:36 +0300

    retire the crowdinspect pkg

--- crowdinspect/tools/chocolateyinstall.ps1
+++ crowdinspect/tools/chocolateyinstall.ps1
@@ -1,17 +1,1 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName = 'crowdinspect'
-$url = 'http://download.crowdstrike.com/crowdinspect/CrowdInspect.zip' 
-$checksum = '32FEC9FE206257901F348512A037BAECC959C7487EB8BAE9913BA010B72CB35E'
-$checksumType = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
-
-# Create an exe shotcut at the desktop.
-Install-ChocolateyShortcut   -ShortcutFilePath "$Home\Desktop\CrowdInspect.lnk" `
-						     -TargetPath "$toolsDir\CrowdInspect.exe"
+﻿Write-Host ""  ** This package has been retired."" -Foreground Magenta
--- crowdinspect/tools/chocolateyuninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Uninstall-ChocolateyZipPackage `
-  -PackageName "crowdinspect" `
-  -ZipFileName "CrowdInspect*" 
-
-Remove-item -path "$Home\Desktop\CrowdInspect.lnk" -Force -ErrorAction 'SilentlyContinue'
-Remove-item -path "$Home\Desktop\CrowdInspect64.exe" -Force -ErrorAction 'SilentlyContinue'
