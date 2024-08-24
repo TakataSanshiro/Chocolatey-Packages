b5d8ebc4c62391eaaefaccb9fa91770c7b7cd708
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-08-24 16:30:28 +0300

    retire the tagflow pkg

--- tagflow/tools/chocolateyinstall.ps1
+++ tagflow/tools/chocolateyinstall.ps1
@@ -1,9 +1,1 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName = 'tagflow' 
-$installerType = 'exe' 
-$url = 'https://tagflow-app-auto-update.herokuapp.com/download/win/' 
-$silentArgs = '/S' 
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes -Checksum DD909BEAA81C828CC7364D123F6B537B37CF2F7A00D7948A14FC65331352A14B -ChecksumType sha256
+﻿Write-Host ""  ** This package has been retired."" -Foreground Magenta
