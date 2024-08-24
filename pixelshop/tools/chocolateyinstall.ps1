61c7bec3a1005ebc7ceee0b8c138d2b9e93ed987
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-08-24 16:24:04 +0300

    retire the pixelshop pkg

--- pixelshop/tools/chocolateyinstall.ps1
+++ pixelshop/tools/chocolateyinstall.ps1
@@ -1,13 +1,1 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName = 'pixelshop'
-$url = 'https://blackflux.com/software/ps/download/PixelShop.zip' 
-$checksum = '3CC3822A0B8337F19DD77B040E1F60A0830D6E316975879A86722E941B30697B'
-$checksumType = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+﻿Write-Host ""  ** This package has been retired."" -Foreground Magenta
