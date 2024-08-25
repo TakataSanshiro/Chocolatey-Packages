f96132c194143b113e2bd63e119ff85c7cafa0af
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-08-25 16:45:01 +0300

    takeover the whoistd.install pkg

---
+++ automatic/whoistd.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  installerType  = 'exe'
+  url            = 'https://www.nirsoft.net/utils/whoistd_setup.exe'
+  checksum       = '355caf9d250634c7f3d15d59ed3be676b61030568b21b2f29a5d1006c3028a9c'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'WhoisThisDomain*'
+  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
