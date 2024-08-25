4c62be9b24f9b853ebb215fa3ac63f018a5886e8
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-08-25 17:00:01 +0300

    takeover the whoistd.portable pkg

---
+++ automatic/whoistd.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/whoistd.zip'
+  checksum       = 'c24d09c7bfb6ba9008ea38bc09b0e4f30b9c55b27708c54724cf7343b76c46cc'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'whoistd.portable'
+}
+Install-ChocolateyZipPackage @packageArgs
