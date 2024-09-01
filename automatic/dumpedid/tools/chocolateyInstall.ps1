188fa821c59de339f67edf48453963b9e5d1998a
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-09-01 16:46:10 +0300

    takeover the dumpedid pkg

---
+++ automatic/dumpedid/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/dumpedid.zip'
+  checksum       = '1773478ecec2d0e7dcb8d41dd3dd1353e26446a4a798ab294a1ce652a9b84609'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'DumpEDID'
+}
+Install-ChocolateyZipPackage @packageArgs
