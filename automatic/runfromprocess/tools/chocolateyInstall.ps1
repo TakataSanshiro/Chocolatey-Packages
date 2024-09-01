c71d94cd3e1a99b908d638e2977ea64a561057ba
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-09-01 16:46:39 +0300

    takeover the runfromprocess pkg

---
+++ automatic/runfromprocess/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://www.nirsoft.net/utils/runfromprocess.zip'
+  checksum       = '1dfe0d12e325480bd65f06a802abc8bb6b6877eba2d442171614c66e271fc0b5'
+  checksumType   = 'sha256'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  softwareName   = 'RunFromProcess'
+}
+Install-ChocolateyZipPackage @packageArgs
