6d374c2f0d8755126ab6b284fc0eb8c77349e864
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-10-05 16:41:11 +0300

    takeover the volumouse.install pkg

---
+++ automatic/volumouse.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  installerType  = 'exe'
+  url            = 'https://www.nirsoft.net/utils/volumouse_setup.exe'
+  checksum       = '6e5b892b5056d407825b88ca9a0c679bd7380506046c37be8f86162363dac450'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Volumouse*'
+  toolsDir       = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
