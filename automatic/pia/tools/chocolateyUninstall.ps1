66b0aa97c21a747f6581aca8bf59f8e225c891be
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-10-13 16:35:22 +0300

    improve pia

--- automatic/pia/tools/chocolateyUninstall.ps1
+++ automatic/pia/tools/chocolateyUninstall.ps1
@@ -9,5 +9,5 @@
 #Uninstall-ChocolateyPackage @UninstallArgs
 
 $File = Join-Path $env:ProgramFiles "Private Internet Access\uninstall.exe"
-Start-Process -FilePath $File -ArgumentList "/qn"
+Start-Process -FilePath $File #-ArgumentList "/qn"
 Start-Sleep -Seconds 30
