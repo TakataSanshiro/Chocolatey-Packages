ea231316ccf17bd690b86d235280dfefca3e1d06
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2024-11-02 15:01:49 +0200

    update grammarly-for-windows imagecacheviewer

--- automatic/imagecacheviewer/tools/chocolateyInstall.ps1
+++ automatic/imagecacheviewer/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url            = 'https://www.nirsoft.net/utils/imagecacheviewer.zip'
-  checksum       = '5ab69af718d8dbdbf61826fc6dfd31a302dae0966a15517d5eb2c9e9046b4996'
+  checksum       = 'b1c5296ac572a8b1807a5553c153445f18ba66ed381174e95c6645499027ae05'
   checksumType   = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   softwareName   = 'ImageCacheViewer'
