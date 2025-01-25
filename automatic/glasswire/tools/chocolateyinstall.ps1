061b5c23d314f0d8d4c74642b90bf7b3a94f6c6f
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2025-01-25 14:41:27 +0200

    improve glasswire

--- automatic/glasswire/tools/chocolateyinstall.ps1
+++ automatic/glasswire/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = 'glasswire'
   installerType  = 'exe'
-  url            = 'https://download.glasswire.com/GlassWireSetup.exe'
+  url            = 'https://download.glasswire.com/f/glasswire-setup-3.4.768-full.exe'
   checksum       = '4beaedc5a10eebc72fca3ae9928028afc402b1e9f92565ff235be93b1217f34f'
   checksumType   = 'sha256'
   silentArgs     = '/S'
--- automatic/glasswire/update.ps1
+++ automatic/glasswire/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://www.glasswire.com/'
+$releases = 'https://www.glasswire.com/changes/'
 
 function global:au_SearchReplace {
    @{
@@ -14,14 +14,13 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
     # -UseBasicParsing
-    $version = $download_page.ParsedHtml.querySelector(".version").innerText
-    $version = $version | % { $_ -split ' ' | select -First 2 }
-    $version = $version | select -Last 1
-    $version = $version.Replace(',', '')
+    $version = $download_page.links.href -match 'setup' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
+    $version = $version | % { $_ -split '-' | select -Last 2 }
+    $version = $version | select -First 1
     
     @{
         Version   = $version
-        URL32     = "https://download.glasswire.com/GlassWireSetup.exe"
+        URL32     = "https://download.glasswire.com/f/glasswire-setup-$version-full.exe"
     }
 }
 
