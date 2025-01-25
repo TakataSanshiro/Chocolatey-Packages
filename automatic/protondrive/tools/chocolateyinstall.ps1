87525880894eb426bb2a731ee549c76ad9c76746
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2025-01-25 16:08:57 +0200

    update protondrive

--- automatic/protondrive/tools/chocolateyinstall.ps1
+++ automatic/protondrive/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protondrive'
   fileType       = 'exe'
-  url            = 'https://proton.me/download/drive/windows/1.7.11/Proton%20Drive%20Setup%201.7.11.exe'
+  url            = 'https://proton.me/download/drive/windows/1.8.1/x64/Proton%20Drive%20Setup%201.8.1.exe'
   silentArgs     = '-s'
   validExitCodes = @(0)
   softwareName   = 'ProtonDrive*'
-  checksum       = '5DC3A632E1434214C01D2AC760002FCDDAEDFC3E4DC5CDB60A5AB49D20AB21E2'
+  checksum       = 'A31DBE748BD9200D192980841B061B37B9705716BDAA7D2FE8824EB9E81ACDCB'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
