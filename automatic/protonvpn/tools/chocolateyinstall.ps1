43f7dae10d7fe79969498a242a5e7e1d4778a8bf
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2025-01-25 16:13:24 +0200

    update protonvpn

--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'protonvpn'
   fileType       = 'exe'
-  url            = 'https://github.com/ProtonVPN/win-app/releases/download/3.5.0/ProtonVPN_v3.5.0_x64.exe'
+  url            = 'https://github.com/ProtonVPN/win-app/releases/download/3.5.1/ProtonVPN_v3.5.1_x64.exe'
   silentArgs     = '/silent'
   validExitCodes = @(0)
   softwareName   = 'ProtonVPN*'
-  checksum       = 'CFD27F4E455DE81D86D6A4F92E87BC66DC2E8321D58CC0864E15D8A2BBF12BB3'
+  checksum       = '5658A2F5506EDE6BFE552BDE6AF35F1DACCD3D7092A60CE4BE85BFF806770056'
   checksumType   = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
