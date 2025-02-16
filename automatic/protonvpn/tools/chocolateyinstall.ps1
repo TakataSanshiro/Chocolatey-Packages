5ba338fd2892a1819ad54bf0844bdeea83f232f8
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2025-02-16 16:24:30 +0200

    fix protonvpn

--- automatic/protonvpn/tools/chocolateyinstall.ps1
+++ automatic/protonvpn/tools/chocolateyinstall.ps1
@@ -1,15 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageArgs = @{
-  packageName    = 'protonvpn'
-  fileType       = 'exe'
-  url            = 'https://github.com/ProtonVPN/win-app/releases/download/3.5.1/ProtonVPN_v3.5.1_x64.exe'
-  silentArgs     = '/silent'
-  validExitCodes = @(0)
-  softwareName   = 'ProtonVPN*'
-  checksum       = '5658A2F5506EDE6BFE552BDE6AF35F1DACCD3D7092A60CE4BE85BFF806770056'
-  checksumType   = 'sha256'
-}
-Install-ChocolateyPackage @packageArgs
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url = 'https://github.com/ProtonVPN/win-app/releases/download/3.5.1/ProtonVPN_v3.5.1_x64.exe'
+$checksum = '5658A2F5506EDE6BFE552BDE6AF35F1DACCD3D7092A60CE4BE85BFF806770056'
+
+#$packageArgs = @{
+#  packageName    = 'protonvpn'
+#  fileType       = 'exe'
+#  url            = 'https://github.com/ProtonVPN/win-app/releases/download/3.5.1/ProtonVPN_v3.5.1_x64.exe'
+#  silentArgs     = '/silent'
+#  validExitCodes = @(0)
+#  softwareName   = 'ProtonVPN*'
+#  checksum       = '5658A2F5506EDE6BFE552BDE6AF35F1DACCD3D7092A60CE4BE85BFF806770056'
+#  checksumType   = 'sha256'
+#}
+#Install-ChocolateyPackage @packageArgs
+
+Get-ChocolateyWebFile -PackageName "$env:ChocolateyPackageName" -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\thefile.exe" -Url $url -Checksum $checksum -ChecksumType 'SHA256' -GetOriginalFileName
+
+Start-Process -FilePath (Get-Childitem -Path $toolsDir -Filter "*.exe").fullname -ArgumentList "/silent"
+Start-Sleep -Seconds 120
+
+Remove-Item -ea 0 -Force -Path $toolsDir\*.exe
 
 Start-WaitandStop "ProtonVPN"
