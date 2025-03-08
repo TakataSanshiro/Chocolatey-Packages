bd969274df453ad8c7a780f12ef8ee0af3d78282
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2025-03-08 14:36:36 +0200

    update grammarly-for-windows tabby ungoogled-chromium

--- automatic/ungoogled-chromium/tools/chocolateyInstall.ps1
+++ automatic/ungoogled-chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\ungoogled-chromium_133.0.6943.126-1.1_windows_x64_x64.zip"
+  file64      = "$toolsDir\ungoogled-chromium_133.0.6943.141-1.1_windows_x64_x64.zip"
   destination = $toolsDir
 }
 
