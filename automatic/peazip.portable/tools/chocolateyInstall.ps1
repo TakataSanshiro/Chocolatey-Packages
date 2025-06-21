1bd84183ad89d513abb28e87c7b611802a559f78
Sanshiro <ncolskdslv02r9f2929kfj@posteo.net>
2025-06-21 16:46:42 +0300

    update dnsquerysniffer element-desktop github-desktop grammarly-for-windows peazip.portable

--- automatic/peazip.portable/tools/chocolateyInstall.ps1
+++ automatic/peazip.portable/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://github.com/peazip/PeaZip/releases/download/10.4.0/peazip_portable-10.4.0.WINDOWS.zip'
-  url64          = 'https://github.com/peazip/PeaZip/releases/download/10.4.0/peazip_portable-10.4.0.WIN64.zip'
-  checksum       = 'a4181feb651ddcdfe7113da686c9b9eae50e64f5d973441dd56f1439a0c4a048'
-  checksum64     = 'c65a03885150c77f3f4d52e15ab08c78c0e13bb926c4bf825ec2b10411e231f3'
+  url            = 'https://github.com/peazip/PeaZip/releases/download/10.5.0/peazip_portable-10.5.0.WINDOWS.zip'
+  url64          = 'https://github.com/peazip/PeaZip/releases/download/10.5.0/peazip_portable-10.5.0.WIN64.zip'
+  checksum       = 'eb85a3fe7962f2e3a82947010a85e9f6b589f4f90ad9a3c7c4da0b136e34a11f'
+  checksum64     = 'b751d9d4d9f486fc4356f30da938003a75c6d02ea1db9c41cfed67ad7d3e6d61'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
